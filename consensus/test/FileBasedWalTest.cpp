737a30da8f407a44f6046f2aac1e99f865923468
Sherman The Tank <5414276+sherman-the-tank@users.noreply.github.com>
2018-08-21 02:00:54 +0800

    Fixes #14 Add more UT for FileBasedWAL

--- consensus/test/FileBasedWalTest.cpp
+++ consensus/test/FileBasedWalTest.cpp
@@ -44,6 +44,67 @@ TEST(FileBasedWal, AppendLogs) {
         ++(*it);
         ++id;
     }
+    EXPECT_EQ(10, id);
+}
+
+
+TEST(FileBasedWal, CacheOverflow) {
+    // Make a message which length > 1KB
+    static const char* kMsg =
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz123456789"
+        "abcdefghijklmnopqrstuvwxyz123456789abcdefghijklmnopqrstuvwxyz-%06d";
+
+    // Force to make each file 1MB, each buffer is 1MB, and there are two
+    // buffers at most
+    FileBasedWalPolicy policy;
+    policy.fileSize = 1;
+    policy.bufferSize = 1;
+    policy.numBuffers = 2;
+
+    TempDir walDir("/tmp/testWal.XXXXXX");
+
+    // Create a new WAL, add one log and close it
+    auto wal = FileBasedWal::getWal(walDir.path(), policy);
+    EXPECT_EQ(-1, wal->lastLogId());
+
+    // Append > 10MB logs in total
+    for (int i = 0; i < 10000; i++) {
+        EXPECT_TRUE(wal->appendLog(i, folly::stringPrintf(kMsg, i)));
+    }
+    EXPECT_EQ(9999, wal->lastLogId());
+    // Close the wal
+    wal.reset();
+
+    // Check the number of files
+    auto files = FileUtils::listAllFilesInDir(walDir.path());
+    EXPECT_EQ(11, files.size());
+
+    // Now let's open it to read
+    wal = FileBasedWal::getWal(walDir.path(), policy);
+    EXPECT_EQ(9999, wal->lastLogId());
+
+    auto it = wal->iterator(0);
+    LogID id = 0;
+    while (it->valid()) {
+        EXPECT_EQ(id, it->logId());
+        EXPECT_EQ(folly::stringPrintf(kMsg, id), it->logMsg());
+        ++(*it);
+        ++id;
+    }
+    EXPECT_EQ(10000, id);
 }
 
 }  // namespace consensus
