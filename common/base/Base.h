192d07695f9f70544e202f95b2479d4e1fc1f931
dutor <440396+dutor@users.noreply.github.com>
2018-09-05 13:31:49 +0800

    [Feature] Added some concurrent utilities, GenericThreadPool, etc. (#3)
    * [Feature] Added some concurrent utilities, GenericThreadPool, etc.

--- common/base/Base.h
+++ common/base/Base.h
@@ -57,6 +57,24 @@
 
 //#include "base/StringUnorderedMap.h"
 
+#define VE_MUST_USE_RESULT              __attribute__((warn_unused_result))
+#define VE_DONT_OPTIMIZE                __attribute__((optimize("O0")))
+
+#define VE_ALWAYS_INLINE                __attribute__((always_inline))
+#define VE_ALWAYS_NO_INLINE             __attribute__((noinline))
+
+#define VE_BEGIN_NO_OPTIMIZATION        _Pragma("GCC push_options") \
+                                        _Pragma("GCC optimize(\"O0\")")
+#define VE_END_NO_OPTIMIZATION          _Pragma("GCC pop_options")
+
+#ifndef UNUSED
+#define UNUSED(x) (void)(x)
+#endif  // UNUSED
+
+#ifndef COMPILER_BARRIER
+#define COMPILER_BARRIER()              asm volatile ("":::"memory")
+#endif  // COMPILER_BARRIER
+
 namespace vesoft {
 
 using GraphSpaceID = int32_t;
