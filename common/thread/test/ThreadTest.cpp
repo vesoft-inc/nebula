c0722dc9503970193163e5b7a0876caa528e1c1b
dutor <440396+dutor@users.noreply.github.com>
2018-09-05 13:31:49 +0800

    [Feature] Added some concurrent utilities, GenericThreadPool, etc. (#3)
    * [Feature] Added some concurrent utilities, GenericThreadPool, etc.

---
+++ common/thread/test/ThreadTest.cpp
@@ -0,0 +1,35 @@
+/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+ *
+ * This source code is licensed under Apache 2.0 License
+ *  (found in the LICENSE.Apache file in the root directory)
+ */
+#include <gtest/gtest.h>
+#include "common/thread/NamedThread.h"
+
+namespace vesoft {
+namespace thread {
+
+TEST(NamedThread, ThreadName) {
+    std::string setname("thread");
+    std::string getname;
+    auto getter = [&] () {
+        NamedThread::Nominator::get(getname);
+    };
+    NamedThread thread(setname, getter);
+    thread.join();
+    ASSERT_EQ(setname, getname);
+}
+
+TEST(NamedThread, ThreadID) {
+    pid_t tid;
+    auto getter = [&] () {
+        tid = ::syscall(SYS_gettid);
+    };
+
+    NamedThread thread("", getter);
+    thread.join();
+    ASSERT_EQ(tid, thread.tid());
+}
+
+}   // namespace thread
+}   // namespace vesoft
