90a711276b1fe70e2c999d8dc45692be252b390e
Sherman The Tank <5414276+sherman-the-tank@users.noreply.github.com>
2018-09-06 15:49:37 +0800

    Fixed build on dev server (#6)
    Woohoo!

--- third-party/rocksdb/build.sh
+++ third-party/rocksdb/build.sh
@@ -21,10 +21,18 @@ echo
 
 cd $SOURCE_DIR
 
-if (CC=$GCC_ROOT/bin/gcc CPP=$GCC_ROOT/bin/cpp CXX=$GCC_ROOT/bin/g++     PLATFORM=OS_LINUX      CCFLAGS="-DROCKSDB_PLATFORM_POSIX -DROCKSDB_SUPPORT_THREAD_LOCAL -DOS_LINUX -fno-builtin-memcmp -DZLIB -DBZIP2 -DSNAPPY -DZSTD -DROCKSDB_MALLOC_USABLE_SIZE -march=native -fPIC -DPIC -Wno-error=shadow  -I$INSTALL_PATH/include -I$BOOST_RELEASE/include -I$OPENSSL_RELEASE/include   $EXTRA_CXXFLAGS"             CXXFLAGS=$CCFLAGS                  PLATFORM_LDFLAGS="-static-libstdc++ -static-libgcc -L$INSTALL_PATH/lib -L$BOOST_RELEASE/lib -L$OPENSSL_RELEASE/lib   $EXTRA_LDFLAGS -lz -lbz2 -lsnappy -lzstd"                     JAVA_LDFLAGS=$PLATFORM_LDFLAGS       INSTALL_PATH=$INSTALL_PATH    DEBUG_LEVEL=0         make $@ static_lib); then
+if !($CMAKE_ROOT/bin/cmake $CMAKE_FLAGS -DBOOST_ROOT=$TOOLS_ROOT/boost -DOPENSSL_ROOT_DIR=$TOOLS_ROOT/openssl .); then
+    cd $CURR_DIR
+    echo
+    echo "### $PROJECT_NAME failed to install ###"
+    echo
+    exit 1
+fi
+
+if (PLATFORM=OS_LINUX      CCFLAGS="-DROCKSDB_PLATFORM_POSIX -DROCKSDB_SUPPORT_THREAD_LOCAL -DOS_LINUX -fno-builtin-memcmp -DZLIB -DBZIP2 -DSNAPPY -DZSTD -DROCKSDB_MALLOC_USABLE_SIZE -march=native -fPIC -DPIC -Wno-error=shadow  -I$INSTALL_PATH/include   $EXTRA_CXXFLAGS"             CXXFLAGS=$CCFLAGS                  PLATFORM_LDFLAGS="-static-libstdc++ -static-libgcc -L$INSTALL_PATH/lib   $EXTRA_LDFLAGS -lz -lbz2 -lsnappy -lzstd"                     JAVA_LDFLAGS=$PLATFORM_LDFLAGS      DEBUG_LEVEL=0         make $@); then
     if [[ $SOURCE_DIR/librocksdb.a -nt $INSTALL_PATH/lib/librocksdb.a ]]; then
         echo "===> Ready to install"
-        if (INSTALL_PATH=$INSTALL_PATH   make install-static); then
+        if (INSTALL_PATH=$INSTALL_PATH   make install); then
             cd $CURR_DIR
             echo
             echo ">>> $PROJECT_NAME is built and installed successfully <<<"
