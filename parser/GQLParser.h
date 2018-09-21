663cb7fcb2d2a83c96a6b51e06197ce2017de4fe
dutor <440396+dutor@users.noreply.github.com>
2018-09-21 15:10:33 +0800

    First Implementation of GQL parser (#11)
    * First Implementation of GQL parser
    
    * Address @sherman-the-tank 's comments
    
    * Implement syntax of UPDATE/INSERT
    
    In addition, added support for hexadecimal and octal numbers.
    
    * Reserve space on the buffer in `toString`

---
+++ parser/GQLParser.h
@@ -0,0 +1,46 @@
+/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+ *
+ * This source code is licensed under Apache 2.0 License
+ *  (found in the LICENSE.Apache file in the root directory)
+ */
+#ifndef PARSER_GQLPARSER_H_
+#define PARSER_GQLPARSER_H_
+
+#include <sstream>
+#include <regex>
+#include "VGraphParser.hpp"
+#include "VGraphScanner.h"
+
+namespace vesoft {
+
+class GQLParser {
+public:
+    GQLParser() : parser_(scanner_, error_, &statement_) {
+    }
+
+    bool parse(const std::string &query) {
+        std::istringstream is(query);
+        scanner_.switch_streams(&is, nullptr);
+        return parser_.parse() == 0;
+    }
+
+    auto statement() {
+        std::unique_ptr<Statement> statement(statement_);
+        statement_ = nullptr;
+        return statement;
+    }
+
+    const std::string& error() const {
+        return error_;
+    }
+
+private:
+    vesoft::VGraphScanner           scanner_;
+    vesoft::VGraphParser            parser_;
+    std::string                     error_;
+    Statement                      *statement_ = nullptr;
+};
+
+}   // namespace vesoft
+
+#endif  // PARSER_GQLPARSER_H_
