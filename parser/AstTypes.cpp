b4e0b1d8a0f3d756929f449483f3d51b69cd5d2f
dutor <440396+dutor@users.noreply.github.com>
2018-09-21 15:10:33 +0800

    First Implementation of GQL parser (#11)
    * First Implementation of GQL parser
    
    * Address @sherman-the-tank 's comments
    
    * Implement syntax of UPDATE/INSERT
    
    In addition, added support for hexadecimal and octal numbers.
    
    * Reserve space on the buffer in `toString`

---
+++ parser/AstTypes.cpp
@@ -0,0 +1,12 @@
+/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+ *
+ * This source code is licensed under Apache 2.0 License
+ *  (found in the LICENSE.Apache file in the root directory)
+ */
+#include "parser/AstTypes.h"
+
+namespace vesoft {
+
+
+
+}
