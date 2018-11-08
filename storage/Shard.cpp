47adb3fa178394279dd7ffca5d7673fe697b02c7
dangleptr <37216992+dangleptr@users.noreply.github.com>
2018-11-09 03:21:25 +0800

    Add copyright

--- storage/Shard.cpp
+++ storage/Shard.cpp
@@ -1,3 +1,9 @@
+/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+ *
+ * This source code is licensed under Apache 2.0 License
+ *  (found in the LICENSE.Apache file in the root directory)
+ */
+
 #include "storage/Shard.h"
 
 namespace vesoft {
