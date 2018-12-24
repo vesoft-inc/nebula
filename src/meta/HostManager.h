b7f49412b087e27a2625a4c70a4f0549da944de9
Sherman The Tank <5414276+sherman-the-tank@users.noreply.github.com>
2018-12-24 16:35:26 +0800

    Renamed ShardManager to PartManager and some code refactoring (#48)

--- src/meta/HostManager.h
+++ src/meta/HostManager.h
@@ -8,34 +8,64 @@
 #define META_HOSTMANAGER_H_
 
 #include "base/Base.h"
+#include "meta/PartManager.h"
 
 namespace nebula {
 namespace meta {
 
 class HostManager final {
 public:
-    static const HostManager& get();
+    static std::shared_ptr<const HostManager> get(GraphSpaceID space);
 
-    // Get the number of storage hosts for the given space
-    size_t numHosts(GraphSpaceID space) const;
+    // Get the number of storage hosts
+    size_t numHosts() const;
 
-    // Get all storage hosts for the given space
-    const std::vector<HostAddr>& allHosts(GraphSpaceID space) const;
+    // Get all storage hosts
+    const std::vector<HostAddr>& allHosts() const;
 
     // Return one storage host that has the given id
-    HostAddr hostForId(GraphSpaceID space, int64_t id, PartitionID& shard) const;
+    HostAddr hostForId(int64_t id, PartitionID& part) const;
 
     // Cluster given ids into the host they belong to
     // The method returns a map
-    //  host_addr (Any one, in most case, the leader will be chosen)
-    //      => (shard -> [ids that belong to the shard])
-    std::unordered_map<HostAddr, std::unordered_map<PartitionID, std::vector<int64_t>>>
-    clusterIdsToHosts(GraphSpaceID space, std::vector<int64_t>& ids) const;
+    //  host_addr (A host, but in most case, the leader will be chosen)
+    //      => (partition -> [ids that belong to the shard])
+    template<class Container, class GetIdFunc>
+    std::unordered_map<HostAddr,
+                       std::unordered_map<PartitionID,
+                                          std::vector<typename Container::value_type>
+                                         >
+                      >
+    clusterIdsToHosts(Container ids, GetIdFunc f) const {
+        std::unordered_map<HostAddr,
+                           std::unordered_map<PartitionID,
+                                              std::vector<typename Container::value_type>
+                                             >
+                          > clusters;
+
+        auto sm = PartManager::get(space_);
+        if (!sm) {
+            LOG(ERROR) << "Cannot find PartManager for the graph space " << space_;
+        } else {
+            for (auto& id : ids) {
+                PartitionID part = sm->partId(f(id));
+                auto hosts = sm->hostsForPart(part);
+                CHECK_GT(hosts.size(), 0U);
+                // TODO We need to use the leader here
+                clusters[hosts.front()][part].push_back(std::move(id));
+            }
+        }
+
+        return clusters;
+    }
 
 private:
-    HostManager() = default;
+    HostManager(GraphSpaceID space) : space_(space) {}
 
 private:
+    static std::unordered_map<GraphSpaceID, std::shared_ptr<HostManager>> hostManagers_;
+
+    const GraphSpaceID space_;
 };
 
 }  // namespace meta
