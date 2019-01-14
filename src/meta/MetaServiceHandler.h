015a731a05d8708abf36be739374a323543da8e3
dangleptr <37216992+dangleptr@users.noreply.github.com>
2019-01-14 09:48:51 +0800

    Define meta service interface (#78)

---
+++ src/meta/MetaServiceHandler.h
@@ -0,0 +1,37 @@
+/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+ *
+ * This source code is licensed under Apache 2.0 License
+ *  (found in the LICENSE.Apache file in the root directory)
+ */
+
+#ifndef META_METASERVICEHANDLER_H_
+#define META_METASERVICEHANDLER_H_
+
+#include "base/Base.h"
+#include "interface/gen-cpp2/MetaService.h"
+#include "kvstore/include/KVStore.h"
+
+namespace nebula {
+namespace meta {
+
+class MetaServiceHandler final : public cpp2::MetaServiceSvIf {
+public:
+  folly::Future<cpp2::ExecResponse>
+  future_createNode(const cpp2::CreateNodeRequest& req) override;
+
+  folly::Future<cpp2::ExecResponse>
+  future_setNode(const cpp2::SetNodeRequest& req) override;
+
+  folly::Future<cpp2::GetNodeResponse>
+  future_getNode(const cpp2::GetNodeRequest& req) override;
+
+  folly::Future<cpp2::ListChildrenResponse>
+  future_listChildren(const cpp2::ListChildrenRequest& req) override;
+
+private:
+    kvstore::KVStore* kvstore_ = nullptr;
+};
+
+}  // namespace meta
+}  // namespace nebula
+#endif  // META_METASERVICEHANDLER_H_
