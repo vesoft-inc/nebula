4d2147813db511844aaa3cc83abb7ae1f6ee44e7
dutor <440396+dutor@users.noreply.github.com>
2019-01-15 14:49:10 +0800

    Check on *.inl files

--- cpplint/bin/pre-commit.sh
+++ cpplint/bin/pre-commit.sh
@@ -5,7 +5,7 @@
 # This source code is licensed under Apache 2.0 License
 #  (found in the LICENSE.Apache file in the root directory)
 
-CPPLINT_FILE=`dirname $0`/../../cpplint/cpplint.py
+CPPLINT=`dirname $0`/../../cpplint/cpplint.py
 
 if [ $# -eq 0 ];then
     # Since cpplint.py could only apply on our working tree,
@@ -13,27 +13,30 @@ if [ $# -eq 0 ];then
     # Otherwise, lints can be bypassed via changing without commit.
     if ! git diff-files --exit-code --quiet
     then
-        echo "You have unstaged changes, please stage or stash them before commit"
+        echo "You have unstaged changes, please stage or stash them first."
         exit 1
     fi
-    CHECK_FILES=$(git diff --name-only HEAD | egrep '.*\.cpp$|.*\.h$')
+    CHECK_FILES=$(git diff --name-only --diff-filter=d HEAD | egrep '.*\.cpp$|.*\.h$|.*\.inl$')
 else
-    CHECK_FILES=$(find $1 -not \( -path src/CMakeFiles -prune \) \
+    CHECK_FILES=$(find $@ -not \( -path src/CMakeFiles -prune \) \
                           -not \( -path src/interface/gen-cpp2 -prune \) \
-                          -name "*.[h]" -o -name "*.cpp" \
+                          -name "*.[h]" -o -name "*.cpp" -o -name '*.inl' \
                           | grep -v 'GraphScanner.*' | grep -v 'GraphParser.*')
 fi
 
-# No changes on .cpp/.h files
+# No changes on interested files
 if [[ -z $CHECK_FILES ]]
 then
     exit 0
 fi
 
-CPPLINT_EXTENS=cpp,h
-CPPLINT_FITER=-whitespace/indent,-build/include_what_you_use,-readability/todo,-build/include,-build/header_guard,-runtime/references,-build/c++11
 echo "Performing C++ linters..."
-python $CPPLINT_FILE --quiet --extensions=$CPPLINT_EXTENS --filter=$CPPLINT_FITER --linelength=100 $CHECK_FILES 2>&1
+
+CPPLINT_EXTENS=cpp,h,inl
+CPPLINT_FILTER=-whitespace/indent,-build/include_what_you_use,-readability/todo,-build/include,-build/header_guard,-runtime/references,-build/c++11
+
+python $CPPLINT --quiet --extensions=$CPPLINT_EXTENS \
+                --filter=$CPPLINT_FILTER --linelength=100 $CHECK_FILES 2>&1
 
 result=$?
 if [ $result -eq 0 ]
