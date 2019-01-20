cf56882f4a7d272066a8d578eb3c2c3ef11b05b0
dangleptr <37216992+dangleptr@users.noreply.github.com>
2019-01-20 10:08:13 +0800

    Address sherman's comments

--- src/interface/meta.thrift
+++ src/interface/meta.thrift
@@ -18,19 +18,25 @@ enum ErrorCode {
     E_FAIL_TO_CONNECT = -2,
     E_RPC_FAILURE = -3,
 
-    E_LEADER_CHANAGED = -11,
+    E_LEADER_CHANGED = -11,
 
     // Operation Failure
 } (cpp.enum_strict)
 
+struct IdName {
+    1: i32    id,
+    2: string name,
+}
+
 struct ExecResp {
     1: ErrorCode ret,
-    // Valid if ret equals E_LEADER_CHANAGED.
+    // Valid if ret equals E_LEADER_CHANGED.
     2: common.HostAddr  leader,
 }
 
+// Graph space related operations.
 struct CreateSpaceReq {
-    1: common.GraphSpaceID space_id,
+    1: string space_name,
     2: i32 parts_num,
     3: i32 replica_factor,
 }
@@ -39,38 +45,110 @@ struct DeleteSpaceReq {
     1: common.GraphSpaceID space_id,
 }
 
+struct ListSpacesReq {
+}
+
+struct ListSpacesResp {
+    1: ErrorCode code,
+    // Valid if ret equals E_LEADER_CHANGED.
+    2: common.HostAddr  leader,
+    3: list<IdName> spaces,
+}
+
+struct GetSpaceReq {
+    1: common.GraphSpaceID space_id,
+}
+
+struct GetSpaceResp {
+    1: IdName space,
+    2: i32    parts_num,
+    3: i32    replica_factor,
+}
+
+// Tags related operations
 struct AddTagReq {
     1: common.GraphSpaceID space_id,
-    2: i32                 tag_id,
+    2: string              tag_name,
     3: common.Schema       schema,
 }
 
+struct RemoveTagReq {
+    1: common.GraphSpaceID space_id,
+    2: i32                 tag_id,
+}
+
+struct ListTagsReq {
+    1: common.GraphSpaceID space_id,
+}
+
+struct ListTagsResp {
+    1: ErrorCode code,
+    // Valid if ret equals E_LEADER_CHANGED.
+    2: common.HostAddr  leader,
+    3: list<IdName> tags,
+}
+
+struct GetTagReq {
+    1: common.GraphSpaceID space_id,
+    2: i32                 tag_id,
+    3: i32                 version,
+}
+
+struct GetTagResp {
+    1: common.Schema    schema,
+}
+
+// Edge related operations.
 struct AddEdgeReq {
     1: common.GraphSpaceID space_id,
-    2: i32                 edge_type,
+    2: string              edge_name,
     3: common.Schema       schema,
 }
 
-struct GetTagSchemaReq {
+struct RemoveEdgeReq {
     1: common.GraphSpaceID space_id,
-    2: i32                 tag_id,
-    3: i32                 version,
+    2: i32                 edge_type,
 }
 
-struct GetTagSchemaResp {
-    1: common.Schema    schema,
+struct ListEdgesReq {
+    1: common.GraphSpaceID space_id,
 }
 
-struct GetEdgeSchemaReq {
+struct ListEdgesResp {
+    1: ErrorCode code,
+    // Valid if ret equals E_LEADER_CHANGED.
+    2: common.HostAddr  leader,
+    3: list<IdName> edges,
+}
+
+struct GetEdgeReq {
     1: common.GraphSpaceID space_id,
     2: i32                 edge_type,
     3: i32                 version,
 }
 
-struct GetEdgeSchemaResp {
+struct GetEdgeResp {
     1: common.Schema    schema,
 }
 
+// Host related operations.
+struct AddHostsReq {
+    1: list<common.HostAddr> hosts;
+}
+
+struct ListHostsReq {
+}
+
+struct ListHostsResp {
+    1: ExecResp exec_code,
+    2: list<common.HostAddr> hosts,
+}
+
+struct RemoveHostsReq {
+    1: list<common.HostAddr> hosts;
+}
+
+// Parts related operations.
 struct GetPartsAllocReq {
     1: common.GraphSpaceID space_id,
 }
@@ -80,32 +158,25 @@ struct GetPartsAllocResp {
     2: map<common.PartitionID, list<common.HostAddr>>(cpp.template = "std::unordered_map") parts,
 }
 
-struct AddHostsReq {
-    1: list<common.HostAddr> hosts;
-}
-
-struct GetAllHostsResp {
-    1: ExecResp exec_code,
-    2: list<common.HostAddr> hosts,
-}
-
-struct RemoveHostsReq {
-    1: list<common.HostAddr> hosts;
-}
-
 service MetaService {
     ExecResp createSpace(1: CreateSpaceReq req);
     ExecResp deleteSpace(1: DeleteSpaceReq req);
+    GetSpaceResp getSpace(1: GetSpaceReq req);
+    ListSpacesResp listSpaces(1: ListSpacesReq req);
 
     ExecResp addTag(1: AddTagReq req);
-    GetTagSchemaResp getTagSchema(1: GetTagSchemaReq req);
+    ExecResp RemoveTag(1: RemoveTagReq req);
+    GetTagResp getTag(1: GetTagReq req);
+    ListTagsResp listTags(1: ListTagsReq req);
 
     ExecResp addEdge(1: AddEdgeReq req);
-    GetEdgeSchemaResp getEdgeSchema(1: GetEdgeSchemaReq req);
+    ExecResp RemoveEdge(1: RemoveEdgeReq req);
+    GetEdgeResp getEdge(1: GetEdgeReq req);
+    ListEdgesResp listEdges(1: ListEdgesReq req);
 
     ExecResp addHosts(1: AddHostsReq req);
-    GetAllHostsResp getAllHosts();
     ExecResp removeHosts(1: RemoveHostsReq req);
+    ListHostsResp listHosts(1: ListHostsReq req);
 
     GetPartsAllocResp getPartsAlloc(1: GetPartsAllocReq req);
 }
