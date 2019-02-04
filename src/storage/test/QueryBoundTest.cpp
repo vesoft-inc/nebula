e0034311d7e30f370afad304a0d528de6374cb5d
dangleptr <37216992+dangleptr@users.noreply.github.com>
2019-02-04 17:17:33 +0800

    [storage] Support request for some fields in key (#85)
    * Support request for some fields in key
    
    * Address sherman's comments

--- src/storage/test/QueryBoundTest.cpp
+++ src/storage/test/QueryBoundTest.cpp
@@ -18,18 +18,7 @@
 namespace nebula {
 namespace storage {
 
-TEST(QueryBoundTest, OutBoundSimpleTest) {
-    fs::TempDir rootPath("/tmp/QueryBoundTest.XXXXXX");
-    LOG(INFO) << "Prepare meta...";
-    std::unique_ptr<kvstore::KVStore> kv(TestUtils::initKV(rootPath.path()));
-    meta::AdHocSchemaManager::addEdgeSchema(
-        0 /*space id*/, 101 /*edge type*/, TestUtils::genEdgeSchemaProvider(10, 10));
-    for (auto tagId = 3001; tagId < 3010; tagId++) {
-        meta::AdHocSchemaManager::addTagSchema(
-            0 /*space id*/, tagId, TestUtils::genTagSchemaProvider(tagId, 3, 3));
-    }
-
-    LOG(INFO) << "Prepare data...";
+void mockData(kvstore::KVStore* kv) {
     for (auto partId = 0; partId < 3; partId++) {
         std::vector<kvstore::KV> data;
         for (auto vertexId = partId * 10; vertexId < (partId + 1) * 10; vertexId++) {
@@ -47,10 +36,8 @@ TEST(QueryBoundTest, OutBoundSimpleTest) {
             }
             // Generate 7 edges for each edgeType.
             for (auto dstId = 10001; dstId <= 10007; dstId++) {
-                VLOG(3) << "Write part " << partId
-                        << ", vertex " << vertexId
-                        << ", dst " << dstId;
-                auto key = KeyUtils::edgeKey(partId, vertexId, 101, dstId, dstId - 10001, 0);
+                VLOG(3) << "Write part " << partId << ", vertex " << vertexId << ", dst " << dstId;
+                auto key = KeyUtils::edgeKey(partId, vertexId, 101, dstId - 10001, dstId, 0);
                 RowWriter writer(nullptr);
                 for (uint64_t numInt = 0; numInt < 10; numInt++) {
                     writer << numInt;
@@ -69,9 +56,9 @@ TEST(QueryBoundTest, OutBoundSimpleTest) {
                 UNUSED(addr);
             });
     }
+}
 
-    LOG(INFO) << "Build QueryBoundRequest...";
-    cpp2::GetNeighborsRequest req;
+void buildRequest(cpp2::GetNeighborsRequest& req) {
     req.set_space_id(0);
     decltype(req.parts) tmpIds;
     for (auto partId = 0; partId < 3; partId++) {
@@ -91,6 +78,10 @@ TEST(QueryBoundTest, OutBoundSimpleTest) {
                                folly::stringPrintf("tag_%d_col_%d", 3001 + i*2, i*2),
                                3001 + i*2));
     }
+    tmpColumns.emplace_back(TestUtils::propDef(cpp2::PropOwner::EDGE,
+                                               folly::stringPrintf("_dst")));
+    tmpColumns.emplace_back(TestUtils::propDef(cpp2::PropOwner::EDGE,
+                                               folly::stringPrintf("_rank")));
     // Return edge props col_0, col_2, col_4 ... col_18
     for (int i = 0; i < 10; i++) {
         tmpColumns.emplace_back(
@@ -98,17 +89,12 @@ TEST(QueryBoundTest, OutBoundSimpleTest) {
                                folly::stringPrintf("col_%d", i*2)));
     }
     req.set_return_columns(std::move(tmpColumns));
+}
 
-    LOG(INFO) << "Test QueryOutBoundRequest...";
-    auto* processor = QueryBoundProcessor::instance(kv.get());
-    auto f = processor->getFuture();
-    processor->process(req);
-    auto resp = std::move(f).get();
-
-    LOG(INFO) << "Check the results...";
+void checkResponse(cpp2::QueryResponse& resp) {
     EXPECT_EQ(0, resp.result.failed_codes.size());
 
-    EXPECT_EQ(10, resp.edge_schema.columns.size());
+    EXPECT_EQ(12, resp.edge_schema.columns.size());
     EXPECT_EQ(3, resp.vertex_schema.columns.size());
     auto provider = std::make_shared<ResultSchemaProvider>(resp.edge_schema);
     auto tagProvider = std::make_shared<ResultSchemaProvider>(resp.vertex_schema);
@@ -134,33 +120,62 @@ TEST(QueryBoundTest, OutBoundSimpleTest) {
         auto it = rsReader.begin();
         int32_t rowNum = 0;
         while (static_cast<bool>(it)) {
-            auto fieldIt = it->begin();
-            int32_t i = 0;
-            std::stringstream ss;
-            while (static_cast<bool>(fieldIt)) {
-                if (i < 5) {
-                    int64_t v;
-                    EXPECT_EQ(ResultType::SUCCEEDED, fieldIt->getInt<int64_t>(v));
-                    CHECK_EQ(i*2, v);
-                    ss << v << ",";
-                } else {
-                    folly::StringPiece v;
-                    EXPECT_EQ(ResultType::SUCCEEDED, fieldIt->getString(v));
-                    CHECK_EQ(folly::stringPrintf("string_col_%d", i*2), v);
-                    ss << v << ",";
-                }
-                i++;
-                ++fieldIt;
+            EXPECT_EQ(12, it->numFields());
+            {
+                // _dst
+                int64_t v;
+                EXPECT_EQ(ResultType::SUCCEEDED, it->getInt<int64_t>(0, v));
+                CHECK_EQ(10001 + rowNum, v);
             }
-            VLOG(3) << ss.str();
-            EXPECT_EQ(fieldIt, it->end());
-            EXPECT_EQ(10, i);
+            {
+                // _rank
+                int64_t v;
+                EXPECT_EQ(ResultType::SUCCEEDED, it->getInt<int64_t>(1, v));
+                CHECK_EQ(rowNum, v);
+            }
+            // col_0, col_2 ... col_8
+            for (auto i = 2; i < 7; i++) {
+                int64_t v;
+                EXPECT_EQ(ResultType::SUCCEEDED, it->getInt<int64_t>(i, v));
+                CHECK_EQ((i - 2) * 2, v);
+            }
+            // col_10, col_12 ... col_18
+            for (auto i = 7; i < 12; i++) {
+                folly::StringPiece v;
+                EXPECT_EQ(ResultType::SUCCEEDED, it->getString(i, v));
+                CHECK_EQ(folly::stringPrintf("string_col_%d", (i - 7 + 5) * 2), v);
+            }
             ++it;
             rowNum++;
         }
         EXPECT_EQ(it, rsReader.end());
         EXPECT_EQ(7, rowNum);
     }
+}
+
+TEST(QueryBoundTest, OutBoundSimpleTest) {
+    fs::TempDir rootPath("/tmp/QueryBoundTest.XXXXXX");
+    LOG(INFO) << "Prepare meta...";
+    std::unique_ptr<kvstore::KVStore> kv(TestUtils::initKV(rootPath.path()));
+    meta::AdHocSchemaManager::addEdgeSchema(
+        0 /*space id*/, 101 /*edge type*/, TestUtils::genEdgeSchemaProvider(10, 10));
+    for (auto tagId = 3001; tagId < 3010; tagId++) {
+        meta::AdHocSchemaManager::addTagSchema(
+            0 /*space id*/, tagId, TestUtils::genTagSchemaProvider(tagId, 3, 3));
+    }
+    mockData(kv.get());
+
+    cpp2::GetNeighborsRequest req;
+    buildRequest(req);
+
+    LOG(INFO) << "Test QueryOutBoundRequest...";
+    auto* processor = QueryBoundProcessor::instance(kv.get());
+    auto f = processor->getFuture();
+    processor->process(req);
+    auto resp = std::move(f).get();
+
+    LOG(INFO) << "Check the results...";
+    checkResponse(resp);
 }
 
 }  // namespace storage
