b2665b6e90470636747a5e3a22276b263b8e7ddb
Sherman The Tank <5414276+sherman-the-tank@users.noreply.github.com>
2019-02-08 15:37:40 +0800

    Web service
    Implemented a framework to support web service (only support GET method) interface
    
    We made getting/setting flags as a common service (We might add stats-reader as
    a common service later). Each server can also register its own web service handler
    to process specific task

---
+++ src/webservice/GetFlagsHandler.h
@@ -0,0 +1,54 @@
+/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+ *
+ * This source code is licensed under Apache 2.0 License
+ *  (found in the LICENSE.Apache file in the root directory)
+ */
+
+#ifndef WEBSERVICE_GETFLAGSHANDLER_H_
+#define WEBSERVICE_GETFLAGSHANDLER_H_
+
+#include "base/Base.h"
+#include <folly/dynamic.h>
+#include <proxygen/httpserver/RequestHandler.h>
+
+namespace nebula {
+
+class GetFlagsHandler : public proxygen::RequestHandler {
+public:
+    GetFlagsHandler() = default;
+
+    void onRequest(std::unique_ptr<proxygen::HTTPMessage> headers)
+        noexcept override;
+
+    void onBody(std::unique_ptr<folly::IOBuf> body) noexcept override;
+
+    void onEOM() noexcept override;
+
+    void onUpgrade(proxygen::UpgradeProtocol proto) noexcept override;
+
+    void requestComplete() noexcept override;
+
+    void onError(proxygen::ProxygenError err) noexcept override;
+
+private:
+    folly::dynamic getFlags();
+    void addOneFlag(folly::dynamic& vals,
+                    const std::string& flagname,
+                    const gflags::CommandLineFlagInfo* info);
+    std::string toStr(folly::dynamic& vals);
+
+private:
+    enum class ErrorCode {
+        SUCCEEDED = 0,
+        E_UNSUPPORTED_METHOD = -1,
+    };
+
+    ErrorCode err_{ErrorCode::SUCCEEDED};
+    bool verbose_{false};
+    bool returnJson_{false};
+    std::vector<std::string> flagnames_;
+};
+
+}  // namespace nebula
+#endif  // WEBSERVICE_GETFLAGSHANDLER_H_
+
