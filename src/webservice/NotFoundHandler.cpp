d0ee4a17f631b1f416fd8005b5ad1a41c603bcb7
Sherman The Tank <5414276+sherman-the-tank@users.noreply.github.com>
2019-02-13 21:21:46 +0800

    Webservice (#139)
    * Web service
    
    Implemented a framework to support web service (only support GET method) interface
    
    We made getting/setting flags as a common service (We might add stats-reader as
    a common service later). Each server can also register its own web service handler
    to process specific task
    
    * Addressed @dangleptr's comment

---
+++ src/webservice/NotFoundHandler.cpp
@@ -0,0 +1,52 @@
+/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+ *
+ * This source code is licensed under Apache 2.0 License
+ *  (found in the LICENSE.Apache file in the root directory)
+ */
+
+#include "base/Base.h"
+#include "webservice/NotFoundHandler.h"
+#include <proxygen/httpserver/ResponseBuilder.h>
+
+namespace nebula {
+
+using proxygen::HTTPMessage;
+using proxygen::ProxygenError;
+using proxygen::UpgradeProtocol;
+using proxygen::ResponseBuilder;
+
+void NotFoundHandler::onRequest(std::unique_ptr<HTTPMessage>) noexcept {
+    // Do nothing
+}
+
+
+void NotFoundHandler::onBody(std::unique_ptr<folly::IOBuf>) noexcept {
+    // Do nothing, we only support GET
+}
+
+
+void NotFoundHandler::onEOM() noexcept {
+    ResponseBuilder(downstream_)
+        .status(404, "Not Found")
+        .sendWithEOM();
+    return;
+}
+
+
+void NotFoundHandler::onUpgrade(UpgradeProtocol) noexcept {
+    // Do nothing
+}
+
+
+void NotFoundHandler::requestComplete() noexcept {
+    delete this;
+}
+
+
+void NotFoundHandler::onError(ProxygenError err) noexcept {
+    LOG(ERROR) << "Web service NotFoundHandler got error: "
+               << proxygen::getErrorString(err);
+    delete this;
+}
+
+}  // namespace nebula
