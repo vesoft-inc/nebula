fa3ebe3a7a5700ef93cf5aee6dc61ac2ecabbd06
boshengchen <41282030+boshengchen@users.noreply.github.com>
2019-03-15 18:26:20 +0800

    support kvstore engine configuration based flag (#196)
    * support kvstore engine configuration based flag
    
    * default space bumber is 1
    
    * Corrected the naming of variables and functions, and avoid passing KVOptions.
    
    * redundant include line
    
    * using call_once instead of static variable
    
    * Resolve some proposals
    
    * add rocksdb wal disable flag
    
    * delete kvPath testcase
    
    * add ompactionFilter/mergeOperato interface
    
    * Changed compactionFilter and mergeOperator pass them from storage
    
    * Delete temporarily unused flags declarations
    
    * using string options instead of useless gFlags
    
    * Simplified options.
    
    * removed global variable

--- src/kvstore/NebulaStore.cpp
+++ src/kvstore/NebulaStore.cpp
@@ -66,7 +66,7 @@ KVStore* KVStore::instance(KVOptions options) {
 
 Engine NebulaStore::newEngine(GraphSpaceID spaceId, std::string rootPath) {
     if (FLAGS_engine_type == "rocksdb") {
-        auto dataPath = folly::stringPrintf("%s/nebula/%d/data", rootPath.c_str(), spaceId);
+        auto dataPath = KV_DATA_PATH_FORMAT(rootPath.c_str(), spaceId);
         auto engine = std::make_pair(
                                 std::unique_ptr<KVEngine>(
                                     new RocksdbEngine(
@@ -88,8 +88,7 @@ std::unique_ptr<Part> NebulaStore::newPart(GraphSpaceID spaceId,
         return std::unique_ptr<Part>(new SimplePart(
                spaceId,
                partId,
-               folly::stringPrintf("%s/nebula/%d/wals/%d",
-                        engine.second.c_str(), spaceId, partId),
+               KV_WAL_PATH_FORMAT(engine.second.c_str(), spaceId, partId),
                engine.first.get()));
     } else {
         LOG(FATAL) << "Unknown Part type " << FLAGS_part_type;
