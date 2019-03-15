fdd55076c285404362c98f793fe0996186a43eb8
yaphet <darion.yaphet@gmail.com>
2019-03-15 10:26:57 +0800

    Integrate HTTP Interface into Query, Storage and Meta daemons (#210)
    * Support Query, Meta and Storage HTTP interface
    
    * move HttpCode to common.h

---
+++ src/graph/GraphHttpHandler.cpp
@@ -0,0 +1,43 @@
+/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+ *
+ * This source code is licensed under Apache 2.0 License
+ *  (found in the LICENSE.Apache file in the root directory)
+ */
+
+#include "graph/GraphHttpHandler.h"
+#include "webservice/Common.h"
+
+#include <proxygen/lib/http/ProxygenErrorEnum.h>
+#include <proxygen/httpserver/ResponseBuilder.h>
+
+namespace nebula {
+namespace graph {
+
+using proxygen::HTTPMessage;
+using proxygen::HTTPMethod;
+using proxygen::ProxygenError;
+using proxygen::UpgradeProtocol;
+
+void GraphHttpHandler::onRequest(std::unique_ptr<HTTPMessage>) noexcept {
+}
+
+void GraphHttpHandler::onBody(std::unique_ptr<folly::IOBuf>) noexcept {
+}
+
+void GraphHttpHandler::onEOM() noexcept {
+}
+
+void GraphHttpHandler::onUpgrade(UpgradeProtocol) noexcept {
+}
+
+void GraphHttpHandler::requestComplete() noexcept {
+    delete this;
+}
+
+void GraphHttpHandler::onError(ProxygenError error) noexcept {
+    LOG(ERROR) << "Graph Handler : "
+               << proxygen::getErrorString(error);
+}
+
+}  // namespace graph
+}  // namespace nebula
