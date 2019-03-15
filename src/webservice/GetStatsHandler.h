6d4714061d377d2a7b0317044bc493c9b90c3321
steppenwolfyuetong <yuetongprairie@163.com>
2019-03-15 14:21:53 +0800

    Stats reader (#211)
    * support statsReader
    
    * Provide a StatsReader web-service to allow reading stats information via HTTP interface
    
    * modify format
    
    * Address dangleptr's comments
    
    * fix compile error because #210 Integrate HTTP Interface into Query, Storage and Meta daemons

---
+++ src/webservice/GetStatsHandler.h
@@ -0,0 +1,48 @@
+/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+ *
+ * This source code is licensed under Apache 2.0 License
+ *  (found in the LICENSE.Apache file in the root directory)
+ */
+
+#ifndef WEBSERVICE_GETSTATSHANDLER_H_
+#define WEBSERVICE_GETSTATSHANDLER_H_
+
+#include "base/Base.h"
+#include "webservice/Common.h"
+#include <folly/dynamic.h>
+#include <proxygen/httpserver/RequestHandler.h>
+
+namespace nebula {
+
+class GetStatsHandler : public proxygen::RequestHandler {
+public:
+    GetStatsHandler() = default;
+
+    void onRequest(std::unique_ptr<proxygen::HTTPMessage> headers)
+        noexcept override;
+
+    void onBody(std::unique_ptr<folly::IOBuf> body) noexcept override;
+
+    void onEOM() noexcept override;
+
+    void onUpgrade(proxygen::UpgradeProtocol proto) noexcept override;
+
+    void requestComplete() noexcept override;
+
+    void onError(proxygen::ProxygenError err) noexcept override;
+
+private:
+    folly::dynamic getStats();
+    void addOneStat(folly::dynamic& vals, const std::string& statName,
+                    int64_t statValue);
+    std::string toStr(folly::dynamic& vals);
+
+private:
+    HttpCode err_{HttpCode::SUCCEEDED};
+    bool returnJson_{false};
+    std::vector<std::string> statNames_;
+};
+
+}  // namespace nebula
+#endif  // WEBSERVICE_GETFLAGSHANDLER_H_
+
