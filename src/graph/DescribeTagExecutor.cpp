6b6404b6d53d2b82cbfc5ad2dff92a6abab472db
laura-ding <48548375+laura-ding@users.noreply.github.com>
2019-03-21 15:58:14 +0800

    Switch the DESCRIBE sentences to use the meta::SchemaManager (#222)
    close #218

--- src/graph/DescribeTagExecutor.cpp
+++ src/graph/DescribeTagExecutor.cpp
@@ -6,6 +6,7 @@
 
 #include "base/Base.h"
 #include "graph/DescribeTagExecutor.h"
+#include "meta/SchemaManager.h"
 
 namespace nebula {
 namespace graph {
@@ -22,10 +23,12 @@ Status DescribeTagExecutor::prepare() {
 
 
 void DescribeTagExecutor::execute() {
-    auto *sm = ectx()->schemaManager();
     auto *name = sentence_->name();
+    auto space = ectx()->rctx()->session()->space();
+    auto schema = meta::SchemaManager::getTagSchema(space, *name);
+
     resp_ = std::make_unique<cpp2::ExecutionResponse>();
-    auto *schema = sm->getTagSchema(*name);
+
     do {
         if (schema == nullptr) {
             onError_(Status::Error("Schema not found for tag `%s'", name->c_str()));
@@ -33,16 +36,17 @@ void DescribeTagExecutor::execute() {
         }
         std::vector<std::string> header{"Column", "Type"};
         resp_->set_column_names(std::move(header));
-        auto &items = schema->getPropertiesSchema()->getItems();
+        uint32_t numFields = schema->getNumFields();
         std::vector<cpp2::RowValue> rows;
-        for (auto &item : items) {
+        for (uint32_t index = 0; index < numFields; index++) {
             std::vector<cpp2::ColumnValue> row;
             row.resize(2);
-            row[0].set_str(item.name_);
-            row[1].set_str(columnTypeToString(item.type_));
+            row[0].set_str(schema->getFieldName(index));
+            row[1].set_str(valueTypeToString(schema->getFieldType(index)));
             rows.emplace_back();
             rows.back().set_columns(std::move(row));
         }
+
         resp_->set_rows(std::move(rows));
     } while (false);
 
