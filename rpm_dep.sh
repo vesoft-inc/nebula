eff7c9f9980f8bbc61b0a6ab46e9a7dc112d17ab
laura.ding <48548375+laura-ding@users.noreply.github.com>
2019-03-27 16:59:59 +0800

    add spec file to build rpm

---
+++ rpm_dep.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# download deps to install rpm 
+
+sudo yum -y install  autoconf automake libtool cmake bison unzip boost gperf
+sudo yum -y install krb5 openssl openssl-devel libunwind libunwind-devel 
+sudo yum -y install ncurses ncurses-devel readline readline-devel 
+
