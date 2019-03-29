31583aada26552db08b64c4715f09463b09af92d
yaphet <darion.yaphet@gmail.com>
2019-03-29 14:11:59 +0800

    Support decoded in JNI (#217)
    * Support decoded in JNI
    
    * remove duplicate dependency

--- src/dataman/NebulaCodecImpl.cpp
+++ src/dataman/NebulaCodecImpl.cpp
@@ -5,11 +5,22 @@
  */
 
 #include <string>
-#include <stdexcept>
+#include "base/Base.h"
 #include "dataman/RowReader.h"
 #include "dataman/RowWriter.h"
 #include "dataman/SchemaWriter.h"
 #include "NebulaCodecImpl.h"
+
+/**
+ * Report error message
+ */
+#define PRINT_ERROR_MESSAGE(code, value) \
+    if (ResultType::SUCCEEDED == code) { \
+        result[field] = value; \
+    } else { \
+        LOG(ERROR) << "ResultType : " << static_cast<int>(code) \
+                   << " Value " << value << std::endl; \
+    }
 
 namespace nebula {
 namespace dataman {
@@ -27,6 +38,8 @@ std::string NebulaCodecImpl::encode(std::vector<Value> values) {
             writer <<  boost::any_cast<float>(value);
         } else if (value.type() == typeid(bool)) {
             writer <<  boost::any_cast<bool>(value);
+        } else {
+            LOG(ERROR) << "Value Type :" << value.type().name() << std::endl;
         }
     }
     std::string result = writer.encode();
@@ -44,8 +57,7 @@ NebulaCodecImpl::decode(std::string encoded,
     }
 
     auto schema = std::make_shared<SchemaWriter>();
-    auto iter = fields.begin();
-    for (; iter != fields.end(); iter++) {
+    for (auto iter = fields.begin(); iter != fields.end(); iter++) {
         schema->appendCol(iter->first, iter->second);
     }
 
@@ -53,49 +65,37 @@ NebulaCodecImpl::decode(std::string encoded,
     ResultType code;
     auto reader = RowReader::getRowReader(encoded, schema);
     std::unordered_map<std::string, Value> result;
-    for (iter = fields.begin(); iter != fields.end(); iter++) {
+    for (auto iter = fields.begin(); iter != fields.end(); iter++) {
         auto field = iter->first;
         switch (iter->second) {
             case cpp2::SupportedType::BOOL:
                 bool b;
                 code = reader->getBool(field, b);
-                if (ResultType::SUCCEEDED == code) {
-                    result[field] = b;
-                }
+                PRINT_ERROR_MESSAGE(code, b);
                 break;
             case cpp2::SupportedType::INT:
                 int32_t i;
                 code = reader->getInt(field, i);
-                if (ResultType::SUCCEEDED == code) {
-                    result[field] = i;
-                }
+                PRINT_ERROR_MESSAGE(code, i);
                 break;
             case cpp2::SupportedType::STRING:
                 code = reader->getString(field, piece);
-                if (ResultType::SUCCEEDED == code) {
-                    result[field] = boost::any(piece.toString());
-                }
+                PRINT_ERROR_MESSAGE(code, piece.toString());
                 break;
             case cpp2::SupportedType::VID:
                 int64_t v;
                 code = reader->getVid(field, v);
-                if (ResultType::SUCCEEDED == code) {
-                    result[field] = v;
-                }
+                PRINT_ERROR_MESSAGE(code, v);
                 break;
             case cpp2::SupportedType::FLOAT:
                 float f;
                 code = reader->getFloat(field, f);
-                if (ResultType::SUCCEEDED == code) {
-                    result[field] = f;
-                }
+                PRINT_ERROR_MESSAGE(code, f);
                 break;
             case cpp2::SupportedType::DOUBLE:
                 double d;
                 code = reader->getDouble(field, d);
-                if (ResultType::SUCCEEDED == code) {
-                    result[field] = d;
-                }
+                PRINT_ERROR_MESSAGE(code, d)
                 break;
             case cpp2::SupportedType::TIMESTAMP:
                 // TODO(darion) Support TIMESTAMP
