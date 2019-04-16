e53f68c1daa9c92e164d961b8b3c4344a3f65ad4
dangleptr <37216992+dangleptr@users.noreply.github.com>
2019-04-16 13:50:16 +0800

    Fix bugs in storage (#277)

--- src/storage/AddVerticesProcessor.cpp
+++ src/storage/AddVerticesProcessor.cpp
@@ -6,6 +6,7 @@
 
 #include "storage/AddVerticesProcessor.h"
 #include <algorithm>
+#include <limits>
 #include "time/TimeUtils.h"
 #include "storage/KeyUtils.h"
 
@@ -14,7 +15,7 @@ namespace storage {
 
 void AddVerticesProcessor::process(const cpp2::AddVerticesRequest& req) {
     VLOG(3) << "Receive AddVerticesRequest...";
-    auto now = time::TimeUtils::nowInMSeconds();
+    auto now = std::numeric_limits<int64_t>::max() - time::TimeUtils::nowInMSeconds();
     const auto& partVertices = req.get_parts();
     auto spaceId = req.get_space_id();
     callingNum_ = partVertices.size();
