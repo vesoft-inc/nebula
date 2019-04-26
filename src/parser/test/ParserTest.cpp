0559670f05bdbc55f16074a6f6fa7863f4b659a3
laura-ding <48548375+laura-ding@users.noreply.github.com>
2019-04-26 15:35:23 +0800

    fix nebula_native_client build failed

--- src/parser/test/ParserTest.cpp
+++ src/parser/test/ParserTest.cpp
@@ -106,7 +106,7 @@ TEST(Parser, UseNamespace) {
 TEST(Parser, DefineTag) {
     {
         GQLParser parser;
-        std::string query = "DEFINE TAG person(name string, age int TTL = 100, "
+        std::string query = "CREATE TAG person(name string, age int TTL = 100, "
                             "married bool, salary double, create_time timestamp)";
         auto result = parser.parse(query);
         ASSERT_TRUE(result.ok()) << result.status();
