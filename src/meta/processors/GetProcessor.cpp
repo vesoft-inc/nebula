5ae0909829bafc21578a13392d478c05b5b74f22
yaphet <darion.yaphet@gmail.com>
2019-05-10 14:26:52 +0800

    Support tag operation in meta client (#295)
    * support tag meta client
    
    * add test cases and logs
    
    * remove duplicate method

--- src/meta/processors/GetProcessor.cpp
+++ src/meta/processors/GetProcessor.cpp
@@ -15,6 +15,7 @@ void GetProcessor::process(const cpp2::GetReq& req) {
     auto key = MetaServiceUtils::assembleSegmentKey(req.get_segment(), req.get_key());
     auto result = doGet(key);
     if (!result.ok()) {
+        LOG(ERROR) << "Get Failed :" << key << " not found!";
         resp_.set_code(cpp2::ErrorCode::E_KEY_NOT_FOUND);
         onFinished();
         return;
