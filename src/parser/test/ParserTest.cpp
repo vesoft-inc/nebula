c3db5373fd10aa0099e371c215f526c3189001f3
dutor <440396+dutor@users.noreply.github.com>
2019-05-13 11:16:27 +0800

    Support IP:Port semantically (#356)
    * Support IP:Port semantically
    
    * Address @dangleptr's comments
    
    * Fixed stupid compiling error

--- src/parser/test/ParserTest.cpp
+++ src/parser/test/ParserTest.cpp
@@ -368,7 +368,7 @@ TEST(Parser, Find) {
 TEST(Parser, AdminOperation) {
     {
         GQLParser parser;
-        std::string query = "add hosts (\"127.0.0.1:1000\", \"127.0.0.1:9000\")";
+        std::string query = "add hosts 127.0.0.1:1000, 127.0.0.1:9000";
         auto result = parser.parse(query);
         ASSERT_TRUE(result.ok()) << result.status();
     }
@@ -380,7 +380,7 @@ TEST(Parser, AdminOperation) {
     }
     {
         GQLParser parser;
-        std::string query = "remove hosts (\"127.0.0.1:1000\", \"127.0.0.1:9000\")";
+        std::string query = "remove hosts 127.0.0.1:1000, 127.0.0.1:9000";
         auto result = parser.parse(query);
         ASSERT_TRUE(result.ok()) << result.status();
     }
