5ab8f3ac3765c78d0d31d035dda740be2835af41
dutor <440396+dutor@users.noreply.github.com>
2019-05-14 16:41:43 +0800

    Adjust the copyright disclaimer

--- src/common/process/ProcessUtils.h
+++ src/common/process/ProcessUtils.h
@@ -1,7 +1,7 @@
-/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+/* Copyright (c) 2018 vesoft inc. All rights reserved.
  *
- * This source code is licensed under Apache 2.0 License
- *  (found in the LICENSE.Apache file in the root directory)
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
  */
 
 #ifndef COMMON_PROCESSUTILS_H_
