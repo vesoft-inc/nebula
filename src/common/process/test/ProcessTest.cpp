8eda4540e6131b0148c53e91ce9eae9f7dd10236
dutor <440396+dutor@users.noreply.github.com>
2019-05-15 15:14:58 +0800

    Fixed unstable tests on ProcessUtils

--- src/common/process/test/ProcessTest.cpp
+++ src/common/process/test/ProcessTest.cpp
@@ -43,12 +43,15 @@ TEST(ProcessUtils, isPidAvailable) {
     }
     {
         // pid file which contains pid of current process
-        auto pidFile = "/tmp/maybe-non-existing-dir/process_test.pid";
+        auto pidFile = folly::stringPrintf("/tmp/non-existing-dir-%d/process_test.pid", ::getpid());
+        SCOPE_EXIT {
+            ::unlink(pidFile.c_str());
+            ::rmdir(fs::FileUtils::dirname(pidFile.c_str()).c_str());
+        };
         auto status = ProcessUtils::makePidFile(pidFile);
         ASSERT_TRUE(status.ok()) << status;
         status = ProcessUtils::isPidAvailable(pidFile);
         ASSERT_FALSE(status.ok());
-        ::unlink(pidFile);
     }
     {
         // pid file not exist
@@ -68,14 +71,16 @@ TEST(ProcessUtils, isPidAvailable) {
                 }
             }
         };
-        auto pidFile = "/tmp/process_test.pid";
+        auto pidFile = folly::stringPrintf("/tmp/process_test-%d.pid", ::getpid());
+        SCOPE_EXIT {
+            ::unlink(pidFile.c_str());
+        };
         auto status = ProcessUtils::makePidFile(pidFile, genPid());
         ASSERT_TRUE(status.ok()) << status;
         // there are chances that the chosen pid was occupied already,
         // but the chances are negligible, so be it.
         status = ProcessUtils::isPidAvailable(pidFile);
         ASSERT_TRUE(status.ok()) << status;
-        ::unlink(pidFile);
     }
 }
 
