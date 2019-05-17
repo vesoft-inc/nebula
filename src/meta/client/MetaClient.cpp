0326776f417d88b3915cc6b2083edea7a5b3fea4
steppenwolfyuetong <yuetongprairie@163.com>
2019-05-17 13:17:30 +0800

    Improve alter edge (#394)
    * alter edge
    
    * address comments
    
    * address comments and fix error using typeId function

--- src/meta/client/MetaClient.cpp
+++ src/meta/client/MetaClient.cpp
@@ -653,6 +653,7 @@ MetaClient::createTagSchema(GraphSpaceID spaceId, std::string name, nebula::cpp2
         return resp.get_id().get_tag_id();
     }, true);
 }
+
 folly::Future<StatusOr<TagID>>
 MetaClient::alterTagSchema(GraphSpaceID spaceId,
                            std::string name,
@@ -719,8 +720,9 @@ MetaClient::createEdgeSchema(GraphSpaceID spaceId, std::string name, nebula::cpp
 }
 
 folly::Future<StatusOr<bool>>
-MetaClient::alterEdge(GraphSpaceID spaceId, std::string name,
-                      std::vector<cpp2::AlterSchemaItem> items) {
+MetaClient::alterEdgeSchema(GraphSpaceID spaceId,
+                            std::string name,
+                            std::vector<cpp2::AlterSchemaItem> items) {
     cpp2::AlterEdgeReq req;
     req.set_space_id(std::move(spaceId));
     req.set_edge_name(std::move(name));
