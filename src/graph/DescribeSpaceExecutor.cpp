6b084ae17ea4478f26b755257ec628c30bdaa50e
darionyaphet <darion.yaphet@gmail.com>
2019-05-17 11:19:03 +0800

    using async

--- src/graph/DescribeSpaceExecutor.cpp
+++ src/graph/DescribeSpaceExecutor.cpp
@@ -21,34 +21,46 @@ Status DescribeSpaceExecutor::prepare() {
 }
 
 void DescribeSpaceExecutor::execute() {
-    auto *mc = ectx()->getMetaClient();
     auto *name = sentence_->spaceName();
     auto spaceId = ectx()->schemaManager()->toGraphSpaceID(*name);
-    auto spaceItem = mc->getSpace(spaceId).get();
-    if (!spaceItem.ok()) {
-        onError_(Status::Error("Space not found `%d`", spaceId));
+    auto future = ectx()->getMetaClient()->getSpace(spaceId);
+    auto *runner = ectx()->rctx()->runner();
+
+    auto cb = [this] (auto &&resp) {
+      if (!resp.ok()) {
+        onError_(Status::Error("Space not found"));
         return;
-    }
+      }
 
-    resp_ = std::make_unique<cpp2::ExecutionResponse>();
-    std::vector<std::string> header{"ID", "Name", "Partition number", "Replica Factor"};
-    resp_->set_column_names(std::move(header));
+      resp_ = std::make_unique<cpp2::ExecutionResponse>();
+      std::vector<std::string> header{"ID", "Name", "Partition number", "Replica Factor"};
+      resp_->set_column_names(std::move(header));
 
-    std::vector<cpp2::RowValue> rows;
-    std::vector<cpp2::ColumnValue> row;
-    row.resize(4);
-    row[0].set_integer(spaceItem.value().get_space_id());
+      std::vector<cpp2::RowValue> rows;
+      std::vector<cpp2::ColumnValue> row;
+      row.resize(4);
+      row[0].set_integer(resp.value().get_space_id());
 
-    auto properties = spaceItem.value().get_properties();
-    row[1].set_str(properties.get_space_name());
-    row[2].set_integer(properties.get_partition_num());
-    row[3].set_integer(properties.get_replica_factor());
+      auto properties = resp.value().get_properties();
+      row[1].set_str(properties.get_space_name());
+      row[2].set_integer(properties.get_partition_num());
+      row[3].set_integer(properties.get_replica_factor());
 
-    rows.emplace_back();
-    rows.back().set_columns(std::move(row));
-    resp_->set_rows(std::move(rows));
-    DCHECK(onFinish_);
-    onFinish_();
+      rows.emplace_back();
+      rows.back().set_columns(std::move(row));
+      resp_->set_rows(std::move(rows));
+      DCHECK(onFinish_);
+      onFinish_();
+    };
+
+    auto error = [this] (auto &&e) {
+        LOG(ERROR) << "Exception caught: " << e.what();
+        DCHECK(onError_);
+        onError_(Status::Error("Internal error"));
+        return;
+    };
+
+    std::move(future).via(runner).thenValue(cb).thenError(error);
 }
 
 void DescribeSpaceExecutor::setupResponse(cpp2::ExecutionResponse &resp) {
