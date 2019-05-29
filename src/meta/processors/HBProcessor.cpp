54beac9cac79ea6dbac304a9a9889da847fa6fa0
dangleptr <37216992+dangleptr@users.noreply.github.com>
2019-05-29 12:58:47 +0800

    Use active hosts when creating space (#425)
    * Use active hosts when creating space
    
    * x

--- src/meta/processors/HBProcessor.cpp
+++ src/meta/processors/HBProcessor.cpp
@@ -11,10 +11,28 @@ DEFINE_int32(expired_hosts_check_interval_sec, 20,
              "Check the expired hosts at the interval");
 DEFINE_int32(expired_threshold_sec, 10 * 60,
              "Hosts will be expired in this time if no heartbeat received");
+DEFINE_bool(hosts_whitelist_enabled, true, "Check host whether in whitelist when received hb");
 
 namespace nebula {
 namespace meta {
 
+void HBProcessor::process(const cpp2::HBReq& req) {
+    HostAddr host(req.host.ip, req.host.port);
+    if (FLAGS_hosts_whitelist_enabled
+            && hostExist(MetaServiceUtils::hostKey(host.first, host.second))
+                == Status::HostNotFound()) {
+        LOG(INFO) << "Reject unregistered host " << host << "!";
+        resp_.set_code(cpp2::ErrorCode::E_INVALID_HOST);
+        onFinished();
+        return;
+    }
+
+    LOG(INFO) << "Receive heartbeat from " << host;
+    HostInfo info;
+    info.lastHBTimeInSec_ = time::TimeUtils::nowInSeconds();
+    ActiveHostsManHolder::hostsMan()->updateHostInfo(host, info);
+    onFinished();
+}
 
 }  // namespace meta
 }  // namespace nebula
