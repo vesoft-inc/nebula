13a849e576aef36e02110ceb98645e505ea498e9
boshengchen <boshengchen@163.com>
2019-06-19 15:20:21 +0800

    User manager in meta (#400)
    * User manager of Meta Server
    
    * 1, Changed user data transport protocol from dataman to thrift. 2, Removed user property email and phone. 3, Added new user property lock status
    
    * 1,Refactor user processors code structure. 2,Remove user properties first name and last name. 3, Add new user properties of resource limit. 4, Changed related thrift structure from 'name' to 'id', 5,Add some comments for role.
    
    * To improve the drop space , on't delete useless role data
    
    * Rebased and added (raftex_obj raftex_thrift_obj wal_obj time_obj) to authentication_test module
    
    * Rebased and resolved some conflicts
    
    * Address laura-ding's comment
    
    * Resolved code alignment

--- src/interface/meta.thrift
+++ src/interface/meta.thrift
@@ -31,6 +31,9 @@ enum ErrorCode {
     E_STORE_FAILURE          = -31,
     E_STORE_SEGMENT_ILLEGAL  = -32,
 
+    E_INVALID_PASSWORD       = -41,
+    E_INPROPER_ROLE          = -42,
+
     E_UNKNOWN        = -99,
 } (cpp.enum_strict)
 
@@ -42,11 +45,27 @@ enum AlterSchemaOp {
     UNKNOWN = 0x04,
 } (cpp.enum_strict)
 
+/**
+** GOD is A global senior administrator.like root of Linux systems.
+** ADMIN is an administrator for a given Graph Space.
+** USER is a normal user for a given Graph Space. A User can access (read and write) the data in the Graph Space.
+** GUEST is a read-only role for a given Graph Space. A Guest cannot modify the data in the Graph Space.
+** Refer to header file src/graph/PermissionManager.h for details.
+**/
+
+enum RoleType {
+    GOD    = 0x01,
+    ADMIN  = 0x02,
+    USER   = 0x03,
+    GUEST  = 0x04,
+} (cpp.enum_strict)
+
 
 union ID {
     1: common.GraphSpaceID  space_id,
     2: common.TagID         tag_id,
     3: common.EdgeType      edge_type,
+    4: common.UserID        user_id,
 }
 
 struct IdName {
@@ -98,6 +117,26 @@ enum HostStatus {
 struct HostItem {
     1: common.HostAddr      hostAddr,
     2: HostStatus           status,
+}
+
+struct UserItem {
+    1: string account;
+    // Disable user if lock status is true.
+    2: bool   is_lock,
+    // The number of queries an account can issue per hour
+    3: i32    max_queries_per_hour,
+    // The number of updates an account can issue per hour
+    4: i32    max_updates_per_hour,
+    // The number of times an account can connect to the server per hour
+    5: i32    max_connections_per_hour,
+    // The number of simultaneous connections to the server by an account
+    6: i32    max_user_connections,
+}
+
+struct RoleItem {
+    1: common.UserID        user_id,
+    2: common.GraphSpaceID  space_id,
+    3: RoleType             role_type,
 }
 
 struct ExecResp {
@@ -311,6 +350,72 @@ struct HBReq {
     1: common.HostAddr host,
 }
 
+struct CreateUserReq {
+    1: UserItem user,
+    2: string encoded_pwd,
+    3: bool missing_ok,
+}
+
+struct DropUserReq {
+    1: string account,
+    2: bool missing_ok,
+}
+
+struct AlterUserReq {
+    1: UserItem user_item,
+}
+
+struct GrantRoleReq {
+    1: RoleItem role_item,
+}
+
+struct RevokeRoleReq {
+    1: RoleItem role_item,
+}
+
+struct GetUserReq {
+    1: string account,
+}
+
+struct GetUserResp {
+    1: ErrorCode code,
+    // Valid if ret equals E_LEADER_CHANGED.
+    2: common.HostAddr  leader,    
+    3: UserItem user_item,
+}
+
+struct ListUsersReq {
+}
+
+struct ListUsersResp {
+    1: ErrorCode code,
+    // Valid if ret equals E_LEADER_CHANGED.
+    2: common.HostAddr  leader,    
+    3: map<common.UserID, UserItem>(cpp.template = "std::unordered_map") users,
+}
+
+struct ListRolesReq {
+    1: common.GraphSpaceID space_id,
+}
+
+struct ListRolesResp {
+    1: ErrorCode code,
+    // Valid if ret equals E_LEADER_CHANGED.
+    2: common.HostAddr  leader,    
+    3: list<RoleItem> roles,
+}
+
+struct ChangePasswordReq {
+    1: string account,
+    2: string new_encoded_pwd,
+    3: string old_encoded_pwd,
+}
+
+struct CheckPasswordReq {
+    1: string account,
+    2: string encoded_pwd,
+}
+
 service MetaService {
     ExecResp createSpace(1: CreateSpaceReq req);
     ExecResp dropSpace(1: DropSpaceReq req);
@@ -343,5 +448,17 @@ service MetaService {
     ScanResp scan(1: ScanReq req);
 
     HBResp           heartBeat(1: HBReq req);
+
+    ExecResp createUser(1: CreateUserReq req);
+    ExecResp dropUser(1: DropUserReq req);
+    ExecResp alterUser(1: AlterUserReq req);
+    ExecResp grantRole(1: GrantRoleReq req);
+    ExecResp revokeRole(1: RevokeRoleReq req);
+    GetUserResp getUser(1: GetUserReq req);
+    ListUsersResp listUsers(1: ListUsersReq req);
+    ListRolesResp listRoles(1: ListRolesReq req);
+    ExecResp changePassword(1: ChangePasswordReq req);
+    ExecResp checkPassword(1: CheckPasswordReq req);
+
 }
 
