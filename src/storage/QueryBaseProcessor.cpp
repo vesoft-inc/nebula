77e13ab118acbb0a1f6c460751f1a86b0175f0f9
heng <heng.chen@vesoft.com>
2019-06-19 19:22:18 +0800

    Fix copyright

--- src/storage/QueryBaseProcessor.cpp
+++ src/storage/QueryBaseProcessor.cpp
@@ -1,8 +1,9 @@
-/* Copyright (c) 2018 - present, VE Software Inc. All rights reserved
+/* Copyright (c) 2019 vesoft inc. All rights reserved.
  *
- * This source code is licensed under Apache 2.0 License
- *  (found in the LICENSE.Apache file in the root directory)
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
  */
+
 #include "storage/QueryBaseProcessor.h"
 
 DEFINE_int32(max_handlers_per_req, 10, "The max handlers used to handle one request");
