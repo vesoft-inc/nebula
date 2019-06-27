f4dfb05f07f62e65fbf7008676b45654551a3197
spacewalkman <spacewalkman@163.com>
2019-06-27 18:49:57 +0800

    Spark sstfile generator (#420)
    * [WIP] add spark sst file generator job to generate sst files per partiton per woker
    
    * add edge's from and to column reference in mapping file, do check those columns exist
    
    * make sure vertex and its outbound edges are in the same partition
    
    * add native client unit test
    
    * manual boxing AnyVal to AnyRef in order to call NativeCLient.encoded ,for that scala has no autoboxing feature like java
    
    * support hive table with date and other partitin columns
    
    * fix double free exception
    
    * remove all rockdbjni related dependency
    
    * use repartitionAndSortWithinPartitions to avoid overlapping sst files key range, update dependency to hadoop 2..7.4
    
    * add mapping file and command line reference, handle mapping load problem
    
    * address comments
    
    * remove duplicate cmake instruction to find JNI header
    
    * fix doc inconsistance
    
    * keep all edges to a single edgeType
    
    * fix flaky UT
    
    * add mapping json schema file and example mapping file
    
    * use hdfs -copyFromLocal to put local sst files to HDFS
    
    * create destination hdfs dir to put sst files before run hdfs -copyFromLocal
    
    * refactor and fix bug when vertex table has only one primary key column but no other column
    
    * edge_type encoded as a property and, clean up local sst file dir and refactor key-value type name
    
    * create parent dir first before creating local sst files
    
    * set java.library.path env variable before run UT in maven surefire pulgin
    
    * files generated suffix with .sst
    
    * COMPILE phase precede PACKAGE phase in default maven lifecycle,so remove redundant COMPILE and enable test in the meantime
    
    * fix build failure caused by imcompatability between maven 3.0.5 and surefire plugin 3.0.0-M2
    
    * add some clearfication about sst file name uniqueness in doc

--- src/tools/native-client/src/main/java/com/vesoft/client/NativeClient.java
+++ src/tools/native-client/src/main/java/com/vesoft/client/NativeClient.java
@@ -12,16 +12,14 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
 
-import org.rocksdb.EnvOptions;
-import org.rocksdb.Options;
-import org.rocksdb.RocksDBException;
-import org.rocksdb.SstFileWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NativeClient implements AutoCloseable {
+public class NativeClient {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NativeClient.class.getName());
+
     static {
-        NativeClientResourceLoader.resourceLoader();
+        System.loadLibrary("nebula_native_client");
     }
 
     public static class Pair {
@@ -47,8 +45,6 @@ public class NativeClient implements AutoCloseable {
         }
     }
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(NativeClient.class.getName());
-
     private static final int PARTITION_ID = 4;
     private static final int VERTEX_ID = 8;
     private static final int TAG_ID = 4;
@@ -58,80 +54,6 @@ public class NativeClient implements AutoCloseable {
     private static final int EDGE_VERSION = 8;
     private static final int VERTEX_SIZE = PARTITION_ID + VERTEX_ID + TAG_ID + TAG_VERSION;
     private static final int EDGE_SIZE = PARTITION_ID + VERTEX_ID + EDGE_TYPE + EDGE_RANKING + VERTEX_ID + EDGE_VERSION;
-
-    private SstFileWriter writer;
-
-    public NativeClient(String path) {
-        EnvOptions env = new EnvOptions();
-        Options options = new Options();
-        options.setCreateIfMissing(true)
-                .setCreateMissingColumnFamilies(true);
-        new NativeClient(path, env, options);
-    }
-
-    public NativeClient(String path, EnvOptions env, Options options) {
-        if (path == null || path.trim().length() == 0) {
-            throw new IllegalArgumentException("File Path should not be null and empty");
-        }
-        writer = new SstFileWriter(env, options);
-        try {
-            writer.open(path);
-        } catch (RocksDBException e) {
-            LOGGER.error("SstFileWriter Open Failed {}", e.getMessage());
-        }
-    }
-
-    public boolean addVertex(String key, Object[] values) {
-        if (checkKey(key) || checkValues(values)) {
-            throw new IllegalArgumentException("Add Vertex key and value should not null");
-        }
-
-        byte[] value = encode(values);
-        try {
-            writer.put(key.getBytes(), value);
-            return true;
-        } catch (RocksDBException e) {
-            LOGGER.error("AddVertex Failed {}", e.getMessage());
-            return false;
-        }
-    }
-
-    public boolean addEdge(String key, Object[] values) {
-        if (checkKey(key) || checkValues(values)) {
-            throw new IllegalArgumentException("Add Vertex key and value should not null");
-        }
-
-        byte[] value = encode(values);
-        try {
-            writer.put(key.getBytes(), value);
-            return true;
-        } catch (RocksDBException e) {
-            LOGGER.error("AddEdge Failed {}", e.getMessage());
-            return false;
-        }
-    }
-
-    public boolean deleteVertex(String key) {
-        return delete(key);
-    }
-
-    public boolean deleteEdge(String key) {
-        return delete(key);
-    }
-
-    private boolean delete(String key) {
-        if (checkKey(key)) {
-            throw new IllegalArgumentException("Add Vertex key and value should not null");
-        }
-
-        try {
-            writer.delete(key.getBytes());
-            return true;
-        } catch (RocksDBException e) {
-            LOGGER.error("Delete Failed {}", e.getMessage());
-            return false;
-        }
-    }
 
     public static byte[] createEdgeKey(int partitionId, long srcId, int edgeType,
                                        long edgeRank, long dstId, long edgeVersion) {
@@ -157,17 +79,9 @@ public class NativeClient implements AutoCloseable {
         return buffer.array();
     }
 
-    private static native byte[] encode(Object[] values);
+    public static native byte[] encode(Object[] values);
 
-    public static byte[] encoded(Object[] values) {
-        return encode(values);
-    }
-
-    private static native Map<String, byte[]> decode(byte[] encoded, Pair[] fields);
-
-    public static Map<String, byte[]> decoded(byte[] encoded, Pair[] fields) {
-        return decode(encoded, fields);
-    }
+    public static native Map<String, byte[]> decode(byte[] encoded, Pair[] fields);
 
     private boolean checkKey(String key) {
         return Objects.isNull(key) || key.length() == 0;
@@ -177,11 +91,4 @@ public class NativeClient implements AutoCloseable {
         return Objects.isNull(values) || values.length == 0
                 || Arrays.asList(values).contains(null);
     }
-
-    @Override
-    public void close() throws Exception {
-        writer.finish();
-        writer.close();
-    }
-
 }
