98adb442f33f23e05e1da17cfc9f67e7b854aa0b
steppenwolfyuetong <yuetongprairie@163.com>
2019-06-28 15:39:58 +0800

    Ttl schema (#422)
    * support time to live
    
    alter tag/edge drop column, and the column to be delete is as TTL column
    
    * improve unreserved keywords feature
    
    * address laura-ding's comment
    
    * address dangleptr's comment
    
    * address dangleptr's comment
    
    * In AlterTagReq/AlterEdgeReq discards AlterSchemaProp, using SchemaProp
    
    * address comments
    
    * address dangleptr's comments
    
    * address comments

--- src/graph/AlterTagExecutor.cpp
+++ src/graph/AlterTagExecutor.cpp
@@ -6,6 +6,7 @@
 
 #include "base/Base.h"
 #include "graph/AlterTagExecutor.h"
+#include "graph/SchemaHelper.h"
 
 namespace nebula {
 namespace graph {
@@ -15,34 +16,28 @@ AlterTagExecutor::AlterTagExecutor(Sentence *sentence,
     sentence_ = static_cast<AlterTagSentence*>(sentence);
 }
 
+
 Status AlterTagExecutor::prepare() {
-    return checkIfGraphSpaceChosen();
+    auto status = checkIfGraphSpaceChosen();
+
+    if (!status.ok()) {
+        return status;
+    }
+
+    const auto& schemaOpts = sentence_->getSchemaOpts();
+    const auto& schemaProps = sentence_->getSchemaProps();
+
+    return SchemaHelper::alterSchema(schemaOpts, schemaProps, options_, schemaProp_);
 }
+
 
 void AlterTagExecutor::execute() {
     auto *mc = ectx()->getMetaClient();
     auto *name = sentence_->name();
-    const auto& schemaOpts = sentence_->schemaOptList();
     auto spaceId = ectx()->rctx()->session()->space();
 
-    std::vector<nebula::meta::cpp2::AlterSchemaItem> schemaItems;
-    for (auto& schemaOpt : schemaOpts) {
-        nebula::meta::cpp2::AlterSchemaItem schemaItem;
-        auto opType = schemaOpt->toType();
-        schemaItem.set_op(std::move(opType));
-        const auto& specs = schemaOpt->columnSpecs();
-        nebula::cpp2::Schema schema;
-        for (auto& spec : specs) {
-            nebula::cpp2::ColumnDef column;
-            column.name = *spec->name();
-            column.type.type = columnTypeToSupportedType(spec->type());
-            schema.columns.emplace_back(std::move(column));
-        }
-        schemaItem.set_schema(std::move(schema));
-        schemaItems.emplace_back(std::move(schemaItem));
-    }
 
-    auto future = mc->alterTagSchema(spaceId, *name, std::move(schemaItems));
+    auto future = mc->alterTagSchema(spaceId, *name, std::move(options_), std::move(schemaProp_));
     auto *runner = ectx()->rctx()->runner();
     auto cb = [this] (auto &&resp) {
         if (!resp.ok()) {
