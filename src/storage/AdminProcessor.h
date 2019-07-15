0a3fbc494c56317a002bb0fd203f8ea7f8f7f57d
dangleptr <37216992+dangleptr@users.noreply.github.com>
2019-07-15 18:52:49 +0800

    Implement admin client for balancer (#553)
    * Implement AdminClient
    
    * Address whitewum's comments
    
    * Address wadeliuyi's comments
    
    * Fix failed compile

---
+++ src/storage/AdminProcessor.h
@@ -0,0 +1,108 @@
+/* Copyright (c) 2019 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+#ifndef STORAGE_TRANSLEADERPROCESSOR_H_
+#define STORAGE_TRANSLEADERPROCESSOR_H_
+
+#include "base/Base.h"
+#include "storage/BaseProcessor.h"
+
+namespace nebula {
+namespace storage {
+
+class TransLeaderProcessor : public BaseProcessor<cpp2::AdminExecResp> {
+public:
+    static TransLeaderProcessor* instance(kvstore::KVStore* kvstore) {
+        return new TransLeaderProcessor(kvstore);
+    }
+
+    void process(const cpp2::TransLeaderReq& req) {
+        UNUSED(req);
+    }
+
+private:
+    explicit TransLeaderProcessor(kvstore::KVStore* kvstore)
+            : BaseProcessor<cpp2::AdminExecResp>(kvstore, nullptr) {}
+};
+
+class AddPartProcessor : public BaseProcessor<cpp2::AdminExecResp> {
+public:
+    static AddPartProcessor* instance(kvstore::KVStore* kvstore) {
+        return new AddPartProcessor(kvstore);
+    }
+
+    void process(const cpp2::AddPartReq& req) {
+        UNUSED(req);
+    }
+
+private:
+    explicit AddPartProcessor(kvstore::KVStore* kvstore)
+            : BaseProcessor<cpp2::AdminExecResp>(kvstore, nullptr) {}
+};
+
+class RemovePartProcessor : public BaseProcessor<cpp2::AdminExecResp> {
+public:
+    static RemovePartProcessor* instance(kvstore::KVStore* kvstore) {
+        return new RemovePartProcessor(kvstore);
+    }
+
+    void process(const cpp2::RemovePartReq& req) {
+        UNUSED(req);
+    }
+
+private:
+    explicit RemovePartProcessor(kvstore::KVStore* kvstore)
+            : BaseProcessor<cpp2::AdminExecResp>(kvstore, nullptr) {}
+};
+
+class MemberChangeProcessor : public BaseProcessor<cpp2::AdminExecResp> {
+public:
+    static MemberChangeProcessor* instance(kvstore::KVStore* kvstore) {
+        return new MemberChangeProcessor(kvstore);
+    }
+
+    void process(const cpp2::MemberChangeReq& req) {
+        UNUSED(req);
+    }
+
+private:
+    explicit MemberChangeProcessor(kvstore::KVStore* kvstore)
+            : BaseProcessor<cpp2::AdminExecResp>(kvstore, nullptr) {}
+};
+
+class AddLearnerProcessor : public BaseProcessor<cpp2::AdminExecResp> {
+public:
+    static AddLearnerProcessor* instance(kvstore::KVStore* kvstore) {
+        return new AddLearnerProcessor(kvstore);
+    }
+
+    void process(const cpp2::AddLearnerReq& req) {
+        UNUSED(req);
+    }
+
+private:
+    explicit AddLearnerProcessor(kvstore::KVStore* kvstore)
+            : BaseProcessor<cpp2::AdminExecResp>(kvstore, nullptr) {}
+};
+
+class WaitingForCatchUpDataProcessor : public BaseProcessor<cpp2::AdminExecResp> {
+public:
+    static WaitingForCatchUpDataProcessor* instance(kvstore::KVStore* kvstore) {
+        return new WaitingForCatchUpDataProcessor(kvstore);
+    }
+
+    void process(const cpp2::CatchUpDataReq& req) {
+        UNUSED(req);
+    }
+
+private:
+    explicit WaitingForCatchUpDataProcessor(kvstore::KVStore* kvstore)
+            : BaseProcessor<cpp2::AdminExecResp>(kvstore, nullptr) {}
+};
+}  // namespace storage
+}  // namespace nebula
+
+#endif  // STORAGE_TRANSLEADERPROCESSOR_H_
