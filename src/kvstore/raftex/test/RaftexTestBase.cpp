ad3ae9d30929c966a5b1f8c5043bd2589475cd97
wade liu <16488659+wadeliuyi@users.noreply.github.com>
2019-07-30 16:20:57 +0800

    fix crash when thrift service port has been used (#627)
    * fix crash when thrift service port has been used
    
    * fix storage crash when raft service port has been used
    
    * unit test failed
    
    * unit test failed
    
    * address dutor's comment
    
    * address comment
    
    * move waitUntilStop out from stop
    
    * rebase master

--- src/kvstore/raftex/test/RaftexTestBase.cpp
+++ src/kvstore/raftex/test/RaftexTestBase.cpp
@@ -174,7 +174,8 @@ void setupRaft(
     // Set up services
     for (int i = 0; i < numCopies; ++i) {
         services.emplace_back(RaftexService::createService(nullptr));
-        services.back()->waitUntilReady();
+        if (!services.back()->start())
+            return;
         uint16_t port = services.back()->getServerPort();
         allHosts.emplace_back(ipInt, port);
     }
