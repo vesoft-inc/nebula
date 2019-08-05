f0d005c686a314a08f643825ad39f38a52562db3
darionyaphet <darion.yaphet@gmail.com>
2019-08-05 17:33:48 +0800

    support status

--- scripts/services.sh
+++ scripts/services.sh
@@ -7,13 +7,12 @@
 
 SCRIPT_PATH=$(cd "$(dirname "$0")";pwd)
 
-if [ $# != 1 ] ; then
-  echo "USAGE: $0 <start|stop|restart|kill>"
+if [ $# != 1 ]; then
+  echo "USAGE: $0 <start|stop|restart|status|kill>"
   exit 1;
 fi
 
-if [ -z $NEBULA_HOME ];
-then
+if [ -z $NEBULA_HOME ]; then
   echo "NEBULA_HOME is not setting, using /usr/local as default value"
   NEBULA_HOME=/usr/local
 fi
@@ -28,7 +27,11 @@ fi
 for host in `cat ${SCRIPT_PATH}/meta.hosts`
 do
   echo "start $host"
-  ssh $host "${NEBULA_HOME}/scripts/nebula.service -c ${NEBULA_HOME}/etc/nebula-metad.conf $1 metad >/dev/null 2>&1 &"
+  if [ $1 = "status" ]; then
+    ssh $host "${NEBULA_HOME}/scripts/nebula.service -c ${NEBULA_HOME}/etc/nebula-metad.conf $1 metad"
+  else
+    ssh $host "${NEBULA_HOME}/scripts/nebula.service -c ${NEBULA_HOME}/etc/nebula-metad.conf $1 metad > /dev/null 2>&1 &"
+  fi
 done
 
 echo "Start Storage Service ..."
@@ -41,7 +44,11 @@ fi
 for host in `cat ${SCRIPT_PATH}/storage.hosts`
 do
   echo "start $host"
-  ssh $host "${NEBULA_HOME}/scripts/nebula.service -c ${NEBULA_HOME}/etc/nebula-storaged.conf $1 storaged >/dev/null 2>&1 &"
+  if [ $1 = "status" ]; then
+    ssh $host "${NEBULA_HOME}/scripts/nebula.service -c ${NEBULA_HOME}/etc/nebula-storaged.conf $1 storaged"
+  else
+    ssh $host "${NEBULA_HOME}/scripts/nebula.service -c ${NEBULA_HOME}/etc/nebula-storaged.conf $1 storaged > /dev/null 2>&1 &"
+  fi
 done
 
 echo "Start Graph Service ..."
@@ -54,5 +61,9 @@ fi
 for host in `cat ${SCRIPT_PATH}/graph.hosts`
 do
   echo "start $host"
-  ssh $host "${NEBULA_HOME}/scripts/nebula.service -c ${NEBULA_HOME}/etc/nebula-graphd.conf $1 graphd >/dev/null 2>&1 &"
+  if [ $1 = "status" ]; then
+    ssh $host "${NEBULA_HOME}/scripts/nebula.service -c ${NEBULA_HOME}/etc/nebula-graphd.conf $1 graphd"
+  else
+    ssh $host "${NEBULA_HOME}/scripts/nebula.service -c ${NEBULA_HOME}/etc/nebula-graphd.conf $1 graphd > /dev/null 2>&1 &"
+  fi
 done
