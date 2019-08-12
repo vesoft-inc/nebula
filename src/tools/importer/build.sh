4451bb49b95311dc03e5709becfe3c975648150a
yaphet <darion.yaphet@gmail.com>
2019-08-12 15:31:11 +0800

    Enhance Java Importer (#719)
    * Enhance Java Importer
    
    * fix ranking type
    
    * update copyright 2019
    
    * add README.md

--- src/tools/importer/build.sh
+++ src/tools/importer/build.sh
@@ -1,3 +1,8 @@
+# Copyright (c) 2018 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License,
+# attached with Common Clause Condition 1.0, found in the LICENSES directory.
+
 # $1: the path of thrift-1.0-SNAPSHOT.jar
 # $2: the path of pom.xml
 mvn compile package -DJAVA_FBTHRIFT_JAR=$1 -f $2/pom.xml
