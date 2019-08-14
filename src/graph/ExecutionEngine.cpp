9fac951d640a99a626067d5f9243e4471835e3cc
dutor <440396+dutor@users.noreply.github.com>
2019-08-14 20:40:37 +0800

    Fixed crash problem due to meta not ready (#769)
    * Fixed crash problem due to meta not ready
    
    * add comments

--- src/graph/ExecutionEngine.cpp
+++ src/graph/ExecutionEngine.cpp
@@ -32,7 +32,8 @@ Status ExecutionEngine::init(std::shared_ptr<folly::IOThreadPoolExecutor> ioExec
     // load data try 3 time
     bool loadDataOk = metaClient_->waitForMetadReady(3);
     if (!loadDataOk) {
-        return Status::Error("ExecutionEngine::init loadData by thread error!");
+        // Resort to retrying in the background
+        LOG(WARNING) << "Failed to synchronously wait for meta service ready";
     }
 
     schemaManager_ = meta::SchemaManager::create();
