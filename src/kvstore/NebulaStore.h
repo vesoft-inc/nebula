67d59c46cb8e8711a5d78bb92c79472a031c356e
dangleptr <37216992+dangleptr@users.noreply.github.com>
2019-08-21 14:43:50 +0800

    Extend CAS in Raft (#800)

--- src/kvstore/NebulaStore.h
+++ src/kvstore/NebulaStore.h
@@ -139,6 +139,11 @@ public:
                            const std::string& prefix,
                            KVCallback cb) override;
 
+    void asyncAtomicOp(GraphSpaceID spaceId,
+                       PartitionID partId,
+                       raftex::AtomicOp op,
+                       KVCallback cb) override;
+
     ErrorOr<ResultCode, std::shared_ptr<Part>> part(GraphSpaceID spaceId,
                                                     PartitionID partId) override;
 
