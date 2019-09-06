1d2bca3cf8173848fc996188c0cf5878704ed001
zhouliang <zlcook@users.noreply.github.com>
2019-09-06 16:29:12 +0800

    Implement delete a vertex (#868)
    * implement delete a vertex
    
    * address the comments of wadeliuyi and dangleptr
    
    * address dangleptr's comments
    
    * address comments of dangleptr and critical27
    
    * address darionyaphet's comments

---
+++ src/storage/test/DeleteEdgesTest.cpp
@@ -0,0 +1,146 @@
+/* Copyright (c) 2019 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+#include "base/Base.h"
+#include <gtest/gtest.h>
+#include <rocksdb/db.h>
+#include "fs/TempDir.h"
+#include "storage/test/TestUtils.h"
+#include "storage/DeleteEdgesProcessor.h"
+#include "storage/AddEdgesProcessor.h"
+#include "base/NebulaKeyUtils.h"
+
+
+namespace nebula {
+namespace storage {
+
+TEST(DeleteEdgesTest, SimpleTest) {
+    fs::TempDir rootPath("/tmp/AddEdgesTest.XXXXXX");
+    std::unique_ptr<kvstore::KVStore> kv(TestUtils::initKV(rootPath.path()));
+    // Add edges
+    {
+        auto* processor = AddEdgesProcessor::instance(kv.get(), nullptr);
+        LOG(INFO) << "Build AddEdgesRequest...";
+        cpp2::AddEdgesRequest req;
+        req.space_id = 0;
+        req.overwritable = true;
+        // partId => List<Edge>
+        // Edge => {EdgeKey, props}
+        for (auto partId = 0; partId < 3; partId++) {
+            std::vector<cpp2::Edge> edges;
+            for (auto srcId = partId * 10; srcId < 10 * (partId + 1); srcId++) {
+                edges.emplace_back(apache::thrift::FragileConstructor::FRAGILE,
+                                   cpp2::EdgeKey(apache::thrift::FragileConstructor::FRAGILE,
+                                                 srcId,
+                                                 srcId*100 + 1,
+                                                 srcId*100 + 2,
+                                                 srcId*100 + 3),
+                                   folly::stringPrintf("%d_%d", partId, srcId));
+            }
+            req.parts.emplace(partId, std::move(edges));
+        }
+
+        LOG(INFO) << "Test AddEdgesProcessor...";
+        auto fut = processor->getFuture();
+        processor->process(req);
+        auto resp = std::move(fut).get();
+        EXPECT_EQ(0, resp.result.failed_codes.size());
+    }
+    // Add multi version edges
+    {
+        auto* processor = AddEdgesProcessor::instance(kv.get(), nullptr);
+        LOG(INFO) << "Build AddEdgesRequest...";
+        cpp2::AddEdgesRequest req;
+        req.space_id = 0;
+        req.overwritable = true;
+        // partId => List<Edge>
+        // Edge => {EdgeKey, props}
+        for (auto partId = 0; partId < 3; partId++) {
+            std::vector<cpp2::Edge> edges;
+            for (auto srcId = partId * 10; srcId < 10 * (partId + 1); srcId++) {
+                edges.emplace_back(apache::thrift::FragileConstructor::FRAGILE,
+                                   cpp2::EdgeKey(apache::thrift::FragileConstructor::FRAGILE,
+                                                 srcId,
+                                                 srcId*100 + 1,
+                                                 srcId*100 + 2,
+                                                 srcId*100 + 3),
+                                   folly::stringPrintf("%d_%d_new", partId, srcId));
+            }
+            req.parts.emplace(partId, std::move(edges));
+        }
+
+        LOG(INFO) << "Test AddEdgesProcessor...";
+        auto fut = processor->getFuture();
+        processor->process(req);
+        auto resp = std::move(fut).get();
+        EXPECT_EQ(0, resp.result.failed_codes.size());
+    }
+
+    LOG(INFO) << "Check data in kv store...";
+    for (auto partId = 0; partId < 3; partId++) {
+        for (auto srcId = 10 * partId; srcId < 10 * (partId + 1); srcId++) {
+            auto prefix = NebulaKeyUtils::prefix(partId, srcId, srcId*100 + 1);
+            std::unique_ptr<kvstore::KVIterator> iter;
+            EXPECT_EQ(kvstore::ResultCode::SUCCEEDED, kv->prefix(0, partId, prefix, &iter));
+            int num = 0;
+            while (iter->valid()) {
+                if (num == 0) {
+                    EXPECT_EQ(folly::stringPrintf("%d_%d_new", partId, srcId), iter->val());
+                } else {
+                    EXPECT_EQ(folly::stringPrintf("%d_%d", partId, srcId), iter->val());
+                }
+                num++;
+                iter->next();
+            }
+            EXPECT_EQ(2, num);
+        }
+    }
+
+    // Delete edges
+    {
+        auto* processor = DeleteEdgesProcessor::instance(kv.get(), nullptr);
+        LOG(INFO) << "Build DeleteEdgesRequest...";
+        cpp2::DeleteEdgesRequest req;
+        req.set_space_id(0);
+        // partId => List<EdgeKey>
+        for (auto partId = 0; partId < 3; partId++) {
+            std::vector<cpp2::EdgeKey> keys;
+            for (auto srcId = partId * 10; srcId < 10 * (partId + 1); srcId++) {
+                keys.emplace_back(apache::thrift::FragileConstructor::FRAGILE,
+                                  srcId, srcId*100 + 1, srcId*100 + 2, srcId*100 + 3);
+            }
+            req.parts.emplace(partId, std::move(keys));
+        }
+
+        LOG(INFO) << "Test DeleteEdgesProcessor...";
+        auto fut = processor->getFuture();
+        processor->process(req);
+        auto resp = std::move(fut).get();
+        EXPECT_EQ(0, resp.result.failed_codes.size());
+    }
+
+    LOG(INFO) << "Check data again in kv store...";
+    for (auto partId = 0; partId < 3; partId++) {
+        for (auto srcId = 10 * partId; srcId < 10 * (partId + 1); srcId++) {
+            auto prefix = NebulaKeyUtils::prefix(partId, srcId, srcId*100 + 1);
+            std::unique_ptr<kvstore::KVIterator> iter;
+            EXPECT_EQ(kvstore::ResultCode::SUCCEEDED, kv->prefix(0, partId, prefix, &iter));
+            CHECK(!iter->valid());
+        }
+    }
+}
+
+}  // namespace storage
+}  // namespace nebula
+
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    folly::init(&argc, &argv, true);
+    google::SetStderrLogging(google::INFO);
+    return RUN_ALL_TESTS();
+}
+
