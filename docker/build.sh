b7b619a875519202725c107bf145890dfbd708da
Shylock Hg <tcath2s@gmail.com>
2019-09-08 18:55:41 +0800

    Building environment based on ubuntu/centos.

--- docker/build.sh
+++ docker/build.sh
@@ -2,9 +2,6 @@
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker build $BASEDIR -f $BASEDIR/build-ubuntu.Dockerfile -t nebula/build-ubuntu
+docker build $BASEDIR -f $BASEDIR/build-ubuntu.Dockerfile -t vesoft/nebula-dev:ubuntu
 
-#docker build $BASEDIR -f $BASEDIR/build-centos.Dockerfile -t nebula/build-centos
-
-# Need too many build tools
-#docker build $BASEDIR -f $BASEDIR/nebula-centos.Dockerfile -t nebula/nebula-centos
+docker build $BASEDIR -f $BASEDIR/build-centos.Dockerfile -t vesoft/nebula-dev:centos
