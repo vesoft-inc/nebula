4086ca96339164e7cba45260aa8f328ba4a20a81
CPWstatic <13495049+CPWstatic@users.noreply.github.com>
2019-09-09 16:03:01 +0800

    Verify column names. (#885)

--- src/common/filter/Expressions.h
+++ src/common/filter/Expressions.h
@@ -564,7 +564,7 @@ public:
 
     SourcePropertyExpression(std::string *tag, std::string *prop) {
         kind_ = kSourceProp;
-        ref_.reset(new std::string("$^"));
+        ref_.reset(new std::string("$^."));
         alias_.reset(tag);
         prop_.reset(prop);
     }
