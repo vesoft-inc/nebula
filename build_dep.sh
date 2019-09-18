b2d4a44cf36861566877b02c83cccd38c07e2cb8
laura-ding <48548375+laura-ding@users.noreply.github.com>
2019-09-18 21:50:58 +0800

    Add fedora

--- build_dep.sh
+++ build_dep.sh
@@ -7,6 +7,7 @@
 #
 # step 1: ./build_dep.sh <C|U>   C: the user in China, U: the user in US
 # step 2: source ~/.bashrc
+#
 
 DIR=/tmp/download
 mkdir $DIR
@@ -22,36 +23,25 @@ fi
 
 # fedora
 function fedora_install {
-    set -e
     echo "###### start install dep in fedora ######"
     sudo yum -y install autoconf \
         autoconf-archive \
         automake \
         bison \
-        boost \
-        boost-devel \
-        boost-static \
         bzip2-devel \
         cmake \
         curl \
-        flex \
         gcc \
         gcc-c++ \
-        gperf  \
         java-1.8.0-openjdk \
         java-1.8.0-openjdk-devel \
-        krb5-devel \
         libstdc++-static \
         libstdc++-devel \
-        libunwind \
-        libunwind-devel \
         libtool \
         make \
         maven \
         ncurses \
         ncurses-devel \
-        openssl \
-        openssl-devel \
         perl \
         perl-WWW-Curl \
         python \
@@ -60,13 +50,19 @@ function fedora_install {
         unzip \
         xz-devel \
         wget
+    wget $url_addr/fedora.tar.gz
+    tar xf fedora.tar.gz
+    pushd fedora
+        sudo rpm -ivh *.rpm
+    popd
     wget $url_addr/vs-nebula-3rdparty.fc.x86_64.rpm
     sudo rpm -ivh vs-nebula-3rdparty.fc.x86_64.rpm
+    echo "alias cmake='cmake -DNEBULA_GPERF_BIN_DIR=/opt/nebula/gperf/bin -DNEBULA_FLEX_ROOT=/opt/nebula/flex -DNEBULA_BOOST_ROOT=/opt/nebula/boost -DNEBULA_OPENSSL_ROOT=/opt/nebula/openssl -DNEBULA_KRB5_ROOT=/opt/nebula/krb5 -DNEBULA_LIBUNWIND_ROOT=/opt/nebula/libunwind ..'" >> ~/.bashrc
+    return 0
 }
 
 # centos6
 function centos6_install {
-    set -e
     echo "###### start install dep in centos6.5 ######"
     sudo yum -y install wget \
         libtool \
@@ -95,11 +91,11 @@ function centos6_install {
     sudo rpm -ivh vs-nebula-3rdparty.el6.x86_64.rpm
     echo "export PATH=/opt/nebula/autoconf/bin:/opt/nebula/automake/bin:/opt/nebula/libtool/bin:/opt/nebula/gettext/bin:/opt/nebula/flex/bin:/opt/nebula/binutils/bin:\$PATH" >> ~/.bashrc
     echo "export ACLOCAL_PATH=/opt/nebula/automake/share/aclocal-1.15:/opt/nebula/libtool/share/aclocal:/opt/nebula/autoconf-archive/share/aclocal" >> ~/.bashrc
+    return 0
 }
 
 # centos7
 function centos7_install {
-    set -e
     echo "###### start install dep in centos7.5 ######"
     sudo yum -y install wget \
         libtool \
@@ -121,11 +117,12 @@ function centos7_install {
 
     wget $url_addr/vs-nebula-3rdparty.el7.x86_64.rpm
     sudo rpm -ivh vs-nebula-3rdparty.el7.x86_64.rpm
+
+    return 0
 }
 
 # ubuntu1604
 function ubuntu16_install {
-    set -e
     echo "###### start install dep in ubuntu16 ######"
     sudo apt-get -y install gcc-multilib \
         libtool \
@@ -145,11 +142,12 @@ function ubuntu16_install {
 
     wget $url_addr/vs-nebula-3rdparty.ubuntu1604.amd64.deb
     sudo dpkg -i vs-nebula-3rdparty.ubuntu1604.amd64.deb
+
+    return 0
 }
 
 # ubuntu1804
 function ubuntu18_install {
-    set -e
     echo "###### start install dep in ubuntu18 ######"
     sudo apt-get -y install gcc-multilib \
         libtool \
@@ -169,12 +167,15 @@ function ubuntu18_install {
 
     wget $url_addr/vs-nebula-3rdparty.ubuntu1804.amd64.deb
     sudo dpkg -i vs-nebula-3rdparty.ubuntu1804.amd64.deb
+
+    return 0
 }
 
 function addAlias {
     echo "alias cmake='/opt/nebula/cmake/bin/cmake -DCMAKE_C_COMPILER=/opt/nebula/gcc/bin/gcc -DCMAKE_CXX_COMPILER=/opt/nebula/gcc/bin/g++ -DNEBULA_GPERF_BIN_DIR=/opt/nebula/gperf/bin -DNEBULA_FLEX_ROOT=/opt/nebula/flex -DNEBULA_BOOST_ROOT=/opt/nebula/boost -DNEBULA_OPENSSL_ROOT=/opt/nebula/openssl -DNEBULA_KRB5_ROOT=/opt/nebula/krb5 -DNEBULA_LIBUNWIND_ROOT=/opt/nebula/libunwind -DNEBULA_BISON_ROOT=/opt/nebula/bison'" >> ~/.bashrc
     echo "alias ctest='/opt/nebula/cmake/bin/ctest'" >> ~/.bashrc
     echo "export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:\$LIBRARY_PATH" >> ~/.bashrc
+    return 0
 }
 
 # fedora:1, centos7:2, centos6:3, ubuntu18:4, ubuntu16:5
@@ -204,33 +205,35 @@ function getSystemVer {
 
 getSystemVer
 
-case $? in
+version=$?
+
+set -e
+
+case $version in
     1)
         fedora_install
-        exit 0
         ;;
     2)
         centos7_install
         addAlias
-        exit 0
         ;;
     3)
         centos6_install
         addAlias
-        exit 0
         ;;
     4)
         ubuntu18_install
         addAlias
-        exit 0
         ;;
     5)
         ubuntu16_install
         addAlias
-        exit 0
         ;;
     *)
         echo "unknown system"
         exit -1
         ;;
 esac
+
+echo "###### install succeed ######"
+exit 0
