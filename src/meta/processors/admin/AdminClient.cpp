a9bec8e34a8e627f7eaf5ecc2f6e5778fdb42f76
dutor <440396+dutor@users.noreply.github.com>
2019-09-30 12:00:36 +0800

    Fixed bug due to undefined eval order on arguments of function call (#1006)
    * Fixed bug due to undefined eval order of arguments
    
    * Fixed same issue elsewhere

--- src/meta/processors/admin/AdminClient.cpp
+++ src/meta/processors/admin/AdminClient.cpp
@@ -294,8 +294,9 @@ void AdminClient::getResponse(
                     LOG(INFO) << "Rpc failure to " << hosts[index]
                               << ", retry " << retry
                               << ", limit " << retryLimit;
+                    index = ++index % hosts.size();
                     getResponse(std::move(hosts),
-                                (index + 1) % hosts.size(),
+                                index,
                                 std::move(req),
                                 remoteFunc,
                                 retry + 1,
@@ -321,8 +322,9 @@ void AdminClient::getResponse(
                             LOG(INFO) << "The leader is in election"
                                       << ", retry " << retry
                                       << ", limit " << retryLimit;
+                            index = ++index % hosts.size();
                             getResponse(std::move(hosts),
-                                    (index + 1) % hosts.size(),
+                                    index,
                                     std::move(req),
                                     std::move(remoteFunc),
                                     retry + 1,
@@ -368,8 +370,9 @@ void AdminClient::getResponse(
                                   << " from " << hosts[index]
                                   << ", retry " << retry
                                   << ", limit " << retryLimit;
+                        index = ++index % hosts.size();
                         getResponse(std::move(hosts),
-                                    (index + 1) % hosts.size(),
+                                    index,
                                     std::move(req),
                                     std::move(remoteFunc),
                                     retry + 1,
