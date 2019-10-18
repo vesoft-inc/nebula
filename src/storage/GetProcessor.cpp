f4c90fb0ccbeb633c469e1b58660acfe9b19a535
Doodle <13706157+critical27@users.noreply.github.com>
2019-10-18 18:16:22 +0800

    fix key format of key-value (#1110)
    * add kv key style
    
    * address @dangleptr's comments

--- src/storage/GetProcessor.cpp
+++ src/storage/GetProcessor.cpp
@@ -36,10 +36,15 @@ GetProcessor::asyncProcess(PartitionID part,
                            const std::vector<std::string>& keys) {
     folly::Promise<std::pair<PartitionID, kvstore::ResultCode>> promise;
     auto future = promise.getFuture();
+    std::vector<std::string> kvKeys;
+    kvKeys.reserve(keys.size());
+    std::transform(keys.begin(), keys.end(), std::back_inserter(kvKeys),
+                   [part](const auto& key) { return NebulaKeyUtils::kvKey(part, key); });
 
-    executor_->add([this, p = std::move(promise), part, keys] () mutable {
+    executor_->add([this, p = std::move(promise), part, &keys,
+                    kvKeys = std::move(kvKeys)] () mutable {
         std::vector<std::string> values;
-        auto ret = this->kvstore_->multiGet(space_, part, keys, &values);
+        auto ret = this->kvstore_->multiGet(space_, part, kvKeys, &values);
         if (ret == kvstore::ResultCode::SUCCEEDED) {
             std::lock_guard<std::mutex> lg(this->lock_);
             for (int32_t i = 0; i < static_cast<int32_t>(keys.size()); i++) {
