241b88934ffc6c514bdd80837126a3bba013a4b8
laura-ding <48548375+laura-ding@users.noreply.github.com>
2019-10-22 09:27:08 +0800

    Fix link dynamic library

--- package/package.sh
+++ package/package.sh
@@ -65,7 +65,7 @@ function build {
 
     cmake -DCMAKE_BUILD_TYPE=Release -DNEBULA_BUILD_VERSION=${version} -DCMAKE_INSTALL_PREFIX=/usr/local/nebula ..
 
-    if !( make -j10 ); then
+    if !( make -j$(nproc) ); then
         echo ">>> build nebula failed <<<"
         exit -1
     fi
