fb717de9d403ec5383e0df797fa057ed78d1d881
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2019-10-28 17:58:00 +0800

    Remove some useless code .

--- src/tools/spark-sstfile-generator/build-image.sh
+++ src/tools/spark-sstfile-generator/build-image.sh
@@ -3,6 +3,4 @@
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 readonly PROJECT_DIR="$(cd "$SCRIPT_DIR/../../.." && pwd)"
 
-echo $PROJECT_DIR
-
 docker build -f $PROJECT_DIR/src/tools/spark-sstfile-generator/Dockerfile -t vesoft/spark-sstfile-generator $PROJECT_DIR
