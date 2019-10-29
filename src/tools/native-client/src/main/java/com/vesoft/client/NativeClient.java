f444a8484624f929c6092e02bc17114ac529b466
William Chen <13495049+CPWstatic@users.noreply.github.com>
2019-10-29 17:22:33 +0800

    Add geo interface in native client.

--- src/tools/native-client/src/main/java/com/vesoft/client/NativeClient.java
+++ src/tools/native-client/src/main/java/com/vesoft/client/NativeClient.java
@@ -8,10 +8,15 @@ package com.vesoft.client;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import com.google.common.geometry.S2CellId;
+import com.google.common.geometry.S2LatLng;
+import com.google.common.primitives.UnsignedLong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -60,6 +65,16 @@ public class NativeClient {
     private static final int TAG_MASK      = 0xBFFFFFFF;
     private static final int EDGE_MASK     = 0x40000000;
 
+    /**
+     *
+     * @param partitionId partition id that corresponding to the srcId
+     * @param srcId src id
+     * @param edgeType edge type in number
+     * @param edgeRank edge ranking
+     * @param dstId dest id
+     * @param edgeVersion edge version
+     * @return byte array
+     */
     public static byte[] createEdgeKey(int partitionId, long srcId, int edgeType,
                                        long edgeRank, long dstId, long edgeVersion) {
         ByteBuffer buffer = ByteBuffer.allocate(EDGE_SIZE);
@@ -75,6 +90,14 @@ public class NativeClient {
         return buffer.array();
     }
 
+    /**
+     *
+     * @param partitionId partition id that corresponding to the vertex id
+     * @param vertexId vertex id
+     * @param tagId tag id of vertex in number
+     * @param tagVersion tag version
+     * @return byte array
+     */
     public static byte[] createVertexKey(int partitionId, long vertexId,
                                          int tagId, long tagVersion) {
         ByteBuffer buffer = ByteBuffer.allocate(VERTEX_SIZE);
@@ -86,6 +109,59 @@ public class NativeClient {
         buffer.putInt(tagId);
         buffer.putLong(tagVersion);
         return buffer.array();
+    }
+
+    /**
+     *
+     * @param partionNum Total number of partitions
+     * @param lat latitude that corresponding to the dstId
+     * @param lng longitude that corresponding to the dstId
+     * @param minCellLevel minimum cell levels for building the geo index
+     * @param maxCellLevel maximum cell levels for building the geo index
+     * @param edgeType edge type in number
+     * @param edgeRank edge ranking
+     * @param dstId destination id
+     * @param edgeVersion edge version
+     * @return list of the geo key in byte array
+     */
+    public static List<byte[]> createGeoKey(int partionNum,
+                                            double lat, double lng, int minCellLevel, int maxCellLevel,
+                                            int edgeType, long edgeRank, long dstId, long edgeVersion) {
+        List<byte[]> result = new ArrayList<>();
+        List<Long> cellIds = getS2IndexCells(lat, lng, minCellLevel, maxCellLevel);
+        for (Long cellId : cellIds) {
+            int partId = getPartitionId(cellId, partionNum);
+            result.add(createEdgeKey(partId, cellId, edgeType, edgeRank, dstId, edgeVersion));
+        }
+
+        return result;
+    }
+
+    public static List<Long> getS2IndexCells(double lat, double lng, int minCellLevel, int maxCellLevel) {
+        S2LatLng s2LatLng = S2LatLng.fromDegrees(lat, lng);
+        S2CellId s2CellId = S2CellId.fromLatLng(s2LatLng);
+
+        ArrayList<Long> cellIds = new ArrayList<>();
+        for (int i = minCellLevel; i <= maxCellLevel; ++i) {
+            cellIds.add(s2CellId.parent(i).id());
+        }
+
+        return cellIds;
+    }
+
+    /**
+     *
+     * @param id The vertexId
+     * @param partionNum Total number of partitions
+     * @return
+     */
+    public static int getPartitionId(long id, int partionNum) {
+        // We'd better make this as an native interface
+        // which will call the cpp func that generate the partid
+        UnsignedLong unsignedId = UnsignedLong.valueOf(id);
+        UnsignedLong ret = unsignedId.mod(UnsignedLong.valueOf(partionNum));
+
+        return ret.intValue() + 1;
     }
 
     public static native byte[] encode(Object[] values);
--- src/tools/native-client/src/test/java/com/vesoft/client/NativeClientTest.java
+++ src/tools/native-client/src/test/java/com/vesoft/client/NativeClientTest.java
@@ -18,8 +18,10 @@ import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.nio.LongBuffer;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class NativeClientTest {
@@ -72,6 +74,56 @@ public class NativeClientTest {
 
         ByteBuffer stringByteBuffer = ByteBuffer.wrap(decodedResult.get("s_field"));
         assertEquals("Hello", new String(stringByteBuffer.array()));
+    }
+
+    @Test
+    public void testGeo() {
+        List<Long> result = NativeClient.getS2IndexCells(30.28522, 120.01338, 10, 18);
+        List<Long> expect = Arrays.asList(
+                3768216563899957248l, 3768216838777864192l,
+                3768216632619433984l, 3768216684159041536l,
+                3768216671274139648l, 3768216670200397824l,
+                3768216669931962368l, 3768216669730635776l, 3768216669680304128l);
+        Assert.assertEquals(expect, result);
+    }
+
+    @Test
+    public void testCreateEdgeKey() {
+        int partitionId = 1;
+        long srcId = 100;
+        int edgeType = 15;
+        long edgeRank = 0;
+        long dstId = 101l;
+        long version = 1;
+        byte[] result = NativeClient.createEdgeKey(partitionId, srcId, edgeType, edgeRank, dstId, version);
+
+        byte[] key = {0x01, 0x01, 0x00, 0x00,
+                0x64, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x0f, 0x00, 0x00, 0x40,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x65, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+        Assert.assertEquals(key.length, result.length);
+        for (int i = 0; i < key.length; ++i) {
+            Assert.assertEquals(key[i], result[i]);
+        }
+    }
+
+    @Test
+    public void testGeoKey() {
+        List<byte[]> keys = NativeClient.createGeoKey(1,
+                30.28522, 120.01338, 10, 10,
+                1, 0, 100l, 1);
+
+        byte[] expect = NativeClient.createEdgeKey(
+                1,3768216563899957248l,1,0,100l,1);
+
+        Assert.assertEquals(1, keys.size());
+        byte[] result = keys.get(0);
+        for (int i = 0; i < expect.length; ++i) {
+            Assert.assertEquals(expect[i], result[i]);
+        }
     }
 }
 
