86d63d19f5a2335352524fe8e917c196ea3527ac
CPWstatic <13495049+CPWstatic@users.noreply.github.com>
2019-11-27 18:36:03 +0800

    Fix input & output in graph. (#1343)
    * Fix input_output in executors.
    
    * Update UT.

--- src/graph/SetExecutor.cpp
+++ src/graph/SetExecutor.cpp
@@ -122,9 +122,8 @@ void SetExecutor::execute() {
 
         if (leftResult_ == nullptr || rightResult_ == nullptr) {
             // Should not reach here.
-            LOG(ERROR) << "Get null input: " << leftResult_.get()
-                        << " " << rightResult_.get();
-            onError_(Status::Error("Internal error."));
+            LOG(ERROR) << "Get null input.";
+            onError_(Status::Error("Get null input."));
             return;
         }
 
@@ -385,11 +384,12 @@ void SetExecutor::doMinus() {
 }
 
 void SetExecutor::onEmptyInputs() {
-    auto result = std::make_unique<InterimResult>(std::move(colNames_));
     if (onResult_) {
+        auto result = std::make_unique<InterimResult>(std::move(colNames_));
         onResult_(std::move(result));
     } else if (resp_ == nullptr) {
         resp_ = std::make_unique<cpp2::ExecutionResponse>();
+        resp_->set_column_names(std::move(colNames_));
     }
     DCHECK(onFinish_);
     onFinish_(Executor::ProcessControl::kNext);
@@ -445,6 +445,8 @@ void SetExecutor::feedResult(std::unique_ptr<InterimResult> result) {
 
 void SetExecutor::setupResponse(cpp2::ExecutionResponse &resp) {
     if (resp_ == nullptr) {
+        resp_ = std::make_unique<cpp2::ExecutionResponse>();
+        resp_->set_column_names(std::move(colNames_));
         return;
     }
 
