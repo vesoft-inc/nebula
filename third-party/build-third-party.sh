3d69e96f01602647edce911022b717bd4e8864b8
dutor <440396+dutor@users.noreply.github.com>
2019-11-28 19:49:30 +0800

    Added lz4

--- third-party/build-third-party.sh
+++ third-party/build-third-party.sh
@@ -91,7 +91,7 @@ then
     checksum=$(md5sum $source_tar_name | cut -d ' ' -f 1)
 fi
 
-if [[ ! $checksum = aa3356c67ea34d38ed28f762dccf1559 ]]
+if [[ ! $checksum = 375f349b7b5ae1212bd4195bfc30f43a ]]
 then
     hash wget &> /dev/null && download_cmd="wget -c"
     hash axel &> /dev/null && download_cmd="axel -a -n 16"
