3222c12b536e0ee0943081dd5f04bd3f8b185640
dutor <440396+dutor@users.noreply.github.com>
2019-12-04 17:38:50 +0800

    Fix guessing building jobs num

--- third-party/guess-building-jobs-num.sh
+++ third-party/guess-building-jobs-num.sh
@@ -1,8 +1,8 @@
-available_mem_mb=$(awk '/^MemFree|^Buffers|^Cached/{s+=$2;}END{print s/1024;}' /proc/meminfo 2>/dev/null)
+available_mem_mb=$(awk '/^MemFree|^Buffers|^Cached/{s+=$2;}END{print int(s/1024);}' /proc/meminfo 2>/dev/null)
 physical_cores=$(lscpu | awk 'BEGIN{s=1;}/Core\(s\) per socket|Socket\(s\)/{s*=$NF;}END{print s;}' 2>/dev/null)
 building_jobs_num=0
 
-[[ -n $available_mem_mb ]] && jobs_by_mem=$((available_mem_mb / 1024 / 3))
+[[ -n $available_mem_mb ]] && jobs_by_mem=$((available_mem_mb / 1024 / 2))
 [[ -n $physical_cores ]] && jobs_by_cpu=$physical_cores
 
 [[ -n $jobs_by_mem ]] && building_jobs_num=$jobs_by_mem
