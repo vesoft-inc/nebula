9a2351e86bca58787798536b49873181d68171b9
dutor <440396+dutor@users.noreply.github.com>
2019-12-24 11:19:11 +0800

    Unconditionally build flex and bison while building package

--- third-party/build-all-third-party.sh
+++ third-party/build-all-third-party.sh
@@ -7,15 +7,15 @@
 
 # Please note that this utility must be invoked with root privilege.
 
-[[ $# -ne 1 ]] && { echo "$0 <path to build-third-party.sh>" 1>&2; exit 1; }
+this_dir=$(dirname $(readlink -f $0))
 
 for v in 7.1.0 7.5.0 8.3.0 9.1.0 9.2.0
 do
     echo $v;
     source /opt/vesoft/toolset/gcc/$v/enable;
     rm -rf /opt/vesoft/third-party
-    build_package=1 disable_cxx11_abi=0 $1 /opt/vesoft/third-party
+    build_package=1 disable_cxx11_abi=0 $this_dir/build-third-party.sh /opt/vesoft/third-party
     rm -rf /opt/vesoft/third-party
-    build_package=1 disable_cxx11_abi=1 $1 /opt/vesoft/third-party
+    build_package=1 disable_cxx11_abi=1 $this_dir/build-third-party.sh /opt/vesoft/third-party
     source /opt/vesoft/toolset/gcc/$v/disable;
 done
