a50d465e7e5d4f2af7be74749e05ff8faa50a2e6
johnson-623 <54924791+johnson-623@users.noreply.github.com>
2019-12-26 16:58:26 +0800

    Update DurationTest.cpp

--- src/common/time/test/DurationTest.cpp
+++ src/common/time/test/DurationTest.cpp
@@ -10,6 +10,36 @@
 
 using nebula::time::Duration;
 
+TEST(WallClock , time1 ) {
+
+    std::vector<int64_t> timer;
+    std::vector<int64_t>s;
+
+    for (uint32_t i = 0; i < 10; i++) {
+
+        auto sec=WallClock::slowNowInMicroSec();
+        usleep(3000);
+        auto sec2=WallClock::slowNowInMicroSec();
+        auto cost_time=sec2-sec;
+        timer.push_back(cost_time);
+    }
+    std::cout << timer[0] << std::endl;
+    int64_t sum=std::accumulate(timer.begin(),timer.end(),0);
+    double average=sum*1.0/10;
+    std::cout << "Mean Error  : " << average-3000 << "ms "<<std::endl;
+
+    for(int i = 0; i < 10 ; i++ ){
+        int64_t a=(timer[i]-average)*(timer[i]-average);
+        s.push_back(a);
+    }
+
+    int64_t sum2=std::accumulate(s.begin(),s.end(),0);
+    double variance=sum2*1.0/9;
+    std::cout << "The  Variance  Is : "<< variance << std::endl;
+    double error=( abs(average-3000) * 1.0 / 3000) * 100;
+    std :: cout << "Test Error Is  : " << error  << "%" << std::endl;
+}//计算均方误差并输出
+
 TEST(Duration, elapsedInSeconds) {
     for (int i = 0; i < 5; i++) {
         Duration dur;
@@ -48,5 +78,5 @@ int main(int argc, char** argv) {
     google::SetStderrLogging(google::INFO);
 
     return RUN_ALL_TESTS();
-}//打卡
+}
 
