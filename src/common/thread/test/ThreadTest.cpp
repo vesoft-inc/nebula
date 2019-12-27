878775d9ef94dd206db62553f668ed019982e5e0
ctx0509 <ctx@ctxhp.localdomain>
2019-12-28 01:13:46 +0800

    this is my srcond change

--- src/common/thread/test/ThreadTest.cpp
+++ src/common/thread/test/ThreadTest.cpp
@@ -3,11 +3,11 @@
  * This source code is licensed under Apache 2.0 License,
  * attached with Common Clause Condition 1.0, found in the LICENSES directory.
  */
-
+#include "iostream"
 #include "base/Base.h"
 #include <gtest/gtest.h>
 #include "thread/NamedThread.h"
-
+#include "vector"
 namespace nebula {
 namespace thread {
 
@@ -25,6 +25,18 @@ TEST(NamedThread, ThreadName) {
 TEST(NamedThread, ThreadID) {
     ASSERT_EQ(::getpid(), nebula::thread::gettid());
 }
+TEST(NameThread, test) {
+   static int j = 0;
+   std::vector <std::thread>t;
+   for (int i = 0; i < 10; i++) {
+     std::thread mythread(j++);
+     t.push_back(std::move(mythread));
+   }
 
+   for (int i = 0; i < 10 ; i++) {
+   t[i].join();
+}
+     ASSERT_GE(j, 10);
+}
 }   // namespace thread
 }   // namespace nebula
