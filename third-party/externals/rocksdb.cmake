a93df714fa23461e7320ac68548ba55bf88e0d4d
dutor <440396+dutor@users.noreply.github.com>
2020-01-01 12:49:33 +0800

    Refactor building (#1332)
    This PR is trying to improve the building experience, by:
     1. Providing `third-party/install-cmake.sh` to download and install a prebuilt CMake
     2. Providing `third-party/build-gcc.sh` to automatically download and build GCC 7.5.0
     3. Providing `third-party/install-gcc.sh` to download and install a prebuilt GCC 7.5.0 according to the current distro and libc
     4. Providing `third-party/build-third-party.sh` to build and optionally package our third party dependencies
     4. Providing `third-party/build-all-third-party.sh` to invoke `build-third-party.sh` using several preset versions of GCC
     5. Providing `third-party/install-third-party.sh` to download and install a prebuilt third party according to the current version of glibc, GCC(along with its ABI version)
     6. Invoke `install-third-party.sh` automatically during the configure stage(cmake)
    
    Besides, all installing scripts support an `--prefix` option to allow one customizing the install location. For the third party installation, works are done to make *Bison* and *krb5* relocatable.
    
    The fresh new building shceme has been tested under various environments:
     1. CentOS 6/7/8
     2. Debian 7/8/9
     3. Ubuntu 16.04/18.04/19.04
     4. GCC 7/8/9, and devtoolset-7 of CentOS 6/7
    
    Since most of these testing are from inside a Docker image, so the Linux Kernel used is nearly the newest one(5.0). There might be other unfound issues.
    
    To try out this PR, run
    ```sh
    $ git clone --single-branch --depth=1 --branch refactor-building https://github.com/dutor/nebula.git nebula-refactor-building
    $ cd nebula-refactor-building
    $ mkdir build && cd build
    $ ../third-party/install-cmake.sh             # Can be skipped if you have CMake 3.5.0+
    $ source cmake-3.15.5/bin/enable-cmake.sh     # Only necessary if you invoked install-cmake.sh
    $ sudo ../third-party/install-gcc.sh          # Can be skipped if you have GCC 7.1.0+
    $ source /opt/vesoft/toolset/gcc/7.5.0/enable # Only necessary if you invoked install-gcc.sh
    $ cmake ..
    $ make
    ```
    
    BTW. This PR also fixes #1449 
    BTW. This PR reduces the build size a lot, as mentioned in #1401

---
+++ third-party/externals/rocksdb.cmake
@@ -0,0 +1,51 @@
+# Copyright (c) 2019 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License,
+# attached with Common Clause Condition 1.0, found in the LICENSES directory.
+
+set(name rocksdb)
+set(source_dir ${CMAKE_CURRENT_BINARY_DIR}/${name}/source)
+set(ROCKSDB_CXX_FLAGS "-Wno-pessimizing-move -Wno-redundant-move -Wno-deprecated-copy -Wno-error=shadow -Wno-error=sign-compare")
+ExternalProject_Add(
+    ${name}
+    URL https://github.com/facebook/rocksdb/archive/v5.15.10.tar.gz
+    URL_HASH MD5=5b1c1fa7ff4756218514205238d8900d
+    DOWNLOAD_NAME rocksdb-5.15.10.tar.gz
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${name}
+    TMP_DIR ${BUILD_INFO_DIR}
+    STAMP_DIR ${BUILD_INFO_DIR}
+    DOWNLOAD_DIR ${DOWNLOAD_DIR}
+    SOURCE_DIR ${source_dir}
+    UPDATE_COMMAND ""
+    CMAKE_ARGS
+        ${common_cmake_args}
+        -DPORTABLE=ON
+        -DWITH_SNAPPY=ON
+        -DWITH_ZSTD=ON
+        -DWITH_ZLIB=ON
+        -DWITH_JEMALLOC=OFF
+        -DWITH_GFLAGS=OFF
+        -DWITH_TESTS=OFF
+        -DWITH_TOOLS=OFF
+        -DFAIL_ON_WARNINGS=OFF
+        -DCMAKE_BUILD_TYPE=Release
+#-DCMAKE_CXX_FLAGS:STRING=${ROCKSDB_CXX_FLAGS}
+    PATCH_COMMAND patch CMakeLists.txt ${CMAKE_SOURCE_DIR}/patches/rocksdb-5.15.10.patch
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND make -s -j${BUILDING_JOBS_NUM} VERBOSE=1
+    INSTALL_COMMAND make -s install -j${BUILDING_JOBS_NUM}
+    LOG_CONFIGURE TRUE
+    LOG_BUILD TRUE
+    LOG_INSTALL TRUE
+)
+
+ExternalProject_Add_Step(${name} clean
+    EXCLUDE_FROM_MAIN TRUE
+    ALWAYS TRUE
+    DEPENDEES configure
+    COMMAND make clean -j
+    COMMAND rm -f ${BUILD_INFO_DIR}/${name}-build
+    WORKING_DIRECTORY ${source_dir}
+)
+
+ExternalProject_Add_StepTargets(${name} clean)
