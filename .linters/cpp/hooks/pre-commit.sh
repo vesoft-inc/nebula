0bb5657ccf01a0cd06d87946afc0988485b82f85
William Chen <13495049+CPWstatic@users.noreply.github.com>
2020-02-11 20:10:02 +0800

    Add linter.

---
+++ .linters/cpp/hooks/pre-commit.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Copyright (c) 2018 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License,
+# attached with Common Clause Condition 1.0, found in the LICENSES directory.
+
+CPPLINT=`dirname $0`/../../.linters/cpp/cpplint.py
+
+if [ $# -eq 0 ];then
+    # Since cpplint.py could only apply on our working tree,
+    # so we forbid committing with unstaged changes present.
+    # Otherwise, lints can be bypassed via changing without commit.
+    if ! git diff-files --exit-code --quiet
+    then
+        echo "You have unstaged changes, please stage or stash them first."
+        exit 1
+    fi
+    CHECK_FILES=$(git diff --name-only --diff-filter=ACMRTUXB HEAD | egrep '.*\.cpp$|.*\.h$|.*\.inl$' | grep -v 'com_vesoft_client_NativeClient.h' | grep -v 'com_vesoft_nebula_NebulaCodec.h')
+else
+    CHECK_FILES=$(find $@ -not \( -path src/CMakeFiles -prune \) \
+                          -not \( -path src/interface/gen-cpp2 -prune \) \
+                          -name "*.[h]" -o -name "*.cpp" -o -name '*.inl' \
+                          | grep -v 'GraphScanner.*' | grep -v 'GraphParser.*' \
+                          | grep -v 'com_vesoft_client_NativeClient.h' \
+                          | grep -v 'com_vesoft_nebula_NebulaCodec.h')
+fi
+
+# No changes on interested files
+if [[ -z $CHECK_FILES ]]
+then
+    exit 0
+fi
+
+echo "Performing C++ linters..."
+
+CPPLINT_EXTENS=cpp,h,inl
+CPPLINT_FILTER=-whitespace/indent,-build/include_what_you_use,-readability/todo,-build/include,-build/header_guard,-runtime/references,-build/c++11
+
+python $CPPLINT --quiet --extensions=$CPPLINT_EXTENS \
+                --filter=$CPPLINT_FILTER --linelength=100 $CHECK_FILES 2>&1
+
+result=$?
+if [ $result -eq 0 ]
+then
+    exit 0
+else
+    echo "cpplint code style check failed, please fix and recommit."
+    exit 1
+fi
