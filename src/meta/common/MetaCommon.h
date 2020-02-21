e1ae1b3d967da7e970b90aa0e3f6aabc02e1f6d1
Doodle <13706157+critical27@users.noreply.github.com>
2020-02-21 11:21:46 +0800

    Return leader changed when meta has multiple replica (#1794)
    * debug
    
    * MetaCommon::to & handleErrorCode
    
    * Modify Balancer
    
    * Modify doSyncPut
    
    Co-authored-by: dangleptr <37216992+dangleptr@users.noreply.github.com>

--- src/meta/common/MetaCommon.h
+++ src/meta/common/MetaCommon.h
@@ -8,6 +8,9 @@
 #define META_METACOMMON_H_
 
 #include "base/Base.h"
+#include "base/Status.h"
+#include "kvstore/KVStore.h"
+#include "interface/gen-cpp2/meta_types.h"
 
 namespace nebula {
 namespace meta {
@@ -22,6 +25,35 @@ public:
             return true;
         }
         return false;
+    }
+
+    static cpp2::ErrorCode to(kvstore::ResultCode code) {
+        switch (code) {
+        case kvstore::ResultCode::SUCCEEDED:
+            return cpp2::ErrorCode::SUCCEEDED;
+        case kvstore::ResultCode::ERR_KEY_NOT_FOUND:
+            return cpp2::ErrorCode::E_NOT_FOUND;
+        case kvstore::ResultCode::ERR_LEADER_CHANGED:
+            return cpp2::ErrorCode::E_LEADER_CHANGED;
+        case kvstore::ResultCode::ERR_CHECKPOINT_ERROR:
+            return cpp2::ErrorCode::E_SNAPSHOT_FAILURE;
+        default:
+            return cpp2::ErrorCode::E_UNKNOWN;
+        }
+    }
+
+    static cpp2::ErrorCode to(const Status& status) {
+        switch (status.code()) {
+        case Status::kOk:
+            return cpp2::ErrorCode::SUCCEEDED;
+        case Status::kSpaceNotFound:
+        case Status::kHostNotFound:
+        case Status::kTagNotFound:
+        case Status::kUserNotFound:
+            return cpp2::ErrorCode::E_NOT_FOUND;
+        default:
+            return cpp2::ErrorCode::E_UNKNOWN;
+        }
     }
 };
 
