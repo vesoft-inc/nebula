10b2e455ba150dd6908a3d95a116834157b7cdda
yaphet <darion.wang@vesoft.com>
2020-02-21 15:22:26 +0800

    update checkstyle (#1793)

--- src/jni/java/src/main/java/com/vesoft/nebula/NebulaCodecResourceLoader.java
+++ src/jni/java/src/main/java/com/vesoft/nebula/NebulaCodecResourceLoader.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
