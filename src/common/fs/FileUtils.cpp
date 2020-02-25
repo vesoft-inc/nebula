7c9d7232b61ceec5788e72349a5b8d468a38194f
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-02-25 17:16:25 +0800

    fix makedir failed when dir is SYM_LINK (#1818)
    Co-authored-by: dutor <440396+dutor@users.noreply.github.com>

--- src/common/fs/FileUtils.cpp
+++ src/common/fs/FileUtils.cpp
@@ -330,7 +330,7 @@ bool FileUtils::makeDir(const std::string& dir, uint32_t mode) {
         return false;
     }
     FileType type = fileType(dir.c_str());
-    if (type == FileType::DIRECTORY) {
+    if (type == FileType::DIRECTORY || type == FileType::SYM_LINK) {
         // The directory already exists
         return true;
     } else if (type != FileType::NOTEXIST) {
