fcbf490f56fadaae2b92be39a241dffa7cb89284
Sherman <sherman.ye@vesoft.com>
2020-02-27 11:38:07 +0800

    This PR fixes the repo dependency issue (#10)
    The cmake built-in ExternalProject_add() configures and builds the project during the build time, so if the project depends on the targets in the external project, the run of cmake will fail.
    
    This PR creates a macro to pull the external project and to configure (executes cmake) it during the configuration time, so it solves the target dependency issues

---
+++ cmake/AddDependentProject.cmake
@@ -0,0 +1,71 @@
+# Copyright (c) 2020 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License,
+# attached with Common Clause Condition 1.0, found in the LICENSES directory.
+#
+
+macro(add_dependent_project base name repo tag)
+
+# Clone or update the repo
+if(EXISTS ${base}/${name}/.git)
+    message(STATUS "Updating from the repo \"" ${repo} "\"")
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} pull --depth=1
+        WORKING_DIRECTORY ${base}/${name}
+        RESULT_VARIABLE clone_result
+    )
+else()
+    message(STATUS "Cloning from the repo \"" ${repo} "\"")
+    execute_process(
+        COMMAND
+            ${GIT_EXECUTABLE} clone --depth 1 --progress --branch ${tag} ${repo} ${name}
+        WORKING_DIRECTORY ${base}
+        RESULT_VARIABLE clone_result
+    )
+endif()
+
+if(NOT ${clone_result} EQUAL 0)
+    message(
+        FATAL_ERROR
+        "Cannot clone the repo from \""
+        ${repo}
+        "\" (branch \""
+        ${tag}
+        "\"): \""
+        ${clone_result}
+        "\"")
+else()
+    message(STATUS "Updated the repo from \"" ${repo} "\" (branch \"" ${tag} "\")")
+endif()
+
+# Configure the repo
+execute_process(
+    COMMAND
+        ${CMAKE_COMMAND}
+        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DNEBULA_THIRDPARTY_ROOT=${NEBULA_THIRDPARTY_ROOT}
+        -DNEBULA_OTHER_ROOT=${NEBULA_OTHER_ROOT}
+        -DENABLE_JEMALLOC=${ENABLE_JEMALLOC}
+        -DENABLE_NATIVE=${ENABLE_NATIVE}
+        -DENABLE_TESTING=false
+        -DENABLE_CCACHE=${ENABLE_CCACHE}
+        -DENABLE_ASAN=${ENABLE_ASAN}
+        -DENABLE_UBSAN=${ENABLE_UBSAN}
+        .
+    WORKING_DIRECTORY ${base}/${name}
+    RESULT_VARIABLE cmake_result
+)
+if(NOT ${cmake_result} EQUAL 0)
+    message(FATAL_ERROR "Failed to configure the dependent project \"" ${name} "\"")
+endif()
+
+# Add a custom target to build the project
+add_custom_target(
+    ${name}_project ALL
+    COMMAND make -j
+    WORKING_DIRECTORY ${base}/${name}
+)
+
+endmacro()
