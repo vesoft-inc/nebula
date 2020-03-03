ecc11c2f8f15cfb317b50389b85bbd96114562f6
Doodle <13706157+critical27@users.noreply.github.com>
2020-03-03 18:33:23 +0800

    support return partial result in get of kv interfaces (#1840)
     * Add a return_partly in get, when it is set to true, we can get the keys which exist, and ignore those keys which are not existed.
     * To implement this, add a tryGet method in KVStore, which return each status of key.
     * Remove async thread in GetProcessor

--- src/kvstore/Common.h
+++ src/kvstore/Common.h
@@ -28,6 +28,7 @@ enum ResultCode {
     ERR_WRITE_BLOCK_ERROR   = -10,
     ERR_TAG_NOT_FOUND       = -11,
     ERR_EDGE_NOT_FOUND      = -12,
+    ERR_PARTIAL_RESULT      = -99,
     ERR_UNKNOWN             = -100,
 };
 
