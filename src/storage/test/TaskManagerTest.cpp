47877a293dd4c4662dee03a21080cbdcadcae456
lionel.liu <lionel.liu@vesoft.com>
2020-03-03 23:00:31 +0800

    add task manager dummy ut

---
+++ src/storage/test/TaskManagerTest.cpp
@@ -0,0 +1,31 @@
+/* Copyright (c) 2020 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+#include "base/Base.h"
+#include <gtest/gtest.h>
+#include <rocksdb/db.h>
+#include "fs/TempDir.h"
+#include "storage/test/TestUtils.h"
+#include "storage/admin/AdminTaskManager.h"
+
+namespace nebula {
+namespace storage {
+
+TEST(TaskManagerTest, ctor) {
+    auto& taskMgr = AdminTaskManager::instance();
+    taskMgr.init();
+    taskMgr.shutdown();
+}
+
+}  // namespace storage
+}  // namespace nebula
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    folly::init(&argc, &argv, true);
+    google::SetStderrLogging(google::INFO);
+    return RUN_ALL_TESTS();
+}
