f89234cd7676b15a19d78a464ad9c5c043adc82e
dangleptr <37216992+dangleptr@users.noreply.github.com>
2020-03-06 12:38:34 +0800

    Fix update logic after store properties on in-edges (#1724)
    Co-authored-by: heng <heng.chen@vesoft.com>

--- src/graph/UpdateEdgeExecutor.h
+++ src/graph/UpdateEdgeExecutor.h
@@ -49,10 +49,10 @@ private:
 
     std::vector<std::string> getReturnColumns();
 
-    void insertReverselyEdge(storage::cpp2::UpdateResponse &&rpcResp);
+    void updateEdge(bool reversely);
 
     // All required data have arrived, finish the execution.
-    void finishExecution(storage::cpp2::UpdateResponse &&rpcResp);
+    void toResponse(storage::cpp2::UpdateResponse &&rpcResp);
 
 private:
     UpdateEdgeSentence                         *sentence_{nullptr};
