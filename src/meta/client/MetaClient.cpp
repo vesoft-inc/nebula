70a06011b5621710229db83542c5b61583421bd5
liuyu85cn <52276794+liuyu85cn@users.noreply.github.com>
2020-03-31 21:14:43 +0800

    fix bug 1990/1991 (#2007)
    * 1. meta client sub mit job only to leader 2. sort jobs in desc id order
    
    * now show jobs return in desc order, adjust UT
    
    Co-authored-by: dangleptr <37216992+dangleptr@users.noreply.github.com>

--- src/meta/client/MetaClient.cpp
+++ src/meta/client/MetaClient.cpp
@@ -697,7 +697,7 @@ MetaClient::submitJob(cpp2::AdminJobOp op, std::vector<std::string> paras) {
                     return client->future_runAdminJob(request);
                 }, [] (cpp2::AdminJobResp&& resp) -> decltype(auto) {
                     return resp.get_result();
-                }, std::move(promise));
+                }, std::move(promise), true);
     return future;
 }
 
