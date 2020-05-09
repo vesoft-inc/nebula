39a52f3dfe5cee4cc7bd6ca30e8ccc1d04c507bd
William Chen <13495049+CPWstatic@users.noreply.github.com>
2020-05-09 10:04:41 +0800

    Address comment.

--- src/graph/GroupByExecutor.cpp
+++ src/graph/GroupByExecutor.cpp
@@ -346,9 +346,7 @@ std::vector<std::string> GroupByExecutor::getResultColumnNames() const {
 
 
 Status GroupByExecutor::generateOutputSchema() {
-    if (rows_.size() == 0) {
-        return Status::Error("Empty inputs.");
-    }
+    CHECK_GE(rows_.size(), 0);
     auto& row = rows_[0];
     using nebula::cpp2::SupportedType;
     if (resultSchema_ == nullptr) {
