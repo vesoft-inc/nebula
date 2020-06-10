26329823283b4e8896d9530295939d833beccbdc
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2020-06-10 11:52:07 +0800

    Revert the 2087 workaround. (#2168)

--- src/graph/GoExecutor.h
+++ src/graph/GoExecutor.h
@@ -231,8 +231,6 @@ private:
     std::unique_ptr<cpp2::ExecutionResponse>    resp_;
     // Record the data of response in GO step
     std::vector<RpcResponse>                    records_;
-    // TODO(shylock) Join lose the data with duplicate input(VID) map
-    bool                                        uniqueStart_{false};  // #2087 Workaround
     // The name of Tag or Edge, index of prop in data
     using SchemaPropIndex = std::unordered_map<std::pair<std::string, std::string>, int64_t>;
 };
