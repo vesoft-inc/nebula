97e3de4bcd99b05a80917deab40e1997151c83b6
heng <heng.chen@vesoft.com>
2020-08-03 18:19:04 +0800

    Fix the wrong usage for LRUCache

--- src/storage/mutate/AddVerticesProcessor.cpp
+++ src/storage/mutate/AddVerticesProcessor.cpp
@@ -30,78 +30,66 @@ void AddVerticesProcessor::process(const cpp2::AddVerticesRequest& req) {
     }
 
     CHECK_NOTNULL(kvstore_);
-    if (indexes_.empty()) {
-        std::for_each(partVertices.begin(), partVertices.end(), [&](auto& pv) {
-            auto partId = pv.first;
-            const auto& vertices = pv.second;
-            std::vector<kvstore::KV> data;
-            std::for_each(vertices.begin(), vertices.end(), [&](auto& v) {
-                const auto& tags = v.get_tags();
-                std::for_each(tags.begin(), tags.end(), [&](auto& tag) {
-                    VLOG(3) << "PartitionID: " << partId << ", VertexID: " << v.get_id()
-                            << ", TagID: " << tag.get_tag_id() << ", TagVersion: " << version;
-                    auto key = NebulaKeyUtils::vertexKey(partId, v.get_id(),
-                                                         tag.get_tag_id(), version);
-                    data.emplace_back(std::move(key), std::move(tag.get_props()));
-                    if (FLAGS_enable_vertex_cache && vertexCache_ != nullptr) {
-                        vertexCache_->evict(std::make_pair(v.get_id(), tag.get_tag_id()));
-                        VLOG(3) << "Evict cache for vId " << v.get_id()
-                                << ", tagId " << tag.get_tag_id();
-                    }
-                });
+    std::for_each(partVertices.begin(), partVertices.end(), [&](auto& pv) {
+        std::vector<kvstore::KV> data;
+        data.reserve(128);
+        std::vector<std::tuple<VertexID, TagID, std::string>> cacheData;
+        if (FLAGS_enable_vertex_cache && vertexCache_ != nullptr) {
+            cacheData.reserve(128);
+        }
+        auto partId = pv.first;
+        const auto& vertices = pv.second;
+        std::for_each(vertices.begin(), vertices.end(), [&](auto& v) {
+            const auto& tags = v.get_tags();
+            std::for_each(tags.begin(), tags.end(), [&](auto& tag) {
+                VLOG(3) << "PartitionID: " << partId << ", VertexID: " << v.get_id()
+                        << ", TagID: " << tag.get_tag_id() << ", TagVersion: " << version;
+                auto key = NebulaKeyUtils::vertexKey(partId, v.get_id(),
+                                                     tag.get_tag_id(), version);
+                if (FLAGS_enable_vertex_cache && vertexCache_ != nullptr) {
+                    cacheData.emplace_back(v.get_id(), tag.get_tag_id(), tag.get_props());
+                }
+                data.emplace_back(std::move(key), std::move(tag.get_props()));
             });
-            doPut(spaceId_, partId, std::move(data));
         });
-    } else {
-        std::for_each(partVertices.begin(), partVertices.end(), [&](auto &pv) {
-            auto partId = pv.first;
-            auto atomic = [version, partId, vertices = std::move(pv.second), this]()
-                          -> folly::Optional<std::string> {
-                return addVertices(version, partId, vertices);
+
+        auto callback = [partId,
+                         this,
+                         cacheData = std::move(cacheData)] (kvstore::ResultCode code) mutable {
+            if (FLAGS_enable_vertex_cache
+                && vertexCache_ != nullptr
+                && code == kvstore::ResultCode::SUCCEEDED) {
+                for (auto&& tup : cacheData) {
+                    vertexCache_->insert(std::make_pair(std::get<0>(tup),
+                                                        std::get<1>(tup)),
+                                         std::move(std::get<2>(tup)));
+                }
+            }
+            handleAsync(spaceId_, partId, code);
+        };
+        if (indexes_.empty()) {
+            this->kvstore_->asyncMultiPut(spaceId_,
+                                          partId,
+                                          std::move(data),
+                                          std::move(callback));
+        } else {
+            auto atomicOp = [partId, data = std::move(data), this]() mutable
+                                            -> folly::Optional<std::string> {
+                return addVerticesWithIndex(partId, std::move(data));
             };
-            auto callback = [partId, this](kvstore::ResultCode code) {
-                handleAsync(spaceId_, partId, code);
-            };
-            this->kvstore_->asyncAtomicOp(spaceId_, partId, atomic, callback);
-        });
-    }
+
+            this->kvstore_->asyncAtomicOp(spaceId_,
+                                          partId,
+                                          std::move(atomicOp),
+                                          std::move(callback));
+        }
+    });
 }
 
-std::string AddVerticesProcessor::addVertices(int64_t version, PartitionID partId,
-                                              const std::vector<cpp2::Vertex>& vertices) {
+std::string AddVerticesProcessor::addVerticesWithIndex(PartitionID partId,
+                                                       std::vector<kvstore::KV>&& data) {
     std::unique_ptr<kvstore::BatchHolder> batchHolder = std::make_unique<kvstore::BatchHolder>();
-    /*
-     * Define the map newIndexes to avoid inserting duplicate vertex.
-     * This map means :
-     * map<vertex_unique_key, prop_value> ,
-     * -- vertex_unique_key is only used as the unique key , for example:
-     * insert below vertices in the same request:
-     *     kv(part1_vid1_tag1 , v1)
-     *     kv(part1_vid1_tag1 , v2)
-     *     kv(part1_vid1_tag1 , v3)
-     *     kv(part1_vid1_tag1 , v4)
-     *
-     * Ultimately, kv(part1_vid1_tag1 , v4) . It's just what I need.
-     */
-    std::map<std::string, std::string> newVertices;
-    std::for_each(vertices.begin(), vertices.end(), [&](auto& v) {
-        auto vId = v.get_id();
-        const auto& tags = v.get_tags();
-        std::for_each(tags.begin(), tags.end(), [&](auto& tag) {
-            auto tagId = tag.get_tag_id();
-            auto prop = tag.get_props();
-            VLOG(3) << "PartitionID: " << partId << ", VertexID: " << vId
-                    << ", TagID: " << tagId << ", TagVersion: " << version;
-            auto key = NebulaKeyUtils::vertexKey(partId, vId, tagId, version);
-            newVertices[key] = std::move(prop);
-            if (FLAGS_enable_vertex_cache && this->vertexCache_ != nullptr) {
-                this->vertexCache_->evict(std::make_pair(vId, tagId));
-                VLOG(3) << "Evict cache for vId " << vId << ", tagId " << tagId;
-            }
-        });
-    });
-
-    for (auto& v : newVertices) {
+    for (auto& v : data) {
         std::string val;
         RowReader nReader = RowReader::getEmptyRowReader();
         auto tagId = NebulaKeyUtils::getTagId(v.first);
@@ -150,9 +138,7 @@ std::string AddVerticesProcessor::addVertices(int64_t version, PartitionID partI
         /*
          * step 3 , Insert new vertex data
          */
-        auto key = v.first;
-        auto prop = v.second;
-        batchHolder->put(std::move(key), std::move(prop));
+        batchHolder->put(std::move(v.first), std::move(v.second));
     }
     return encodeBatchValue(batchHolder->getBatch());
 }
