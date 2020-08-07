2242b5465dbec050888b2c67bde2170597c62ee8
Yee <2520865+yixinglu@users.noreply.github.com>
2020-08-07 11:35:13 +0800

    Replace drone ci with github action (#191)
    * Replace drone ci with github action
    
    * Fix cache dir env
    
    try to debug action
    
    * Fix pat outputs of step
    
    * Rename ENABLE_UPDATE_EXTERNAL_PROJ
    
    * Fix compile error

--- cmake/AddDependentProject.cmake
+++ cmake/AddDependentProject.cmake
@@ -16,12 +16,14 @@ macro(add_dependent_project)
     set(CLONE_DIR ${DEP_PROJ_BASE}/${DEP_PROJ_NAME})
     # Clone or update the repo
     if(EXISTS ${CLONE_DIR}/.git)
-        message(STATUS "Updating from the repo \"" ${DEP_PROJ_REPO} "\"")
-        execute_process(
-            COMMAND ${GIT_EXECUTABLE} pull --depth=1
-            WORKING_DIRECTORY ${CLONE_DIR}
-            RESULT_VARIABLE clone_result
-        )
+        message(STATUS "Updating from the repo \"" ${DEP_PROJ_REPO} "\", update option: ${ENABLE_UPDATE_EXTERNAL_PROJ}")
+        if (ENABLE_UPDATE_EXTERNAL_PROJ)
+            execute_process(
+                COMMAND ${GIT_EXECUTABLE} pull --depth=1
+                WORKING_DIRECTORY ${CLONE_DIR}
+                RESULT_VARIABLE clone_result
+            )
+        endif()
     else()
         message(STATUS "Cloning from the repo \"" ${DEP_PROJ_REPO} "\"")
         execute_process(
