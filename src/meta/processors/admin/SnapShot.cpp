a3d53480c838578db0df57248cf84eebf8fbd210
bright-starry-sky <56461666+bright-starry-sky@users.noreply.github.com>
2020-08-10 12:13:03 +0800

    Fixed bug of snapshot from chaos test. (#2281)
    * fixed checkpoint bug
    
    * Addressed comments

--- src/meta/processors/admin/SnapShot.cpp
+++ src/meta/processors/admin/SnapShot.cpp
@@ -21,26 +21,20 @@ cpp2::ErrorCode Snapshot::createSnapshot(const std::string& name) {
                    << static_cast<int32_t>(ret);
         return cpp2::ErrorCode::E_STORE_FAILURE;
     }
-    for (auto& space : spaces) {
-        auto status = client_->createSnapshot(space, name).get();
-        if (!status.ok()) {
-            return cpp2::ErrorCode::E_RPC_FAILURE;
+    auto hosts = ActiveHostsMan::getActiveHosts(kv_);
+    for (const auto& host : hosts) {
+        for (auto& space : spaces) {
+            auto status = client_->createSnapshot(space, name, host).get();
+            if (!status.ok()) {
+                return cpp2::ErrorCode::E_RPC_FAILURE;
+            }
         }
     }
     return cpp2::ErrorCode::SUCCEEDED;
 }
 
 cpp2::ErrorCode Snapshot::dropSnapshot(const std::string& name,
-                                       const std::vector<HostAddr> hosts) {
-    // The drop checkpoint will be skip if original host has been lost.
-    auto activeHosts = ActiveHostsMan::getActiveHosts(kv_);
-    std::vector<HostAddr> realHosts;
-    for (auto& host : hosts) {
-        if (std::find(activeHosts.begin(), activeHosts.end(), host) != activeHosts.end()) {
-            realHosts.emplace_back(host);
-        }
-    }
-
+                                       const std::vector<HostAddr>& hosts) {
     std::vector<GraphSpaceID> spaces;
     kvstore::ResultCode ret = kvstore::ResultCode::SUCCEEDED;
     if (!getAllSpaces(spaces, ret)) {
@@ -48,11 +42,21 @@ cpp2::ErrorCode Snapshot::dropSnapshot(const std::string& name,
                    << static_cast<int32_t>(ret);
         return cpp2::ErrorCode::E_STORE_FAILURE;
     }
-    for (auto& space : spaces) {
-        auto status = client_->dropSnapshot(space, name, realHosts).get();
-
-        if (!status.ok()) {
-            return cpp2::ErrorCode::E_RPC_FAILURE;
+    // The drop checkpoint will be skip if original host has been lost.
+    auto activeHosts = ActiveHostsMan::getActiveHosts(kv_);
+    for (auto& host : hosts) {
+        if (std::find(activeHosts.begin(), activeHosts.end(), host) != activeHosts.end()) {
+            std::for_each(spaces.begin(), spaces.end(), [name, host, this](auto& space) {
+                auto status = client_->dropSnapshot(space, name, host).get();
+                if (!status.ok()) {
+                    auto msg = "failed drop checkpoint : \"%s\". on host %s. error %s";
+                    auto error = folly::stringPrintf(msg,
+                                                     name.c_str(),
+                                                     network::NetworkUtils::toHosts({host}).c_str(),
+                                                     status.toString().c_str());
+                    LOG(ERROR) << error;
+                }
+            });
         }
     }
     return cpp2::ErrorCode::SUCCEEDED;
@@ -84,71 +88,17 @@ cpp2::ErrorCode Snapshot::blockingWrites(storage::cpp2::EngineSignType sign) {
                    << static_cast<int32_t>(ret);
         return cpp2::ErrorCode::E_STORE_FAILURE;
     }
-    for (auto& space : spaces) {
-        auto status = client_->blockingWrites(space, sign).get();
-
-        if (!status.ok()) {
-            return cpp2::ErrorCode::E_BLOCK_WRITE_FAILURE;
-        }
+    auto hosts = ActiveHostsMan::getActiveHosts(kv_);
+    for (const auto& host : hosts) {
+        std::for_each(spaces.begin(), spaces.end(), [host, sign, this](auto& space) {
+            auto status = client_->blockingWrites(space, sign, host).get();
+            if (!status.ok()) {
+                LOG(ERROR) << " Send blocking sign error on host : "
+                           << network::NetworkUtils::toHosts({host});
+            }
+        });
     }
     return cpp2::ErrorCode::SUCCEEDED;
-}
-
-std::unordered_map<HostAddr, std::vector<PartitionID>>
-Snapshot::getLeaderParts(HostLeaderMap *hostLeaderMap, GraphSpaceID spaceId) {
-    std::unordered_map<PartitionID, std::vector<HostAddr>> peersMap;
-    std::unordered_map<HostAddr, std::vector<PartitionID>> leaderHostParts;
-
-    auto key = MetaServiceUtils::spaceKey(spaceId);
-    std::string value;
-    auto code = kv_->get(kDefaultSpaceId, kDefaultPartId, key, &value);
-    if (code != kvstore::ResultCode::SUCCEEDED) {
-        LOG(ERROR) << "Access kvstore failed, spaceId " << spaceId;
-        return leaderHostParts;
-    }
-    auto properties = MetaServiceUtils::parseSpace(value);
-
-    auto leaderParts = 0;
-    {
-        // store peers of all paritions in peerMap
-        folly::SharedMutex::ReadHolder rHolder(LockUtils::spaceLock());
-        auto prefix = MetaServiceUtils::partPrefix(spaceId);
-        std::unique_ptr<kvstore::KVIterator> iter;
-        auto ret = kv_->prefix(kDefaultSpaceId, kDefaultPartId, prefix, &iter);
-        if (ret != kvstore::ResultCode::SUCCEEDED) {
-            LOG(ERROR) << "Access kvstore failed, spaceId " << spaceId;
-            return leaderHostParts;
-        }
-        // Check the current cluster status, If the number of replica is
-        // less than half of the total, return null list.
-        while (iter->valid()) {
-            auto k = iter->key();
-            PartitionID partId;
-            memcpy(&partId, k.data() + prefix.size(), sizeof(PartitionID));
-            auto thriftPeers = MetaServiceUtils::parsePartVal(iter->val());
-            std::vector<HostAddr> peers;
-            peers.resize(thriftPeers.size());
-            if (peers.size() < static_cast<size_t>(properties.replica_factor)/2) {
-                LOG(ERROR) << "Replica part must be greater than or equal to half "
-                              "the total of parts. Space : " << spaceId << " Part : " << partId;
-                return leaderHostParts;
-            }
-            ++leaderParts;
-            iter->next();
-        }
-    }
-
-    if (leaderParts < properties.partition_num) {
-        LOG(ERROR) << "Leader part lost, expected part : " << properties.partition_num
-                   << "actual : " << leaderParts;
-        return leaderHostParts;
-    }
-
-    for (const auto& host : *hostLeaderMap) {
-        leaderHostParts[host.first] = std::move((*hostLeaderMap)[host.first][spaceId]);
-    }
-
-    return leaderHostParts;
 }
 
 }  // namespace meta
