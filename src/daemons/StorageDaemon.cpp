1d2a3469a41cf26e44b1ac8d7c546600f5e93184
Doodle <13706157+critical27@users.noreply.github.com>
2020-08-11 15:51:30 +0800

    fix multi daemon can't start on same node (#2289)
    * fix multi daemon can't start on same node
    
    * address comments

--- src/daemons/StorageDaemon.cpp
+++ src/daemons/StorageDaemon.cpp
@@ -36,6 +36,7 @@ int main(int argc, char *argv[]) {
     // Detect if the server has already been started
     // Check pid before glog init, in case of user may start daemon twice
     // the 2nd will make the 1st failed to output log anymore
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
     auto pidPath = FLAGS_pid_file;
     auto status = ProcessUtils::isPidAvailable(pidPath);
     if (!status.ok()) {
