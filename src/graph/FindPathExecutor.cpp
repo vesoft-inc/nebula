89812b9079354b45055f02bc9d02f6f0580a17ed
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-08-17 12:22:20 +0800

    Move prepare handle to execute

--- src/graph/FindPathExecutor.cpp
+++ src/graph/FindPathExecutor.cpp
@@ -16,6 +16,10 @@ FindPathExecutor::FindPathExecutor(Sentence *sentence, ExecutionContext *exct)
 }
 
 Status FindPathExecutor::prepare() {
+    return Status::OK();
+}
+
+Status FindPathExecutor::prepareClauses() {
     spaceId_ = ectx()->rctx()->session()->space();
     Status status;
     expCtx_ = std::make_unique<ExpressionContext>();
@@ -69,6 +73,11 @@ Status FindPathExecutor::beforeExecute() {
         status = checkIfGraphSpaceChosen();
         if (!status.ok()) {
             break;;
+        }
+
+        status = prepareClauses();
+        if (!status.ok()) {
+            break;
         }
 
         status = prepareOver();
