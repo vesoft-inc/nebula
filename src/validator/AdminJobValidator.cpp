8dfc71350c7ce9ca541e4ea3d72f2f5672b0f6a7
panda-sheep <59197347+panda-sheep@users.noreply.github.com>
2020-12-25 09:50:58 +0800

    fix pr #494 (#516)
    Co-authored-by: cpw <13495049+CPWstatic@users.noreply.github.com>

--- src/validator/AdminJobValidator.cpp
+++ src/validator/AdminJobValidator.cpp
@@ -13,31 +13,34 @@ namespace graph {
 Status AdminJobValidator::validateImpl() {
     if (sentence_->getOp() == meta::cpp2::AdminJobOp::ADD) {
         auto cmd = sentence_->getCmd();
-        if (cmd == meta::cpp2::AdminCmd::REBUILD_TAG_INDEX ||
-            cmd == meta::cpp2::AdminCmd::REBUILD_EDGE_INDEX) {
+        if (requireSpace()) {
             const auto &spaceInfo = qctx()->rctx()->session()->space();
             auto spaceId = spaceInfo.id;
             const auto &spaceName = spaceInfo.name;
             sentence_->addPara(spaceName);
-            DCHECK_EQ(sentence_->getParas().size(), 2);
 
-            const auto &indexName = sentence_->getParas()[0];
-            auto ret = cmd == meta::cpp2::AdminCmd::REBUILD_TAG_INDEX
-                           ? qctx()->getMetaClient()->getTagIndexesFromCache(spaceId)
-                           : qctx()->getMetaClient()->getEdgeIndexesFromCache(spaceId);
-            if (!ret.ok()) {
-                return Status::SemanticError(
-                    "Index %s not found in space %s", indexName.c_str(), spaceName.c_str());
-            }
-            auto indexes = std::move(ret).value();
-            auto it = std::find_if(indexes.begin(),
-                                   indexes.end(),
-                                   [&indexName](std::shared_ptr<meta::cpp2::IndexItem>& item) {
-                                       return item->get_index_name() == indexName;
-                                   });
-            if (it == indexes.end()) {
-                return Status::SemanticError(
-                    "Index %s not found in space %s", indexName.c_str(), spaceName.c_str());
+            if (cmd == meta::cpp2::AdminCmd::REBUILD_TAG_INDEX ||
+                cmd == meta::cpp2::AdminCmd::REBUILD_EDGE_INDEX) {
+                DCHECK_EQ(sentence_->getParas().size(), 2);
+
+                const auto &indexName = sentence_->getParas()[0];
+                auto ret = cmd == meta::cpp2::AdminCmd::REBUILD_TAG_INDEX
+                               ? qctx()->getMetaClient()->getTagIndexesFromCache(spaceId)
+                               : qctx()->getMetaClient()->getEdgeIndexesFromCache(spaceId);
+                if (!ret.ok()) {
+                    return Status::SemanticError(
+                        "Index %s not found in space %s", indexName.c_str(), spaceName.c_str());
+                }
+                auto indexes = std::move(ret).value();
+                auto it = std::find_if(indexes.begin(),
+                                       indexes.end(),
+                                       [&indexName](std::shared_ptr<meta::cpp2::IndexItem>& item) {
+                                           return item->get_index_name() == indexName;
+                                       });
+                if (it == indexes.end()) {
+                    return Status::SemanticError(
+                        "Index %s not found in space %s", indexName.c_str(), spaceName.c_str());
+                }
             }
         }
     }
--- src/validator/AdminJobValidator.h
+++ src/validator/AdminJobValidator.h
@@ -35,10 +35,10 @@ private:
                 switch (sentence_->getCmd()) {
                     case meta::cpp2::AdminCmd::REBUILD_TAG_INDEX:
                     case meta::cpp2::AdminCmd::REBUILD_EDGE_INDEX:
+                    case meta::cpp2::AdminCmd::STATS:
                         return true;
                     case meta::cpp2::AdminCmd::COMPACT:
                     case meta::cpp2::AdminCmd::FLUSH:
-                    case meta::cpp2::AdminCmd::STATS:
                     case meta::cpp2::AdminCmd::DATA_BALANCE:
                     case meta::cpp2::AdminCmd::UNKNOWN:
                         return false;
