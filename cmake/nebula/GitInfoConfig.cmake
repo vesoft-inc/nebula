85973e9b50c76101473183359093f2926b16a010
Yee <2520865+yixinglu@users.noreply.github.com>
2020-12-29 22:29:22 +0800

    Get git info sha from outside (#372)
    Co-authored-by: dutor <440396+dutor@users.noreply.github.com>

--- cmake/nebula/GitInfoConfig.cmake
+++ cmake/nebula/GitInfoConfig.cmake
@@ -1,12 +1,3 @@
-find_package(Git)
-if (GIT_FOUND AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git")
-    execute_process(
-        COMMAND
-        ${GIT_EXECUTABLE} rev-parse --short HEAD
-        OUTPUT_VARIABLE GIT_INFO_SHA
-    )
-endif()
-
 if (GIT_INFO_SHA)
     string(REGEX REPLACE "[^0-9a-f]+" "" GIT_INFO_SHA "${GIT_INFO_SHA}")
     add_definitions(-DGIT_INFO_SHA=${GIT_INFO_SHA})
