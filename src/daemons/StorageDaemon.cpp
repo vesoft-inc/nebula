79cbc0d5cff2cb158f692cd732eae4cad01afa86
lionel.liu@vesoft.com <lionel.liu@vesoft.com>
2020-12-29 09:56:02 +0800

    set storage version at the beginning of storage daemon (#265)
    Co-authored-by: Doodle <13706157+critical27@users.noreply.github.com>

--- src/daemons/StorageDaemon.cpp
+++ src/daemons/StorageDaemon.cpp
@@ -36,6 +36,7 @@ static Status setupSignalHandler();
 std::unique_ptr<nebula::storage::StorageServer> gStorageServer;
 
 int main(int argc, char *argv[]) {
+    google::SetVersionString(nebula::versionString());
     // Detect if the server has already been started
     // Check pid before glog init, in case of user may start daemon twice
     // the 2nd will make the 1st failed to output log anymore
@@ -47,7 +48,6 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    google::SetVersionString(nebula::versionString());
     folly::init(&argc, &argv, true);
     if (FLAGS_daemonize) {
         google::SetStderrLogging(google::FATAL);
