55f10f6a4817a0301b7d483efae89e161cf9de09
jievince <38901892+jievince@users.noreply.github.com>
2021-03-05 13:12:05 +0800

    build nebula-tools docker image

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -7,6 +7,6 @@
 
 PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"/../
 
-for suffix in graphd metad storaged console; do
+for suffix in graphd metad storaged console tools; do
     docker build -t vesoft/nebula-$suffix:latest -f $PROJECT_DIR/docker/Dockerfile.$suffix $PROJECT_DIR
 done
