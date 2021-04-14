a9aa0aeb265536f007ddf16b4a3a7520d701c406
lionel.liu@vesoft.com <lionel.liu@vesoft.com>
2021-04-14 16:39:46 +0800

    Revert "Send leader term in heart beat (#485)" (#499)
    This reverts commit 9cd643a23aaa9e3151721d3fa70eaee9674d0595.

--- src/common/interface/meta.thrift
+++ src/common/interface/meta.thrift
@@ -624,16 +624,11 @@ enum HostRole {
     UNKNOWN     = 0x04
 } (cpp.enum_strict)
 
-struct LeaderInfo {
-    1: common.PartitionID part_id,
-    2: i64 term
-}
-
 struct HBReq {
     1: HostRole   role,
     2: common.HostAddr host,
     3: ClusterID cluster_id,
-    4: optional map<common.GraphSpaceID, list<LeaderInfo>>
+    4: optional map<common.GraphSpaceID, list<common.PartitionID>>
         (cpp.template = "std::unordered_map") leader_partIds;
     5: binary     git_info_sha
 }
