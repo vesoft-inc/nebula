baab50baf3576b71c6ffc446846ec21251574600
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2021-04-16 17:37:55 +0800

    Ignore the mv issue first (#505)

--- cmake/ThriftGenerate.cmake
+++ cmake/ThriftGenerate.cmake
@@ -89,7 +89,7 @@ add_custom_command(
     --gen "java:hashcode"
     --gen "go:thrift_import=github.com/facebook/fbthrift/thrift/lib/go/thrift,package_prefix=github.com/vesoft-inc/nebula-go/,use_context"
     -o "." "${file_path}/${file_name}.thrift"
-    && mv ./gen-rust/lib.rs ./gen-rust/${file_name}.rs
+    && ( mv ./gen-rust/lib.rs ./gen-rust/${file_name}.rs || true )
   DEPENDS "${file_path}/${file_name}.thrift"
   COMMENT "Generating thrift files for ${file_name}"
 )
