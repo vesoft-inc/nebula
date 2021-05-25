a45fa4c1dea84e1afed8c3f4587139cfe95acfd9
Yee <2520865+yixinglu@users.noreply.github.com>
2021-05-25 16:19:52 +0800

    Add memory info to get system memory usage (#541)
    * Add memory info to get system memory usage
    
    * Update usage compute method
    
    * Check sysinfo return result
    
    * Fix compile error
    
    * print errno
    
    * throw errors
    
    * Add memory test

---
+++ src/common/base/test/MemoryTest.cpp
@@ -0,0 +1,27 @@
+/* Copyright (c) 2021 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+#include <gtest/gtest.h>
+
+#include "common/base/Memory.h"
+
+namespace nebula {
+
+TEST(MemInfoTest, TestMemInfo) {
+    auto status = MemInfo::make();
+    ASSERT(status.ok());
+    auto mem = std::move(status).value();
+    ASSERT_GE(mem->totalInKB(), mem->usedInKB());
+    ASSERT_EQ(mem->totalInKB(), mem->usedInKB() + mem->freeInKB());
+
+    float ratio = static_cast<double>(mem->usedInKB() - 100) / mem->totalInKB();
+    ASSERT(mem->hitsHighWatermark(ratio));
+
+    ratio = static_cast<double>(mem->usedInKB() + 100) / mem->totalInKB();
+    ASSERT_FALSE(mem->hitsHighWatermark(ratio));
+}
+
+}   // namespace nebula
