83713b63ad4a28db4b69a7aafc1c69d3132f2acf
kyle.cao <kyle.cao@vesoft.com>
2021-07-15 13:49:01 +0800

    small fix for libc selection version (#580)

--- third-party/install-third-party.sh
+++ third-party/install-third-party.sh
@@ -27,7 +27,7 @@ cxx_cmd=${CXX:-g++}
 
 # We consider two derivatives: Red Hat and Debian
 # Place preset libc versions of each from newer to older
-libc_preset_versions=( 2.32 2.31 2.27 2.23 2.17 )
+libc_preset_versions=( 2.32 2.31 2.28 2.27 2.23 2.17 )
 gcc_preset_versions=( 10.1.0 9.3.0 9.2.0 9.1.0 8.3.0 7.5.0 7.1.0 )
 
 selected_libc_version=
