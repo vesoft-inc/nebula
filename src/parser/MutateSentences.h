d04c9437a0e873e60174185899bbe47ca4318954
Doodle <13706157+critical27@users.noreply.github.com>
2021-08-13 18:47:36 +0800

    delete tag

--- src/parser/MutateSentences.h
+++ src/parser/MutateSentences.h
@@ -420,6 +420,45 @@ class DeleteVerticesSentence final : public Sentence {
   std::unique_ptr<VerticesClause> vertices_;
 };
 
+class DeleteTagsSentence final : public Sentence {
+ public:
+  DeleteTagsSentence(VertexIDList *vidList, NameLabelList *labelList) {
+    kind_ = Kind::kDeleteTags;
+    vertices_.reset(new VerticesClause(vidList));
+    tags_.reset(labelList);
+  }
+
+  explicit DeleteTagsSentence(VertexIDList *vidList) {
+    kind_ = Kind::kDeleteTags;
+    vertices_.reset(new VerticesClause(vidList));
+    tags_ = std::make_unique<NameLabelList>();
+  }
+
+  DeleteTagsSentence(Expression *ref, NameLabelList *labelList) {
+    kind_ = Kind::kDeleteTags;
+    vertices_.reset(new VerticesClause(ref));
+    tags_.reset(labelList);
+  }
+
+  explicit DeleteTagsSentence(Expression *ref) {
+    kind_ = Kind::kDeleteTags;
+    vertices_.reset(new VerticesClause(ref));
+    tags_ = std::make_unique<NameLabelList>();
+  }
+
+  const VerticesClause *vertices() const { return vertices_.get(); }
+
+  const NameLabelList *tags() const { return tags_.get(); }
+
+  bool isAllTag() { return tags_->empty(); }
+
+  std::string toString() const override;
+
+ private:
+  std::unique_ptr<VerticesClause> vertices_;
+  std::unique_ptr<NameLabelList> tags_;
+};
+
 class DeleteEdgesSentence final : public Sentence {
  public:
   DeleteEdgesSentence(std::string *edge, EdgeKeys *keys) {
