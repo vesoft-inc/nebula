vesoft-inc/nebula

commit d886310fd3a7e69f3faafa4b53d3be4750a3b0e7

Add kvstore rate limiter (#2557)
* add simple rate limiter

* update some comments

Co-authored-by: Yee <2520865+yixinglu@users.noreply.github.com>

src/storage/admin/RebuildEdgeIndexTask.cpp
@@ -13,6 +13,8 @@
 namespace nebula {
 namespace storage {
 
+const int32_t kReserveNum = 1024 * 4;
+
 StatusOr<IndexItems> RebuildEdgeIndexTask::getIndexes(GraphSpaceID space) {
   return env_->indexMan_->getEdgeIndexes(space);
 }
@@ -54,21 +56,23 @@ nebula::cpp2::ErrorCode RebuildEdgeIndexTask::buildIndexGlobal(GraphSpaceID spac
   VertexID currentDstVertex = "";
   EdgeRanking currentRanking = 0;
   std::vector<kvstore::KV> data;
-  data.reserve(FLAGS_rebuild_index_batch_num);
+  data.reserve(kReserveNum);
   RowReaderWrapper reader;
+  size_t batchSize = 0;
   while (iter && iter->valid()) {
     if (canceled_) {
       LOG(ERROR) << "Rebuild Edge Index is Canceled";
       return nebula::cpp2::ErrorCode::SUCCEEDED;
     }
 
-    if (static_cast<int32_t>(data.size()) == FLAGS_rebuild_index_batch_num) {
-      auto result = writeData(space, part, data);
+    if (batchSize >= FLAGS_rebuild_index_batch_size) {
+      auto result = writeData(space, part, data, batchSize);
       if (result != nebula::cpp2::ErrorCode::SUCCEEDED) {
         LOG(ERROR) << "Write Part " << part << " Index Failed";
         return result;
       }
       data.clear();
+      batchSize = 0;
     }
 
     auto key = iter->key();
@@ -149,14 +153,14 @@ nebula::cpp2::ErrorCode RebuildEdgeIndexTask::buildIndexGlobal(GraphSpaceID spac
                                                     ranking,
                                                     destination.toString(),
                                                     std::move(valuesRet).value());
-        data.emplace_back(std::move(indexKey), indexVal);
+        batchSize += indexKey.size() + indexVal.size();
+        data.emplace_back(std::move(indexKey), std::move(indexVal));
       }
     }
     iter->next();
-    usleep(FLAGS_rebuild_index_process_interval);
   }
 
-  auto result = writeData(space, part, std::move(data));
+  auto result = writeData(space, part, std::move(data), batchSize);
   if (result != nebula::cpp2::ErrorCode::SUCCEEDED) {
     LOG(ERROR) << "Write Part " << part << " Index Failed";
     return nebula::cpp2::ErrorCode::E_STORE_FAILURE;

src/storage/admin/RebuildTagIndexTask.cpp
@@ -13,6 +13,8 @@
 namespace nebula {
 namespace storage {
 
+const int32_t kReserveNum = 1024 * 4;
+
 StatusOr<IndexItems> RebuildTagIndexTask::getIndexes(GraphSpaceID space) {
   return env_->indexMan_->getTagIndexes(space);
 }
@@ -52,22 +54,23 @@ nebula::cpp2::ErrorCode RebuildTagIndexTask::buildIndexGlobal(GraphSpaceID space
 
   VertexID currentVertex = "";
   std::vector<kvstore::KV> data;
-  data.reserve(FLAGS_rebuild_index_batch_num);
+  data.reserve(kReserveNum);
   RowReaderWrapper reader;
+  size_t batchSize = 0;
   while (iter && iter->valid()) {
     if (canceled_) {
       LOG(ERROR) << "Rebuild Tag Index is Canceled";
       return nebula::cpp2::ErrorCode::SUCCEEDED;
     }
 
-    if (static_cast<int32_t>(data.size()) == FLAGS_rebuild_index_batch_num) {
-      auto result = writeData(space, part, data);
+    if (batchSize >= FLAGS_rebuild_index_batch_size) {
+      auto result = writeData(space, part, data, batchSize);
       if (result != nebula::cpp2::ErrorCode::SUCCEEDED) {
         LOG(ERROR) << "Write Part " << part << " Index Failed";
         return result;
       }
-
       data.clear();
+      batchSize = 0;
     }
 
     auto key = iter->key();
@@ -131,14 +134,14 @@ nebula::cpp2::ErrorCode RebuildTagIndexTask::buildIndexGlobal(GraphSpaceID space
         }
         auto indexKey = IndexKeyUtils::vertexIndexKey(
             vidSize, part, item->get_index_id(), vertex.toString(), std::move(valuesRet).value());
-        data.emplace_back(std::move(indexKey), indexVal);
+        batchSize += indexKey.size() + indexVal.size();
+        data.emplace_back(std::move(indexKey), std::move(indexVal));
       }
     }
     iter->next();
-    usleep(FLAGS_rebuild_index_process_interval);
   }
 
-  auto result = writeData(space, part, std::move(data));
+  auto result = writeData(space, part, std::move(data), batchSize);
   if (result != nebula::cpp2::ErrorCode::SUCCEEDED) {
     LOG(ERROR) << "Write Part " << part << " Index Failed";
     return nebula::cpp2::ErrorCode::E_STORE_FAILURE;

src/kvstore/CMakeLists.txt
@@ -7,7 +7,7 @@ nebula_add_library(
     NebulaStore.cpp
     RocksEngineConfig.cpp
     LogEncoder.cpp
-    SnapshotManagerImpl.cpp
+    NebulaSnapshotManager.cpp
     plugins/elasticsearch/ESListener.cpp
 )
 

src/kvstore/test/CMakeLists.txt
@@ -185,3 +185,18 @@ nebula_add_executable(
         gtest
         boost_regex
 )
+
+nebula_add_test(
+    NAME
+        rate_limiter_test
+    SOURCES
+        RateLimiterTest.cpp
+    OBJECTS
+        ${KVSTORE_TEST_LIBS}
+    LIBRARIES
+        ${THRIFT_LIBRARIES}
+        ${ROCKSDB_LIBRARIES}
+        ${PROXYGEN_LIBRARIES}
+        wangle
+        gtest
+)

conf/nebula-storaged.conf.default
@@ -100,9 +100,3 @@
 --rocksdb_column_family_options={"write_buffer_size":"67108864","max_write_buffer_number":"4","max_bytes_for_level_base":"268435456"}
 # rocksdb BlockBasedTableOptions in json, each name and value of option is string, given as "option_name":"option_value" separated by comma
 --rocksdb_block_based_table_options={"block_size":"8192"}
-
-############## Runtime global Options ##############
-# Index rebuild batch number
---rebuild_index_batch_num=256
-# Index rebuild processing interval, microsecond.
---rebuild_index_process_interval=3000

conf/nebula-storaged.conf.production
@@ -100,10 +100,7 @@
 --enable_rocksdb_prefix_filtering=false
 
 ############### misc ####################
---max_handlers_per_req=1
-
-############## Runtime global Options ##############
-# Index rebuild batch number
---rebuild_index_batch_num=256
-# Index rebuild processing interval, microsecond.
---rebuild_index_process_interval=3000
+--snapshot_part_rate_limit=8388608
+--snapshot_batch_size=1048576
+--rebuild_index_part_rate_limit=4194304
+--rebuild_index_batch_size=1048576

src/kvstore/LogEncoder.h
@@ -7,6 +7,7 @@
 #ifndef KVSTORE_LOGENCODER_H_
 #define KVSTORE_LOGENCODER_H_
 
+#include "common/cpp/helpers.h"
 #include "kvstore/Common.h"
 
 namespace nebula {
@@ -54,37 +55,44 @@ HostAddr decodeHost(LogType type, const folly::StringPiece& encoded);
 
 int64_t getTimestamp(const folly::StringPiece& command);
 
-class BatchHolder {
+class BatchHolder : public nebula::cpp::NonCopyable, public nebula::cpp::NonMovable {
  public:
   BatchHolder() = default;
   ~BatchHolder() = default;
 
   void put(std::string&& key, std::string&& val) {
+    size_ += key.size() + val.size();
     auto op = std::make_tuple(
         BatchLogType::OP_BATCH_PUT, std::forward<std::string>(key), std::forward<std::string>(val));
     batch_.emplace_back(std::move(op));
   }
 
   void remove(std::string&& key) {
+    size_ += key.size();
     auto op = std::make_tuple(BatchLogType::OP_BATCH_REMOVE, std::forward<std::string>(key), "");
     batch_.emplace_back(std::move(op));
   }
 
   void rangeRemove(std::string&& begin, std::string&& end) {
+    size_ += begin.size() + end.size();
     auto op = std::make_tuple(BatchLogType::OP_BATCH_REMOVE_RANGE,
                               std::forward<std::string>(begin),
                               std::forward<std::string>(end));
     batch_.emplace_back(std::move(op));
   }
 
-  void clear() { batch_.clear(); }
+  void reserve(int32_t size) { batch_.reserve(size); }
 
   const std::vector<std::tuple<BatchLogType, std::string, std::string>>& getBatch() {
     return batch_;
   }
 
+  // size of the batch, in bytes
+  size_t size() { return size_; }
+
  private:
   std::vector<std::tuple<BatchLogType, std::string, std::string>> batch_;
+  size_t size_{0};
 };
 
 }  // namespace kvstore

src/kvstore/NebulaSnapshotManager.cpp
@@ -4,25 +4,46 @@
  * attached with Common Clause Condition 1.0, found in the LICENSES directory.
  */
 
-#include "kvstore/SnapshotManagerImpl.h"
+#include "kvstore/NebulaSnapshotManager.h"
 
 #include "common/utils/NebulaKeyUtils.h"
 #include "kvstore/LogEncoder.h"
+#include "kvstore/RateLimiter.h"
 
-DEFINE_int32(snapshot_batch_size, 1024 * 1024 * 10, "batch size for snapshot");
+DEFINE_uint32(snapshot_part_rate_limit,
+              1024 * 1024 * 2,
+              "max bytes of pulling snapshot for each partition in one second");
+DEFINE_uint32(snapshot_batch_size, 1024 * 512, "batch size for snapshot, in bytes");
 
 namespace nebula {
 namespace kvstore {
 
 const int32_t kReserveNum = 1024 * 4;
-void SnapshotManagerImpl::accessAllRowsInSnapshot(GraphSpaceID spaceId,
-                                                  PartitionID partId,
-                                                  raftex::SnapshotCallback cb) {
+
+NebulaSnapshotManager::NebulaSnapshotManager(NebulaStore* kv) : store_(kv) {
+  // Snapshot rate is limited to FLAGS_snapshot_worker_threads * FLAGS_snapshot_part_rate_limit.
+  // So by default, the total send rate is limited to 4 * 2Mb = 8Mb.
+  rateLimiter_.reset(
+      new RateLimiter(FLAGS_snapshot_part_rate_limit, FLAGS_snapshot_part_rate_limit));
+}
+
+void NebulaSnapshotManager::accessAllRowsInSnapshot(GraphSpaceID spaceId,
+                                                    PartitionID partId,
+                                                    raftex::SnapshotCallback cb) {
   CHECK_NOTNULL(store_);
   auto tables = NebulaKeyUtils::snapshotPrefix(partId);
   std::vector<std::string> data;
   int64_t totalSize = 0;
   int64_t totalCount = 0;
+  LOG(INFO) << folly::format(
+      "Space {} Part {} start send snapshot, rate limited to {}, batch size is {}",
+      spaceId,
+      partId,
+      FLAGS_snapshot_part_rate_limit,
+      FLAGS_snapshot_batch_size);
+  // raft will make sure that there won't be concurrent accessAllRowsInSnapshot of a given partition
+  rateLimiter_->add(spaceId, partId);
+  SCOPE_EXIT { rateLimiter_->remove(spaceId, partId); };
 
   for (const auto& prefix : tables) {
     if (!accessTable(spaceId, partId, prefix, cb, data, totalCount, totalSize)) {
@@ -34,13 +55,13 @@ void SnapshotManagerImpl::accessAllRowsInSnapshot(GraphSpaceID spaceId,
 
 // Promise is set in callback. Access part of the data, and try to send to
 // peers. If send failed, will return false.
-bool SnapshotManagerImpl::accessTable(GraphSpaceID spaceId,
-                                      PartitionID partId,
-                                      const std::string& prefix,
-                                      raftex::SnapshotCallback& cb,
-                                      std::vector<std::string>& data,
-                                      int64_t& totalCount,
-                                      int64_t& totalSize) {
+bool NebulaSnapshotManager::accessTable(GraphSpaceID spaceId,
+                                        PartitionID partId,
+                                        const std::string& prefix,
+                                        raftex::SnapshotCallback& cb,
+                                        std::vector<std::string>& data,
+                                        int64_t& totalCount,
+                                        int64_t& totalSize) {
   std::unique_ptr<KVIterator> iter;
   auto ret = store_->prefix(spaceId, partId, prefix, &iter);
   if (ret != nebula::cpp2::ErrorCode::SUCCEEDED) {
@@ -50,9 +71,10 @@ bool SnapshotManagerImpl::accessTable(GraphSpaceID spaceId,
     return false;
   }
   data.reserve(kReserveNum);
-  int32_t batchSize = 0;
+  size_t batchSize = 0;
   while (iter && iter->valid()) {
     if (batchSize >= FLAGS_snapshot_batch_size) {
+      rateLimiter_->consume(spaceId, partId, batchSize);
       if (cb(data, totalCount, totalSize, raftex::SnapshotStatus::IN_PROGRESS)) {
         data.clear();
         batchSize = 0;

commit 4dd638e86a4bd50ee8abbf5e13eb280a1aeafc37

Cherry-pick PR from v2.5.0 (#2581)
* get meta version in hb

* cherry-pick #549 #550 #551

* damn timeout

* fix rebuild index bug introduced in #2557

.github/workflows/pull_request.yml
@@ -140,7 +140,7 @@ jobs:
         run: |
           make RM_DIR=false DEBUG=false J=${{ steps.cmake.outputs.j }} tck
         working-directory: tests/
-        timeout-minutes: 30
+        timeout-minutes: 60
       - name: Down cluster
         run: |
           make down

src/clients/meta/MetaClient.cpp
@@ -55,12 +55,17 @@ MetaClient::~MetaClient() {
 
 bool MetaClient::isMetadReady() {
   auto ret = heartbeat().get();
-  if (!ret.ok() && ret.status() != Status::LeaderChanged()) {
+  if (!ret.ok()) {
     LOG(ERROR) << "Heartbeat failed, status:" << ret.status();
-    ready_ = false;
+    return ready_;
+  } else if (options_.role_ == cpp2::HostRole::STORAGE &&
+             metaServerVersion_ != EXPECT_META_VERSION) {
+    LOG(ERROR) << "Expect meta version is " << EXPECT_META_VERSION << ", but actual is "
+               << metaServerVersion_;
     return ready_;
   }
 
+  // ready_ will be set in loadData
   bool ldRet = loadData();
   bool lcRet = true;
   if (!options_.skipConfig_) {
@@ -2330,7 +2335,8 @@ folly::Future<StatusOr<bool>> MetaClient::heartbeat() {
         heartbeatTime_ = time::WallClock::fastNowInMilliSec();
         metadLastUpdateTime_ = resp.get_last_update_time_in_ms();
         VLOG(1) << "Metad last update time: " << metadLastUpdateTime_;
-        return true;  // resp.code == nebula::cpp2::ErrorCode::SUCCEEDED
+        metaServerVersion_ = resp.get_meta_version();
+        return resp.get_code() == nebula::cpp2::ErrorCode::SUCCEEDED;
       },
       std::move(promise));
   return future;

src/clients/meta/MetaClient.h
@@ -23,6 +23,7 @@
 #include "interface/gen-cpp2/meta_types.h"
 
 DECLARE_int32(meta_client_retry_times);
+DECLARE_int32(heartbeat_interval_secs);
 
 namespace nebula {
 namespace meta {
@@ -184,7 +185,7 @@ class MetaClient {
 
   bool isMetadReady();
 
-  bool waitForMetadReady(int count = -1, int retryIntervalSecs = 2);
+  bool waitForMetadReady(int count = -1, int retryIntervalSecs = FLAGS_heartbeat_interval_secs);
 
   void stop();
 
@@ -694,6 +695,8 @@ class MetaClient {
   folly::RWSpinLock leaderIdsLock_;
   int64_t localLastUpdateTime_{0};
   int64_t metadLastUpdateTime_{0};
+  int64_t metaServerVersion_{-1};
+  static constexpr int64_t EXPECT_META_VERSION = 2;
 
   // leadersLock_ is used to protect leadersInfo
   folly::RWSpinLock leadersLock_;

src/interface/meta.thrift
@@ -547,6 +547,7 @@ struct HBResp {
     2: common.HostAddr  leader,
     3: ClusterID        cluster_id,
     4: i64              last_update_time_in_ms,
+    5: i32              meta_version,
 }
 
 enum HostRole {

src/meta/CMakeLists.txt
@@ -96,6 +96,11 @@ nebula_add_library(
     processors/session/SessionManagerProcessor.cpp
 )
 
+add_dependencies(
+    meta_service_handler
+    meta_version_man_obj
+)
+
 nebula_add_library(
     meta_version_man_obj OBJECT
     MetaVersionMan.cpp
@@ -109,6 +114,9 @@ add_dependencies(
 set(meta_test_deps
     $<TARGET_OBJECTS:mock_obj>
     $<TARGET_OBJECTS:meta_service_handler>
+    $<TARGET_OBJECTS:meta_version_man_obj>
+    $<TARGET_OBJECTS:meta_v1_thrift_obj>
+    $<TARGET_OBJECTS:meta_data_upgrade_obj>
     $<TARGET_OBJECTS:general_storage_service_handler>
     $<TARGET_OBJECTS:storage_admin_service_handler>
     $<TARGET_OBJECTS:graph_storage_service_handler>

src/meta/MetaServiceUtils.cpp
@@ -28,8 +28,11 @@ static const std::unordered_map<std::string, std::pair<std::string, bool>> syste
     {"groups", {"__groups__", true}},
     {"zones", {"__zones__", true}},
     {"ft_service", {"__ft_service__", false}},
-    {"sessions", {"__sessions__", true}},
-    {"id", {"__id__", true}}};
+    {"sessions", {"__sessions__", true}}};
+
+// SystemInfo will always be backuped
+static const std::unordered_map<std::string, std::pair<std::string, bool>> systemInfoMaps{
+    {"autoIncrementId", {"__id__", true}}, {"lastUpdateTime", {"__last_update_time__", true}}};
 
 // name => {prefix, parseSpaceid}, nullptr means that the backup should be skipped.
 static const std::unordered_map<
@@ -45,7 +48,6 @@ static const std::unordered_map<
         {"index", {"__index__", nullptr}},
         {"index_status", {"__index_status__", MetaServiceUtils::parseIndexStatusKeySpaceID}},
         {"roles", {"__roles__", MetaServiceUtils::parseRoleSpace}},
-        {"last_update_time", {"__last_update_time__", nullptr}},
         {"leaders", {"__leaders__", nullptr}},
         {"leader_terms", {"__leader_terms__", nullptr}},
         {"listener", {"__listener__", nullptr}},
@@ -68,7 +70,6 @@ static const std::string kUsersTable          = systemTableMaps.at("users").firs
 static const std::string kRolesTable          = tableMaps.at("roles").first;          // NOLINT
 static const std::string kConfigsTable        = systemTableMaps.at("configs").first;        // NOLINT
 static const std::string kSnapshotsTable      = systemTableMaps.at("snapshots").first;      // NOLINT
-static const std::string kLastUpdateTimeTable = tableMaps.at("last_update_time").first; // NOLINT
 static const std::string kLeadersTable        = tableMaps.at("leaders").first;          // NOLINT
 static const std::string kLeaderTermsTable    = tableMaps.at("leader_terms").first;     // NOLINT
 static const std::string kGroupsTable         = systemTableMaps.at("groups").first;           // NOLINT
@@ -87,7 +88,9 @@ const std::string kFTIndexTable        = tableMaps.at("ft_index").first;        
 const std::string kFTServiceTable = systemTableMaps.at("ft_service").first;      // NOLINT
 const std::string kSessionsTable = systemTableMaps.at("sessions").first;         // NOLINT
 
-const std::string kIdKey = systemTableMaps.at("id").first;                       // NOLINT
+const std::string kIdKey = systemInfoMaps.at("autoIncrementId").first;                // NOLINT
+const std::string kLastUpdateTimeTable = systemInfoMaps.at("lastUpdateTime").first;   // NOLINT
+
 // clang-format on
 
 const int kMaxIpAddrLen = 15;  // '255.255.255.255'
@@ -1093,6 +1096,18 @@ ErrorOr<nebula::cpp2::ErrorCode, std::vector<std::string>> MetaServiceUtils::bac
       }
       LOG(INFO) << table.first << " table backup successed";
     }
+  }
+
+  for (const auto& table : systemInfoMaps) {
+    if (!table.second.second) {
+      LOG(INFO) << table.first << " table skipped";
+      continue;
+    }
+    auto result = backupTable(kvstore, backupName, table.second.first, files, nullptr);
+    if (result != nebula::cpp2::ErrorCode::SUCCEEDED) {
+      return result;
+    }
+    LOG(INFO) << table.first << " table backup successed";
   }
 
   // The mapping of space name and space id needs to be handled separately.

src/meta/processors/admin/HBProcessor.cpp
@@ -9,13 +9,14 @@
 #include "common/time/WallClock.h"
 #include "meta/ActiveHostsMan.h"
 #include "meta/KVBasedClusterIdMan.h"
-
-DEFINE_bool(hosts_whitelist_enabled, false, "Check host whether in whitelist when received hb");
+#include "meta/MetaVersionMan.h"
 
 namespace nebula {
 namespace meta {
 
 HBCounters kHBCounters;
+
+std::atomic<int64_t> HBProcessor::metaVersion_ = -1;
 
 void HBProcessor::onFinished() {
   if (counters_) {
@@ -29,18 +30,6 @@ void HBProcessor::onFinished() {
 void HBProcessor::process(const cpp2::HBReq& req) {
   HostAddr host((*req.host_ref()).host, (*req.host_ref()).port);
   nebula::cpp2::ErrorCode ret;
-  if (FLAGS_hosts_whitelist_enabled) {
-    ret = hostExist(MetaServiceUtils::hostKey(host.host, host.port));
-    if (ret != nebula::cpp2::ErrorCode::SUCCEEDED) {
-      LOG(INFO) << "Reject unregistered host " << host << "!";
-      if (ret != nebula::cpp2::ErrorCode::E_LEADER_CHANGED) {
-        ret = nebula::cpp2::ErrorCode::E_INVALID_HOST;
-      }
-      handleErrorCode(ret);
-      onFinished();
-      return;
-    }
-  }
 
   LOG(INFO) << "Receive heartbeat from " << host
             << ", role = " << apache::thrift::util::enumNameSafe(req.get_role());
@@ -79,6 +68,14 @@ void HBProcessor::process(const cpp2::HBReq& req) {
   } else if (nebula::error(lastUpdateTimeRet) == nebula::cpp2::ErrorCode::E_KEY_NOT_FOUND) {
     resp_.set_last_update_time_in_ms(0);
   }
+
+  auto version = metaVersion_.load();
+  if (version == -1) {
+    metaVersion_.store(static_cast<int64_t>(MetaVersionMan::getMetaVersionFromKV(kvstore_)));
+  }
+
+  resp_.set_meta_version(metaVersion_.load());
+
   handleErrorCode(ret);
   onFinished();
 }

src/meta/processors/admin/HBProcessor.h
@@ -56,6 +56,7 @@ class HBProcessor : public BaseProcessor<cpp2::HBResp> {
 
   ClusterID clusterId_{0};
   const HBCounters* counters_{nullptr};
+  static std::atomic<int64_t> metaVersion_;
 };
 
 }  // namespace meta

src/storage/admin/RebuildEdgeIndexTask.cpp
@@ -154,7 +154,7 @@ nebula::cpp2::ErrorCode RebuildEdgeIndexTask::buildIndexGlobal(GraphSpaceID spac
                                                     destination.toString(),
                                                     std::move(valuesRet).value());
         batchSize += indexKey.size() + indexVal.size();
-        data.emplace_back(std::move(indexKey), std::move(indexVal));
+        data.emplace_back(std::move(indexKey), indexVal);
       }
     }
     iter->next();

src/storage/admin/RebuildTagIndexTask.cpp
@@ -135,7 +135,7 @@ nebula::cpp2::ErrorCode RebuildTagIndexTask::buildIndexGlobal(GraphSpaceID space
         auto indexKey = IndexKeyUtils::vertexIndexKey(
             vidSize, part, item->get_index_id(), vertex.toString(), std::move(valuesRet).value());
         batchSize += indexKey.size() + indexVal.size();
-        data.emplace_back(std::move(indexKey), std::move(indexVal));
+        data.emplace_back(std::move(indexKey), indexVal);
       }
     }
     iter->next();

src/tools/db-dump/CMakeLists.txt
@@ -1,5 +1,8 @@
 set(tools_test_deps
     $<TARGET_OBJECTS:meta_service_handler>
+    $<TARGET_OBJECTS:meta_version_man_obj>
+    $<TARGET_OBJECTS:meta_v1_thrift_obj>
+    $<TARGET_OBJECTS:meta_data_upgrade_obj>
     $<TARGET_OBJECTS:storage_admin_service_handler>
     $<TARGET_OBJECTS:graph_storage_service_handler>
     $<TARGET_OBJECTS:storage_transaction_executor>

src/tools/db-upgrade/CMakeLists.txt
@@ -8,6 +8,9 @@ nebula_add_executable(
         DbUpgrader.cpp
     OBJECTS
         $<TARGET_OBJECTS:meta_service_handler>
+        $<TARGET_OBJECTS:meta_version_man_obj>
+        $<TARGET_OBJECTS:meta_v1_thrift_obj>
+        $<TARGET_OBJECTS:meta_data_upgrade_obj>
         $<TARGET_OBJECTS:storage_admin_service_handler>
         $<TARGET_OBJECTS:graph_storage_service_handler>
         $<TARGET_OBJECTS:storage_transaction_executor>

src/tools/meta-dump/CMakeLists.txt
@@ -5,6 +5,9 @@ nebula_add_executable(
         MetaDumpTool.cpp
     OBJECTS
         $<TARGET_OBJECTS:meta_service_handler>
+        $<TARGET_OBJECTS:meta_version_man_obj>
+        $<TARGET_OBJECTS:meta_v1_thrift_obj>
+        $<TARGET_OBJECTS:meta_data_upgrade_obj>
         $<TARGET_OBJECTS:storage_admin_service_handler>
         $<TARGET_OBJECTS:graph_storage_service_handler>
         $<TARGET_OBJECTS:storage_transaction_executor>

src/tools/simple-kv-verify/CMakeLists.txt
@@ -5,6 +5,9 @@ nebula_add_executable(
         SimpleKVVerifyTool.cpp
     OBJECTS
         $<TARGET_OBJECTS:meta_service_handler>
+        $<TARGET_OBJECTS:meta_version_man_obj>
+        $<TARGET_OBJECTS:meta_v1_thrift_obj>
+        $<TARGET_OBJECTS:meta_data_upgrade_obj>
         $<TARGET_OBJECTS:storage_admin_service_handler>
         $<TARGET_OBJECTS:graph_storage_service_handler>
         $<TARGET_OBJECTS:storage_common_obj>

src/tools/storage-perf/CMakeLists.txt
@@ -1,5 +1,8 @@
 set(perf_test_deps
     $<TARGET_OBJECTS:meta_service_handler>
+    $<TARGET_OBJECTS:meta_version_man_obj>
+    $<TARGET_OBJECTS:meta_v1_thrift_obj>
+    $<TARGET_OBJECTS:meta_data_upgrade_obj>
     $<TARGET_OBJECTS:storage_admin_service_handler>
     $<TARGET_OBJECTS:graph_storage_service_handler>
     $<TARGET_OBJECTS:storage_transaction_executor>

src/meta/test/HBProcessorTest.cpp
@@ -11,8 +11,6 @@
 #include "meta/processors/admin/HBProcessor.h"
 #include "meta/test/TestUtils.h"
 
-DECLARE_bool(hosts_whitelist_enabled);
-
 namespace nebula {
 namespace meta {
 

src/meta/test/RestoreProcessorTest.cpp
@@ -89,6 +89,14 @@ TEST(RestoreProcessorTest, RestoreTest) {
                     std::string(reinterpret_cast<const char*>(&zoneId), sizeof(ZoneID)));
   data.emplace_back(MetaServiceUtils::zoneKey(zoneName), MetaServiceUtils::zoneVal(hosts));
 
+  int32_t autoId = 666;
+  data.emplace_back(MetaServiceUtils::idKey(),
+                    std::string(reinterpret_cast<const char*>(&autoId), sizeof(autoId)));
+
+  auto lastUpdateTime = time::WallClock::fastNowInMilliSec();
+  data.emplace_back(MetaServiceUtils::lastUpdateTimeKey(),
+                    MetaServiceUtils::lastUpdateTimeVal(lastUpdateTime));
+
   folly::Baton<true, std::atomic> baton;
   kv->asyncMultiPut(0, 0, std::move(data), [&](nebula::cpp2::ErrorCode code) {
     ret = (code == nebula::cpp2::ErrorCode::SUCCEEDED);
@@ -218,6 +226,17 @@ TEST(RestoreProcessorTest, RestoreTest) {
     result = kvRestore->prefix(kDefaultSpaceId, kDefaultPartId, prefix, &iter);
     ASSERT_EQ(nebula::cpp2::ErrorCode::SUCCEEDED, result);
     ASSERT_FALSE(iter->valid());
+
+    std::string key = "__id__";
+    std::string value;
+    result = kvRestore->get(kDefaultSpaceId, kDefaultPartId, key, &value);
+    ASSERT_EQ(nebula::cpp2::ErrorCode::SUCCEEDED, result);
+    ASSERT_EQ(autoId, *reinterpret_cast<const int32_t*>(value.data()));
+
+    key = "__last_update_time__";
+    result = kvRestore->get(kDefaultSpaceId, kDefaultPartId, key, &value);
+    ASSERT_EQ(nebula::cpp2::ErrorCode::SUCCEEDED, result);
+    ASSERT_EQ(lastUpdateTime, *reinterpret_cast<const int64_t*>(value.data()));
   }
 }
 

src/storage/test/CMakeLists.txt
@@ -1,6 +1,9 @@
 set(storage_test_deps
     $<TARGET_OBJECTS:mock_obj>
     $<TARGET_OBJECTS:meta_service_handler>
+    $<TARGET_OBJECTS:meta_version_man_obj>
+    $<TARGET_OBJECTS:meta_v1_thrift_obj>
+    $<TARGET_OBJECTS:meta_data_upgrade_obj>
     $<TARGET_OBJECTS:storage_admin_service_handler>
     $<TARGET_OBJECTS:graph_storage_service_handler>
     $<TARGET_OBJECTS:storage_common_obj>
@@ -575,13 +578,6 @@ nebula_add_test(
     SOURCES
         KVTest.cpp
     OBJECTS
-        $<TARGET_OBJECTS:storage_admin_service_handler>
-        $<TARGET_OBJECTS:storage_common_obj>
-        $<TARGET_OBJECTS:meta_service_handler>
-        $<TARGET_OBJECTS:mock_obj>
-        $<TARGET_OBJECTS:ws_common_obj>
-        $<TARGET_OBJECTS:ws_obj>
-        $<TARGET_OBJECTS:http_client_obj>
         ${storage_test_deps}
     LIBRARIES
         ${ROCKSDB_LIBRARIES}
@@ -597,15 +593,7 @@ nebula_add_test(
     SOURCES
         KVClientTest.cpp
     OBJECTS
-        $<TARGET_OBJECTS:storage_admin_service_handler>
-        $<TARGET_OBJECTS:storage_common_obj>
-        $<TARGET_OBJECTS:meta_service_handler>
-        $<TARGET_OBJECTS:mock_obj>
         $<TARGET_OBJECTS:general_storage_client_obj>
-        $<TARGET_OBJECTS:storage_client_base_obj>
-        $<TARGET_OBJECTS:ws_common_obj>
-        $<TARGET_OBJECTS:ws_obj>
-        $<TARGET_OBJECTS:http_client_obj>
         ${storage_test_deps}
     LIBRARIES
         ${ROCKSDB_LIBRARIES}
@@ -636,15 +624,6 @@ nebula_add_executable(
     SOURCES
         ElasticSearchBulkInsertTest.cpp
     OBJECTS
-        $<TARGET_OBJECTS:storage_admin_service_handler>
-        $<TARGET_OBJECTS:storage_common_obj>
-        $<TARGET_OBJECTS:meta_service_handler>
-        $<TARGET_OBJECTS:mock_obj>
-        $<TARGET_OBJECTS:general_storage_client_obj>
-        $<TARGET_OBJECTS:storage_client_base_obj>
-        $<TARGET_OBJECTS:ws_common_obj>
-        $<TARGET_OBJECTS:ws_obj>
-        $<TARGET_OBJECTS:http_client_obj>
         ${storage_test_deps}
     LIBRARIES
         ${ROCKSDB_LIBRARIES}