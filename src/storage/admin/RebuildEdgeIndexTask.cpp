vesoft-inc/nebula

commit d886310fd3a7e69f3faafa4b53d3be4750a3b0e7

Add kvstore rate limiter (#2557)
* add simple rate limiter

* update some comments

Co-authored-by: Yee <2520865+yixinglu@users.noreply.github.com>

src/storage/admin/RebuildEdgeIndexTask.cpp
@@ -13,6 +13,8 @@
 namespace nebula {
 namespace storage {
 
+const int32_t kReserveNum = 1024 * 4;
+
 StatusOr<IndexItems> RebuildEdgeIndexTask::getIndexes(GraphSpaceID space) {
   return env_->indexMan_->getEdgeIndexes(space);
 }
@@ -54,21 +56,23 @@ nebula::cpp2::ErrorCode RebuildEdgeIndexTask::buildIndexGlobal(GraphSpaceID spac
   VertexID currentDstVertex = "";
   EdgeRanking currentRanking = 0;
   std::vector<kvstore::KV> data;
-  data.reserve(FLAGS_rebuild_index_batch_num);
+  data.reserve(kReserveNum);
   RowReaderWrapper reader;
+  size_t batchSize = 0;
   while (iter && iter->valid()) {
     if (canceled_) {
       LOG(ERROR) << "Rebuild Edge Index is Canceled";
       return nebula::cpp2::ErrorCode::SUCCEEDED;
     }
 
-    if (static_cast<int32_t>(data.size()) == FLAGS_rebuild_index_batch_num) {
-      auto result = writeData(space, part, data);
+    if (batchSize >= FLAGS_rebuild_index_batch_size) {
+      auto result = writeData(space, part, data, batchSize);
       if (result != nebula::cpp2::ErrorCode::SUCCEEDED) {
         LOG(ERROR) << "Write Part " << part << " Index Failed";
         return result;
       }
       data.clear();
+      batchSize = 0;
     }
 
     auto key = iter->key();
@@ -149,14 +153,14 @@ nebula::cpp2::ErrorCode RebuildEdgeIndexTask::buildIndexGlobal(GraphSpaceID spac
                                                     ranking,
                                                     destination.toString(),
                                                     std::move(valuesRet).value());
-        data.emplace_back(std::move(indexKey), indexVal);
+        batchSize += indexKey.size() + indexVal.size();
+        data.emplace_back(std::move(indexKey), std::move(indexVal));
       }
     }
     iter->next();
-    usleep(FLAGS_rebuild_index_process_interval);
   }
 
-  auto result = writeData(space, part, std::move(data));
+  auto result = writeData(space, part, std::move(data), batchSize);
   if (result != nebula::cpp2::ErrorCode::SUCCEEDED) {
     LOG(ERROR) << "Write Part " << part << " Index Failed";
     return nebula::cpp2::ErrorCode::E_STORE_FAILURE;

src/storage/admin/RebuildTagIndexTask.cpp
@@ -13,6 +13,8 @@
 namespace nebula {
 namespace storage {
 
+const int32_t kReserveNum = 1024 * 4;
+
 StatusOr<IndexItems> RebuildTagIndexTask::getIndexes(GraphSpaceID space) {
   return env_->indexMan_->getTagIndexes(space);
 }
@@ -52,22 +54,23 @@ nebula::cpp2::ErrorCode RebuildTagIndexTask::buildIndexGlobal(GraphSpaceID space
 
   VertexID currentVertex = "";
   std::vector<kvstore::KV> data;
-  data.reserve(FLAGS_rebuild_index_batch_num);
+  data.reserve(kReserveNum);
   RowReaderWrapper reader;
+  size_t batchSize = 0;
   while (iter && iter->valid()) {
     if (canceled_) {
       LOG(ERROR) << "Rebuild Tag Index is Canceled";
       return nebula::cpp2::ErrorCode::SUCCEEDED;
     }
 
-    if (static_cast<int32_t>(data.size()) == FLAGS_rebuild_index_batch_num) {
-      auto result = writeData(space, part, data);
+    if (batchSize >= FLAGS_rebuild_index_batch_size) {
+      auto result = writeData(space, part, data, batchSize);
       if (result != nebula::cpp2::ErrorCode::SUCCEEDED) {
         LOG(ERROR) << "Write Part " << part << " Index Failed";
         return result;
       }
-
       data.clear();
+      batchSize = 0;
     }
 
     auto key = iter->key();
@@ -131,14 +134,14 @@ nebula::cpp2::ErrorCode RebuildTagIndexTask::buildIndexGlobal(GraphSpaceID space
         }
         auto indexKey = IndexKeyUtils::vertexIndexKey(
             vidSize, part, item->get_index_id(), vertex.toString(), std::move(valuesRet).value());
-        data.emplace_back(std::move(indexKey), indexVal);
+        batchSize += indexKey.size() + indexVal.size();
+        data.emplace_back(std::move(indexKey), std::move(indexVal));
       }
     }
     iter->next();
-    usleep(FLAGS_rebuild_index_process_interval);
   }
 
-  auto result = writeData(space, part, std::move(data));
+  auto result = writeData(space, part, std::move(data), batchSize);
   if (result != nebula::cpp2::ErrorCode::SUCCEEDED) {
     LOG(ERROR) << "Write Part " << part << " Index Failed";
     return nebula::cpp2::ErrorCode::E_STORE_FAILURE;

conf/nebula-storaged.conf.default
@@ -100,9 +100,3 @@
 --rocksdb_column_family_options={"write_buffer_size":"67108864","max_write_buffer_number":"4","max_bytes_for_level_base":"268435456"}
 # rocksdb BlockBasedTableOptions in json, each name and value of option is string, given as "option_name":"option_value" separated by comma
 --rocksdb_block_based_table_options={"block_size":"8192"}
-
-############## Runtime global Options ##############
-# Index rebuild batch number
---rebuild_index_batch_num=256
-# Index rebuild processing interval, microsecond.
---rebuild_index_process_interval=3000

conf/nebula-storaged.conf.production
@@ -100,10 +100,7 @@
 --enable_rocksdb_prefix_filtering=false
 
 ############### misc ####################
---max_handlers_per_req=1
-
-############## Runtime global Options ##############
-# Index rebuild batch number
---rebuild_index_batch_num=256
-# Index rebuild processing interval, microsecond.
---rebuild_index_process_interval=3000
+--snapshot_part_rate_limit=8388608
+--snapshot_batch_size=1048576
+--rebuild_index_part_rate_limit=4194304
+--rebuild_index_batch_size=1048576

src/kvstore/CMakeLists.txt
@@ -7,7 +7,7 @@ nebula_add_library(
     NebulaStore.cpp
     RocksEngineConfig.cpp
     LogEncoder.cpp
-    SnapshotManagerImpl.cpp
+    NebulaSnapshotManager.cpp
     plugins/elasticsearch/ESListener.cpp
 )
 

src/kvstore/LogEncoder.h
@@ -7,6 +7,7 @@
 #ifndef KVSTORE_LOGENCODER_H_
 #define KVSTORE_LOGENCODER_H_
 
+#include "common/cpp/helpers.h"
 #include "kvstore/Common.h"
 
 namespace nebula {
@@ -54,37 +55,44 @@ HostAddr decodeHost(LogType type, const folly::StringPiece& encoded);
 
 int64_t getTimestamp(const folly::StringPiece& command);
 
-class BatchHolder {
+class BatchHolder : public nebula::cpp::NonCopyable, public nebula::cpp::NonMovable {
  public:
   BatchHolder() = default;
   ~BatchHolder() = default;
 
   void put(std::string&& key, std::string&& val) {
+    size_ += key.size() + val.size();
     auto op = std::make_tuple(
         BatchLogType::OP_BATCH_PUT, std::forward<std::string>(key), std::forward<std::string>(val));
     batch_.emplace_back(std::move(op));
   }
 
   void remove(std::string&& key) {
+    size_ += key.size();
     auto op = std::make_tuple(BatchLogType::OP_BATCH_REMOVE, std::forward<std::string>(key), "");
     batch_.emplace_back(std::move(op));
   }
 
   void rangeRemove(std::string&& begin, std::string&& end) {
+    size_ += begin.size() + end.size();
     auto op = std::make_tuple(BatchLogType::OP_BATCH_REMOVE_RANGE,
                               std::forward<std::string>(begin),
                               std::forward<std::string>(end));
     batch_.emplace_back(std::move(op));
   }
 
-  void clear() { batch_.clear(); }
+  void reserve(int32_t size) { batch_.reserve(size); }
 
   const std::vector<std::tuple<BatchLogType, std::string, std::string>>& getBatch() {
     return batch_;
   }
 
+  // size of the batch, in bytes
+  size_t size() { return size_; }
+
  private:
   std::vector<std::tuple<BatchLogType, std::string, std::string>> batch_;
+  size_t size_{0};
 };
 
 }  // namespace kvstore

src/kvstore/NebulaSnapshotManager.cpp
@@ -4,25 +4,46 @@
  * attached with Common Clause Condition 1.0, found in the LICENSES directory.
  */
 
-#include "kvstore/SnapshotManagerImpl.h"
+#include "kvstore/NebulaSnapshotManager.h"
 
 #include "common/utils/NebulaKeyUtils.h"
 #include "kvstore/LogEncoder.h"
+#include "kvstore/RateLimiter.h"
 
-DEFINE_int32(snapshot_batch_size, 1024 * 1024 * 10, "batch size for snapshot");
+DEFINE_uint32(snapshot_part_rate_limit,
+              1024 * 1024 * 2,
+              "max bytes of pulling snapshot for each partition in one second");
+DEFINE_uint32(snapshot_batch_size, 1024 * 512, "batch size for snapshot, in bytes");
 
 namespace nebula {
 namespace kvstore {
 
 const int32_t kReserveNum = 1024 * 4;
-void SnapshotManagerImpl::accessAllRowsInSnapshot(GraphSpaceID spaceId,
-                                                  PartitionID partId,
-                                                  raftex::SnapshotCallback cb) {
+
+NebulaSnapshotManager::NebulaSnapshotManager(NebulaStore* kv) : store_(kv) {
+  // Snapshot rate is limited to FLAGS_snapshot_worker_threads * FLAGS_snapshot_part_rate_limit.
+  // So by default, the total send rate is limited to 4 * 2Mb = 8Mb.
+  rateLimiter_.reset(
+      new RateLimiter(FLAGS_snapshot_part_rate_limit, FLAGS_snapshot_part_rate_limit));
+}
+
+void NebulaSnapshotManager::accessAllRowsInSnapshot(GraphSpaceID spaceId,
+                                                    PartitionID partId,
+                                                    raftex::SnapshotCallback cb) {
   CHECK_NOTNULL(store_);
   auto tables = NebulaKeyUtils::snapshotPrefix(partId);
   std::vector<std::string> data;
   int64_t totalSize = 0;
   int64_t totalCount = 0;
+  LOG(INFO) << folly::format(
+      "Space {} Part {} start send snapshot, rate limited to {}, batch size is {}",
+      spaceId,
+      partId,
+      FLAGS_snapshot_part_rate_limit,
+      FLAGS_snapshot_batch_size);
+  // raft will make sure that there won't be concurrent accessAllRowsInSnapshot of a given partition
+  rateLimiter_->add(spaceId, partId);
+  SCOPE_EXIT { rateLimiter_->remove(spaceId, partId); };
 
   for (const auto& prefix : tables) {
     if (!accessTable(spaceId, partId, prefix, cb, data, totalCount, totalSize)) {
@@ -34,13 +55,13 @@ void SnapshotManagerImpl::accessAllRowsInSnapshot(GraphSpaceID spaceId,
 
 // Promise is set in callback. Access part of the data, and try to send to
 // peers. If send failed, will return false.
-bool SnapshotManagerImpl::accessTable(GraphSpaceID spaceId,
-                                      PartitionID partId,
-                                      const std::string& prefix,
-                                      raftex::SnapshotCallback& cb,
-                                      std::vector<std::string>& data,
-                                      int64_t& totalCount,
-                                      int64_t& totalSize) {
+bool NebulaSnapshotManager::accessTable(GraphSpaceID spaceId,
+                                        PartitionID partId,
+                                        const std::string& prefix,
+                                        raftex::SnapshotCallback& cb,
+                                        std::vector<std::string>& data,
+                                        int64_t& totalCount,
+                                        int64_t& totalSize) {
   std::unique_ptr<KVIterator> iter;
   auto ret = store_->prefix(spaceId, partId, prefix, &iter);
   if (ret != nebula::cpp2::ErrorCode::SUCCEEDED) {
@@ -50,9 +71,10 @@ bool SnapshotManagerImpl::accessTable(GraphSpaceID spaceId,
     return false;
   }
   data.reserve(kReserveNum);
-  int32_t batchSize = 0;
+  size_t batchSize = 0;
   while (iter && iter->valid()) {
     if (batchSize >= FLAGS_snapshot_batch_size) {
+      rateLimiter_->consume(spaceId, partId, batchSize);
       if (cb(data, totalCount, totalSize, raftex::SnapshotStatus::IN_PROGRESS)) {
         data.clear();
         batchSize = 0;

src/kvstore/NebulaSnapshotManager.h
@@ -4,19 +4,21 @@
  * attached with Common Clause Condition 1.0, found in the LICENSES directory.
  */
 
-#ifndef KVSTORE_SNAPSHOTMANAGERIMPL_H_
-#define KVSTORE_SNAPSHOTMANAGERIMPL_H_
+#pragma once
+
+#include <folly/TokenBucket.h>
 
 #include "common/base/Base.h"
-#include "kvstore/KVStore.h"
+#include "kvstore/NebulaStore.h"
+#include "kvstore/RateLimiter.h"
 #include "kvstore/raftex/SnapshotManager.h"
 
 namespace nebula {
 namespace kvstore {
 
-class SnapshotManagerImpl : public raftex::SnapshotManager {
+class NebulaSnapshotManager : public raftex::SnapshotManager {
  public:
-  explicit SnapshotManagerImpl(KVStore* kv) : store_(kv) {}
+  explicit NebulaSnapshotManager(NebulaStore* kv);
 
   void accessAllRowsInSnapshot(GraphSpaceID spaceId,
                                PartitionID partId,
@@ -31,10 +33,9 @@ class SnapshotManagerImpl : public raftex::SnapshotManager {
                    int64_t& totalCount,
                    int64_t& totalSize);
 
-  KVStore* store_;
+  std::unique_ptr<RateLimiter> rateLimiter_;
+  NebulaStore* store_;
 };
 
 }  // namespace kvstore
 }  // namespace nebula
-
-#endif  // KVSTORE_SNAPSHOTMANAGERIMPL_H_

commit b68fab2cfcf43e1c430afec542fd2b97e27e821a

fix rebuild index bug introduced in #2557

src/storage/admin/RebuildEdgeIndexTask.cpp
@@ -154,7 +154,7 @@ nebula::cpp2::ErrorCode RebuildEdgeIndexTask::buildIndexGlobal(GraphSpaceID spac
                                                     destination.toString(),
                                                     std::move(valuesRet).value());
         batchSize += indexKey.size() + indexVal.size();
-        data.emplace_back(std::move(indexKey), std::move(indexVal));
+        data.emplace_back(std::move(indexKey), indexVal);
       }
     }
     iter->next();

src/storage/admin/RebuildTagIndexTask.cpp
@@ -135,7 +135,7 @@ nebula::cpp2::ErrorCode RebuildTagIndexTask::buildIndexGlobal(GraphSpaceID space
         auto indexKey = IndexKeyUtils::vertexIndexKey(
             vidSize, part, item->get_index_id(), vertex.toString(), std::move(valuesRet).value());
         batchSize += indexKey.size() + indexVal.size();
-        data.emplace_back(std::move(indexKey), std::move(indexVal));
+        data.emplace_back(std::move(indexKey), indexVal);
       }
     }
     iter->next();