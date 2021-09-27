d9b8e286902a3c97e6a7e68a77f4bff4270646d0
yaphet <4414314+darionyaphet@users.noreply.github.com>
2021-09-27 21:03:34 +0800

    fix iter miss match (#2936)
    Co-authored-by: Doodle <13706157+critical27@users.noreply.github.com>

--- src/meta/processors/zone/ListZonesProcessor.cpp
+++ src/meta/processors/zone/ListZonesProcessor.cpp
@@ -28,35 +28,12 @@ void ListZonesProcessor::process(const cpp2::ListZonesReq&) {
     auto hosts = MetaServiceUtils::parseZoneHosts(iter->val());
     cpp2::Zone zone;
     zone.set_zone_name(std::move(zoneName));
-    zone.set_nodes(std::move(hosts));
-    zones.emplace_back(std::move(zone));
-    iter->next();
-  }
-
-  const auto& groupPrefix = MetaServiceUtils::groupPrefix();
-  auto groupIterRet = doPrefix(groupPrefix);
-  if (!nebula::ok(groupIterRet)) {
-    auto retCode = nebula::error(groupIterRet);
-    LOG(ERROR) << "Get groups failed, error: " << apache::thrift::util::enumNameSafe(retCode);
-    handleErrorCode(retCode);
-    onFinished();
-    return;
-  }
-
-  auto groupIter = nebula::value(groupIterRet).get();
-  while (groupIter->valid()) {
-    auto zoneNames = MetaServiceUtils::parseZoneNames(groupIter->val());
-    for (auto& name : zoneNames) {
-      auto it = std::find_if(zones.begin(), zones.end(), [&name](const auto& zone) {
-        return name == zone.get_zone_name();
-      });
-      if (it == zones.end()) {
-        cpp2::Zone zone;
-        zone.set_zone_name(name);
-        zone.set_nodes({});
-        zones.emplace_back(std::move(zone));
-      }
+    if (hosts.size() != 0) {
+      zone.set_nodes(std::move(hosts));
+    } else {
+      zone.set_nodes({HostAddr("", 0)});
     }
+    zones.emplace_back(std::move(zone));
     iter->next();
   }
 
