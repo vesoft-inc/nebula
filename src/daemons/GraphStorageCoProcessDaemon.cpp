ddee99bb54cf1a5c9924c72071ea1c46ff5c4930
SuperYoko <90179377+SuperYoko@users.noreply.github.com>
2021-10-11 04:26:32 +0000

    cmake togethor

---
+++ src/daemons/GraphStorageCoProcessDaemon.cpp
@@ -0,0 +1,11 @@
+/* Copyright (c) 2021 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+int main(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
+  return 0;
+}
