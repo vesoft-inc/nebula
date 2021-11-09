5684e0f6d6c673611c3ee074345c69590394839e
Josh Soref <jsoref@users.noreply.github.com>
2021-11-09 17:29:20 -0500

    spelling: divided

--- src/common/network/NetworkUtils.cpp
+++ src/common/network/NetworkUtils.cpp
@@ -88,7 +88,7 @@ bool NetworkUtils::getDynamicPortRange(uint16_t& low, uint16_t& high) {
 
   if (fscanf(pipe, "%hu %hu", &low, &high) != 2) {
     LOG(ERROR) << "Failed to read from /proc/sys/net/ipv4/ip_local_port_range";
-    // According to ICANN, the port range is devided into three sections
+    // According to ICANN, the port range is divided into three sections
     //
     // Well-known ports: 0 to 1023 (used for system services)
     // Registered/user ports: 1024 to 49151
