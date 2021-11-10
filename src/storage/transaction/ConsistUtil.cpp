6acb5cbbaa64e9e7e1871bc667db89c0d3322eb1
Josh Soref <jsoref@users.noreply.github.com>
2021-11-09 21:19:54 -0500

    spelling: happen

--- src/storage/transaction/ConsistUtil.cpp
+++ src/storage/transaction/ConsistUtil.cpp
@@ -62,7 +62,7 @@ RequestType ConsistUtil::parseType(folly::StringPiece val) {
     case 'a':
       return RequestType::INSERT;
     default:
-      LOG(FATAL) << "shoule not happend, identifier is " << identifier;
+      LOG(FATAL) << "shoule not happen, identifier is " << identifier;
   }
 }
 
