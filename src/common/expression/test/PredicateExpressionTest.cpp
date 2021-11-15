026dea5bb002b356ce3a1d1530fcadb0f512c6a3
Yee <2520865+yixinglu@users.noreply.github.com>
2021-11-15 12:17:01 +0800

    Merge all expression tests to save disk consumption (#3301)
    * Merge all expression tests
    
    You can use --gtest_filter to run your special tests
    
    * Restore expr mock obj
    
    * Fix compile error
    
    * fix compile

--- src/common/expression/test/PredicateExpressionTest.cpp
+++ src/common/expression/test/PredicateExpressionTest.cpp
@@ -140,11 +140,3 @@ TEST_F(PredicateExpressionTest, PredicateExprToString) {
   }
 }
 }  // namespace nebula
-
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  folly::init(&argc, &argv, true);
-  google::SetStderrLogging(google::INFO);
-
-  return RUN_ALL_TESTS();
-}
