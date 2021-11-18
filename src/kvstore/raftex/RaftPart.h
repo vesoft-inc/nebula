9ebc49d5bb67a3643d83655492c9553a9624eaf2
Doodle <13706157+critical27@users.noreply.github.com>
2021-11-17 19:41:24 -0600

    [Raft] make me crazy (#3172)
    * cherry
    
    * minor log changes, fix a part is reset more than expected during waiting for snapshot
    
    * optimize when rpc timeout
    
    * * no more weird "has been sended" and "self->lastLogIdSent_ = self->logIdToSend_ - 1;"
    * do not send pending wals when leader is sending a snapshot
    * unify E_LOG_GAP and E_LOG_STALE
    
    * wal_not_found
    
    * * getPendingReqIfAny
    * unify SUCCEEDED E_LOG_GAP E_LOG_STALE
    
    * election don't wait forever
    
    * handle elelction resp in worker thread
    
    * fix deadlock
    
    * resolve conflicts, simplify, add a bit more comments
    
    * fix pytest
    
    * fix pytest again
    
    Co-authored-by: yaphet <4414314+darionyaphet@users.noreply.github.com>

--- src/kvstore/raftex/RaftPart.h
+++ src/kvstore/raftex/RaftPart.h
@@ -344,14 +344,20 @@ class RaftPart : public std::enable_shared_from_this<RaftPart> {
   void cleanupSnapshot();
 
   // The method sends out AskForVote request
-  // It return true if a leader is elected, otherwise returns false
-  bool leaderElection();
+  // Return true if a leader is elected (the leader could be self or others),
+  // otherwise returns false
+  folly::Future<bool> leaderElection();
 
   // The method will fill up the request object and return TRUE
   // if the election should continue. Otherwise the method will
   // return FALSE
   bool prepareElectionRequest(cpp2::AskForVoteRequest& req,
                               std::vector<std::shared_ptr<Host>>& hosts);
+
+  // return true if elected as the leader, else return false
+  bool handleElectionResponses(const ElectionResponses& resps,
+                               const std::vector<std::shared_ptr<Host>>& hosts,
+                               TermID proposedTerm);
 
   // The method returns the partition's role after the election
   Role processElectionResponses(const ElectionResponses& results,
