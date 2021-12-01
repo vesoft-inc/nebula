68ea96e38206a29be01926eb91c0a997fd4b2314
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2021-12-01 18:49:39 +0800

    add cmake git hooks options

--- cmake/nebula/GitHooksConfig.cmake
+++ cmake/nebula/GitHooksConfig.cmake
@@ -1,19 +1,21 @@
-if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git/")
-    # Create the pre-commit hook every time we run cmake
-    message(STATUS "Create the pre-commit hook")
-    set(PRE_COMMIT_HOOK ${CMAKE_CURRENT_SOURCE_DIR}/.git/hooks/pre-commit)
-    execute_process(
-        COMMAND
-        "rm" "-f" ${PRE_COMMIT_HOOK}
-    )
-    execute_process(
-        COMMAND
-        "ln" "-s" ${CMAKE_CURRENT_SOURCE_DIR}/.linters/cpp/hooks/pre-commit.sh ${PRE_COMMIT_HOOK}
-        RESULT_VARIABLE retcode
-    )
-    if(${retcode} EQUAL 0)
-        MESSAGE(STATUS "Creating pre-commit hook done")
-    else()
-        MESSAGE(FATAL_ERROR "Creating pre-commit hook failed: ${retcode}")
+if(ENABLE_CREATE_GIT_HOOKS) 
+    if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git/")
+        # Create the pre-commit hook every time we run cmake
+        message(STATUS "Create the pre-commit hook")
+        set(PRE_COMMIT_HOOK ${CMAKE_CURRENT_SOURCE_DIR}/.git/hooks/pre-commit)
+        execute_process(
+            COMMAND
+            "rm" "-f" ${PRE_COMMIT_HOOK}
+        )
+        execute_process(
+            COMMAND
+            "ln" "-s" ${CMAKE_CURRENT_SOURCE_DIR}/.linters/cpp/hooks/pre-commit.sh ${PRE_COMMIT_HOOK}
+            RESULT_VARIABLE retcode
+        )
+        if(${retcode} EQUAL 0)
+            MESSAGE(STATUS "Creating pre-commit hook done")
+        else()
+            MESSAGE(FATAL_ERROR "Creating pre-commit hook failed: ${retcode}")
+        endif()
     endif()
 endif()
