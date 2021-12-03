af3da371a5bff6a9dc0d54be2b717b31c525337b
panda-sheep <59197347+panda-sheep@users.noreply.github.com>
2021-12-03 11:18:38 +0800

    support specify partId mode

--- src/tools/db-upgrade/DbUpgraderTool.cpp
+++ src/tools/db-upgrade/DbUpgraderTool.cpp
@@ -4,17 +4,19 @@
  */
 
 #include "common/base/Base.h"
+#include "common/thrift/ThriftTypes.h"
 #include "kvstore/RocksEngineConfig.h"
 #include "tools/db-upgrade/DbUpgrader.h"
+#include "tools/db-upgrade/NebulaKeyUtilsV1.h"
 
 void printHelp() {
   fprintf(
       stderr,
-      R"(  ./db_upgrade --src_db_path=<path to rocksdb> --dst_db_path=<path to rocksdb> --upgrade_meta_server=<ip:port,...> --upgrade_version=<1|2>
+      R"(  ./db_upgrade --src_db_path=<path to rocksdb> --dst_db_path=<path to rocksdb> --upgrade_meta_server=<ip:port,...> --raw_data_version=<1|2>
 
 desc:
         This tool is used to upgrade data from nebula 1.x or the previous versions of nebula 2.0 RC
-        to nebula 2.0 GA version.
+        to nebula 2.6 version.
 
 required:
        --src_db_path=<path to rocksdb>
@@ -39,11 +41,15 @@ required:
          A list of meta severs' ip:port seperated by comma.
          Default: 127.0.0.1:45500
 
-       --upgrade_version=<1|2>
+       --raw_data_version=<1|2>
          This tool can only upgrade 1.x data or 2.0 RC data.
-         When the value is 1, upgrade the data from 1.x to 2.0 GA.
-         When the value is 2, upgrade the data from 2.0 RC to 2.0 GA.
+         When the value is 1, upgrade the data from 1.x to 2.6.
+         When the value is 2, upgrade the data from 2.0 RC to 2.6.
          Default: 0
+
+       --partIds
+         Specify all partIds to be processed, separated by comma.
+
 
  optional:
        --write_batch_num=<N>
@@ -65,19 +71,23 @@ required:
 }
 
 void printParams() {
-  std::cout << "===========================PARAMS============================\n";
+  std::cout << "=========================== PARAMS ============================\n";
   std::cout << "meta server: " << FLAGS_upgrade_meta_server << "\n";
   std::cout << "source data path: " << FLAGS_src_db_path << "\n";
   std::cout << "destination data path: " << FLAGS_dst_db_path << "\n";
   std::cout << "The size of the batch written: " << FLAGS_write_batch_num << "\n";
-  std::cout << "upgrade data from version: " << FLAGS_upgrade_version << "\n";
+  std::cout << "raw data from version: " << FLAGS_raw_data_version << "\n";
+  std::cout << "specify all partIds to be processed: " << FLAGS_partIds << "\n ";
   std::cout << "whether to compact all data: " << (FLAGS_compactions == true ? "true" : "false")
             << "\n";
   std::cout << "maximum number of concurrent parts allowed:" << FLAGS_max_concurrent_parts << "\n";
   std::cout << "maximum number of concurrent spaces allowed: " << FLAGS_max_concurrent_spaces
             << "\n";
-  std::cout << "===========================PARAMS============================\n\n";
+  std::cout << "=========================== PARAMS ============================\n\n";
 }
+
+using nebula::GraphSpaceID;
+using nebula::PartitionID;
 
 int main(int argc, char* argv[]) {
   // When begin to upgrade the data, close compaction
@@ -110,7 +120,7 @@ int main(int argc, char* argv[]) {
   printParams();
 
   // Handle arguments
-  LOG(INFO) << "Prepare phase begin";
+  LOG(INFO) << "================== Prepare phase begin ==================";
   if (FLAGS_src_db_path.empty() || FLAGS_dst_db_path.empty()) {
     LOG(ERROR) << "Source data path or destination data path should be not empty.";
     return EXIT_FAILURE;
@@ -164,15 +174,96 @@ int main(int argc, char* argv[]) {
   CHECK_NOTNULL(schemaMan);
   CHECK_NOTNULL(indexMan);
 
-  if (FLAGS_upgrade_version != 1 && FLAGS_upgrade_version != 2) {
-    LOG(ERROR) << "Flag upgrade_version : " << FLAGS_upgrade_version
-               << " illegal, upgrade_version can only be 1 or 2";
+  if (FLAGS_raw_data_version != 1 && FLAGS_raw_data_version != 2) {
+    LOG(ERROR) << "Flag raw_data_version : " << FLAGS_raw_data_version
+               << " illegal, raw_data_version can only be 1 or 2";
     return EXIT_FAILURE;
   }
-  LOG(INFO) << "Prepare phase end";
+
+  // one space, check partId
+  std::vector<std::string> parts;
+  std::unordered_set<PartitionID> partsAll;
+  folly::split(",", FLAGS_partIds, parts, true);
+  for (auto& part : parts) {
+    PartitionID partId;
+    try {
+      partId = folly::to<PartitionID>(part);
+    } catch (const std::exception& ex) {
+      LOG(ERROR) << "Cannot convert part id " << part;
+      return EXIT_FAILURE;
+    }
+    partsAll.emplace(partId);
+  }
+
+  for (auto& path : srcPaths) {
+    auto dataPath = nebula::fs::FileUtils::joinPath(path, "nebula");
+    if (!nebula::fs::FileUtils::exist(dataPath)) {
+      LOG(ERROR) << "Source data path " << dataPath << " not exists!";
+      return EXIT_FAILURE;
+    }
+    auto subDirs = nebula::fs::FileUtils::listAllDirsInDir(dataPath.c_str());
+    if (subDirs.size() != 1) {
+      LOG(ERROR) << "Only handle one space!";
+      return EXIT_FAILURE;
+    }
+    auto space = subDirs[0];
+    GraphSpaceID spaceId;
+    try {
+      spaceId = folly::to<GraphSpaceID>(space);
+    } catch (const std::exception& ex) {
+      LOG(ERROR) << "Cannot convert space id " << space;
+      return EXIT_FAILURE;
+    }
+
+    auto sRet = schemaMan->toGraphSpaceName(spaceId);
+    if (!sRet.ok()) {
+      LOG(ERROR) << "Space id " << spaceId << " no found";
+      return EXIT_FAILURE;
+    }
+
+    auto spaceVidLenRet = metaClient->getSpaceVidLen(spaceId);
+    if (!spaceVidLenRet.ok()) {
+      LOG(ERROR) << "Space id " << spaceId << " vid len no found";
+      return EXIT_FAILURE;
+    }
+    auto spaceVidLen = spaceVidLenRet.value();
+    std::unique_ptr<nebula::kvstore::RocksEngine> readEngine(
+        new nebula::kvstore::RocksEngine(spaceId, spaceVidLen, path, "", nullptr, nullptr, true));
+
+    auto prefix = nebula::NebulaKeyUtilsV1::systemPrefix();
+    std::unique_ptr<nebula::kvstore::KVIterator> iter;
+    auto retCode = readEngine->prefix(prefix, &iter);
+    if (retCode != nebula::cpp2::ErrorCode::SUCCEEDED) {
+      LOG(ERROR) << "Space id " << spaceId << " get part system data failed";
+      return EXIT_FAILURE;
+    }
+
+    while (iter && iter->valid()) {
+      auto key = iter->key();
+      auto partId = nebula::NebulaKeyUtilsV1::getPart(key);
+      if (nebula::NebulaKeyUtilsV1::isSystemCommit(key)) {
+        // do nothing
+      } else if (nebula::NebulaKeyUtilsV1::isSystemPart(key)) {
+        LOG(INFO) << "Find part id " << partId << " system data.";
+        partsAll.erase(partId);
+      } else {
+        LOG(FATAL) << "Space id " << spaceId << " part " << partId << " illegal data!";
+      }
+      iter->next();
+    }
+  }
+  if (!partsAll.empty()) {
+    std::string partstr;
+    for (auto part : partsAll) {
+      partstr += folly::stringPrintf("%d ", part);
+    }
+    LOG(FATAL) << "There are some part " << partstr << "not found";
+  }
+  LOG(INFO) << "All parts has found, part id " << FLAGS_partIds;
+  LOG(INFO) << "================== Prepare phase end ==================";
 
   // Upgrade data
-  LOG(INFO) << "Upgrade phase bengin";
+  LOG(INFO) << "================== Upgrade phase bengi ==================";
 
   // The data path in storage conf is generally one, not too many.
   // So there is no need to control the number of threads here.
@@ -200,6 +291,6 @@ int main(int argc, char* argv[]) {
     t.join();
   }
 
-  LOG(INFO) << "Upgrade phase end";
+  LOG(INFO) << "================== Upgrade phase end ==================";
   return 0;
 }
