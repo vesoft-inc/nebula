56985d8c18ca6092ce0ebffeed26cfa71369d049
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2021-12-22 17:57:23 +0800

    Disable breakpad in debug mode.

--- src/daemons/GraphDaemon.cpp
+++ src/daemons/GraphDaemon.cpp
@@ -36,7 +36,7 @@ static Status setupSignalHandler();
 extern Status setupLogging();
 static void printHelp(const char *prog);
 static void setupThreadManager();
-#if defined(__x86_64__)
+#if defined(__x86_64__) && defined(NDEBUG)
 extern Status setupBreakpad();
 #endif
 
@@ -74,7 +74,7 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-#if defined(__x86_64__)
+#if defined(__x86_64__) && defined(NDEBUG)
   status = setupBreakpad();
   if (!status.ok()) {
     LOG(ERROR) << status;
