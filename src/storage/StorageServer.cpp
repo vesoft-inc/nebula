48bfca551d1a7932ddc1095ae065cb0103bf1ab4
Nivras <12605142+Nivras@users.noreply.github.com>
2021-12-28 15:28:55 +0800

    add LogMonitor to check the log disk is full

--- src/storage/StorageServer.cpp
+++ src/storage/StorageServer.cpp
@@ -198,6 +198,9 @@ bool StorageServer::start() {
   LOG(INFO) << "Init kvstore";
   kvstore_ = getStoreInstance();
 
+  LOG(INFO) << "Init LogMonitor";
+  logMonitor_ = std::make_unique<LogMonitor>();
+
   if (nullptr == kvstore_) {
     LOG(ERROR) << "Init kvstore failed";
     return false;
