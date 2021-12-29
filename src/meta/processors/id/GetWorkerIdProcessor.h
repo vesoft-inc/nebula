a18b6b848265dfd48d469a2c5ecf4991dab767d3
jackwener <30525741+jackwener@users.noreply.github.com>
2021-12-29 16:42:51 +0800

    fix header

--- src/meta/processors/id/GetWorkerIdProcessor.h
+++ src/meta/processors/id/GetWorkerIdProcessor.h
@@ -1,6 +1,6 @@
 /* Copyright (c) 2021 vesoft inc. All rights reserved.
  *
- * This source code is licensed under Apache 2.0 License,
+ * This source code is licensed under Apache 2.0 License.
  */
 
 #ifndef META_GETWORKERIDPROCESSOR_H_
