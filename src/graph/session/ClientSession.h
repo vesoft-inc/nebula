54ba2956a891496ced1f39d9e5bc578df13cae32
zhaohaifei <32253291+zhaohaifei@users.noreply.github.com>
2022-02-10 18:41:56 +0800

    comment

--- src/graph/session/ClientSession.h
+++ src/graph/session/ClientSession.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2020 vesoft inc. All rights reserved.
-
+//
 // This source code is licensed under Apache 2.0 License.
 
 #ifndef GRAPH_SESSION_CLIENTSESSION_H_
--- src/graph/session/GraphSessionManager.cpp
+++ src/graph/session/GraphSessionManager.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2018 vesoft inc. All rights reserved.
-
+//
 // This source code is licensed under Apache 2.0 License.
 
 #include "graph/session/GraphSessionManager.h"
