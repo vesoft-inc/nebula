25a06217ebaf169e1f0e5ff6a797ba6f0c41fc35
jie.wang <38901892+jievince@users.noreply.github.com>
2022-02-11 15:02:29 +0800

    Property pruner (#3750)
    * property pruner
    
    * postprocess works.
    
    * add Travese::pruneProperties
    
    * make vertexprops of travese be null when node alias is not used
    
    * fix extractPropsFromExps for tagPropExpr and edgePropexpr, vFilter and eFilter
    
    * add PropertyTracker::update for project node
    
    * add DeduceMatchPropsVisitor and FLAGS_enable_opt_collapse_project_rule
    
    * add Filter::pruneProperties and only 3 tck cases not passed
    
    * Do not do propsUsed.colsSet.erase(it)
    
    * revert labeltagpropexpr and pass all tck
    
    * add hasAlias, add deduce types for id, src, dst func
    
    * rename visitor
    
    * ingore func id, src, dst, type, typeid, rank, hash in property tracker visitor
    
    * add Aggregate::pruneProperties and store the alias of id/src/dst... to propsused
    
    * format tck
    
    * remove some unusedless headers
    
    * add tck for plan
    
    * rename has1, has2, has3
    
    * add flag_enable_optimizer_property_pruner_rule and support prune edge props...
    
    * add kUnknownEdgeType
    
    * add PrunePropertiesVisitor
    
    * remove PlanNode::pruneProperties, and move PropertyTracker to PropertyTrackerVisitor
    
    * add PrunePropertiesRule.feature
    
    * add markDeleted interface
    
    * remove unused code
    
    * fix header macro
    
    * update tck
    
    Co-authored-by: kyle.cao <kyle.cao@vesoft.com>

---
+++ src/graph/planner/plan/PlanNodeVisitor.h
@@ -0,0 +1,31 @@
+/* Copyright (c) 2020 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License.
+ */
+
+#ifndef PLAN_PLANNODEVISITOR_H_
+#define PLAN_PLANNODEVISITOR_H_
+
+#include "graph/planner/plan/PlanNode.h"
+#include "graph/planner/plan/Query.h"
+
+namespace nebula {
+namespace graph {
+
+class PlanNodeVisitor {
+ public:
+  virtual ~PlanNodeVisitor() = default;
+
+  virtual void visit(PlanNode *node) = 0;
+  virtual void visit(Filter *node) = 0;
+  virtual void visit(Project *node) = 0;
+  virtual void visit(Aggregate *node) = 0;
+  virtual void visit(Traverse *node) = 0;
+  virtual void visit(AppendVertices *node) = 0;
+  virtual void visit(BiJoin *node) = 0;
+};
+
+}  // namespace graph
+}  // namespace nebula
+
+#endif  // PLAN_PLANNODEVISITOR_H_
