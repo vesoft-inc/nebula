745206f4bd71a5a8d0488290b8b30070f015fbca
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2022-02-15 17:05:14 +0800

    Add permission checking for kill queries.

---
+++ tests/tck/steps/test_kill_permission_via_different_service.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2022 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License.
+
+from pytest_bdd import scenarios
+
+scenarios('slowquery/permissionViaDifferentService.feature')
