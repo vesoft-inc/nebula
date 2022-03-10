47022b7ea8ec24666e882945333d56b360709755
冯亮(26011345) <fengliang003@ke.com>
2022-03-10 11:48:29 +0800

    format code

--- src/tools/session-tool/SessionTool.cpp
+++ src/tools/session-tool/SessionTool.cpp
@@ -21,7 +21,10 @@ namespace meta {
 
 class SessionTool {
  public:
-  static Status sessionsCount(const char* path, const std::string op, const std::string user, const int limit) {
+  static Status sessionsCount(const char* path,
+                              const std::string op,
+                              const std::string user,
+                              const int limit) {
     LOG(INFO) << "open rocksdb on " << path;
     rocksdb::DB* db = nullptr;
     rocksdb::Options options;
@@ -34,7 +37,7 @@ class SessionTool {
     if (!iter) {
       return Status::Error("Init iterator failed");
     }
-    
+
     std::string prefix = "__sessions__";
     iter->Seek(rocksdb::Slice(prefix));
     {
@@ -42,7 +45,8 @@ class SessionTool {
         int count = 0;
         while (iter->Valid() && iter->key().starts_with(prefix)) {
           if (!user.empty()) {
-            auto session = MetaKeyUtils::parseSessionVal(folly::StringPiece(iter->value().data(), iter->value().size()));
+            auto session = MetaKeyUtils::parseSessionVal(
+                folly::StringPiece(iter->value().data(), iter->value().size()));
             std::string userName = session.get_user_name();
             if (userName == user) {
               count++;
@@ -56,33 +60,27 @@ class SessionTool {
       }
     }
 
-    {  
+    {
       if ("list" == op) {
         int count = 0;
         LOG(INFO) << "SessionId\tUserName\tSpaceName\tCreateTime\tUpdateTime\tGraphAddr\tClientIp";
         while (iter->Valid() && iter->key().starts_with(prefix)) {
-          auto sessionID = MetaKeyUtils::getSessionId(folly::StringPiece(iter->key().data(), iter->key().size()));
-          auto session = MetaKeyUtils::parseSessionVal(folly::StringPiece(iter->value().data(), iter->value().size()));
+          auto sessionID = MetaKeyUtils::getSessionId(
+              folly::StringPiece(iter->key().data(), iter->key().size()));
+          auto session = MetaKeyUtils::parseSessionVal(
+              folly::StringPiece(iter->value().data(), iter->value().size()));
           std::string userName = session.get_user_name();
           if (!user.empty()) {
             if (userName == user) {
-              LOG(INFO) << sessionID << "\t" 
-                        << userName << "\t"
-                        << session.get_space_name() << "\t"
-                        << session.get_create_time() << "\t"
-                        << session.get_update_time() << "\t"
-                        << session.get_graph_addr() << "\t"
-                        << session.get_client_ip();
+              LOG(INFO) << sessionID << "\t" << userName << "\t" << session.get_space_name() << "\t"
+                        << session.get_create_time() << "\t" << session.get_update_time() << "\t"
+                        << session.get_graph_addr() << "\t" << session.get_client_ip();
               count++;
             }
           } else {
-            LOG(INFO) << sessionID << "\t" 
-                      << userName << "\t"
-                      << session.get_space_name() << "\t"
-                      << session.get_create_time() << "\t"
-                      << session.get_update_time() << "\t"
-                      << session.get_graph_addr() << "\t"
-                      << session.get_client_ip();
+            LOG(INFO) << sessionID << "\t" << userName << "\t" << session.get_space_name() << "\t"
+                      << session.get_create_time() << "\t" << session.get_update_time() << "\t"
+                      << session.get_graph_addr() << "\t" << session.get_client_ip();
             count++;
           }
           if (limit > 0 && count >= limit) {
@@ -97,7 +95,8 @@ class SessionTool {
       if ("groupCount" == op) {
         auto map = std::unordered_map<std::string, int>();
         while (iter->Valid() && iter->key().starts_with(prefix)) {
-          auto session = MetaKeyUtils::parseSessionVal(folly::StringPiece(iter->value().data(), iter->value().size()));
+          auto session = MetaKeyUtils::parseSessionVal(
+              folly::StringPiece(iter->value().data(), iter->value().size()));
           std::string userName = session.get_user_name();
           auto findPtr = map.find(userName);
           if (findPtr == map.end()) {
@@ -147,14 +146,18 @@ class SessionTool {
       if (user.empty()) {
         std::string prefix1 = "__sessiont__";
         LOG(INFO) << "Will delete all sessions of cluster ";
-        db->DeleteRange(rocksdb::WriteOptions(), db->DefaultColumnFamily(), rocksdb::Slice(prefix), rocksdb::Slice(prefix1));
+        db->DeleteRange(rocksdb::WriteOptions(),
+                        db->DefaultColumnFamily(),
+                        rocksdb::Slice(prefix),
+                        rocksdb::Slice(prefix1));
         LOG(INFO) << "All Sessions have been deleted";
       } else {
         int count = 0;
         iter->Seek(rocksdb::Slice(prefix));
         LOG(INFO) << "Will delete all sessions of " << user << " user";
         while (iter->Valid() && iter->key().starts_with(prefix)) {
-          auto session = MetaKeyUtils::parseSessionVal(folly::StringPiece(iter->value().data(), iter->value().size()));
+          auto session = MetaKeyUtils::parseSessionVal(
+              folly::StringPiece(iter->value().data(), iter->value().size()));
           std::string userName = session.get_user_name();
           if (userName == user) {
             db->Delete(rocksdb::WriteOptions(), iter->key());
@@ -179,9 +182,13 @@ class SessionTool {
   static std::string getUsage() {
     std::string usage = "Usage: session_tool <path> <operator> [user] [limit] \n";
     usage += "options introduction: \n";
-    usage += "-path    [require] the meta rocksdb path, absolute path need to end with path/nebula/0/data\n";
+    usage +=
+        "-path    [require] the meta rocksdb path, absolute path need to end with "
+        "path/nebula/0/data\n";
     usage += "-operator    [require] operator type, including count、delete、list and groupCount\n";
-    usage += "-user    use name, it can be used with list/delete operator to get/delete sessions of the specificed user \n";
+    usage +=
+        "-user    use name, it can be used with list/delete operator to get/delete sessions of the "
+        "specificed user \n";
     usage += "-limit   show number of sessions, it can be used with 'list' operator\n";
     return usage;
   }
@@ -206,15 +213,17 @@ int main(int argc, char* argv[]) {
     std::cout << nebula::meta::SessionTool::getUsage() << std::endl;
     return -1;
   }
-  if (FLAGS_operator.empty() || ("count" != FLAGS_operator && "delete" != FLAGS_operator
-     && "groupCount" != FLAGS_operator && "list" != FLAGS_operator)) {
-    LOG(INFO) << "Please specify the operator type, operator type includes count/delete/list/groupCount";
+  if (FLAGS_operator.empty() || ("count" != FLAGS_operator && "delete" != FLAGS_operator &&
+                                 "groupCount" != FLAGS_operator && "list" != FLAGS_operator)) {
+    LOG(INFO)
+        << "Please specify the operator type, operator type includes count/delete/list/groupCount";
     std::cout << nebula::meta::SessionTool::getUsage() << std::endl;
     return -1;
   }
   auto status = nebula::Status::OK();
   if ("count" == FLAGS_operator || "list" == FLAGS_operator || "groupCount" == FLAGS_operator) {
-    status = nebula::meta::SessionTool::sessionsCount(FLAGS_path.c_str(), FLAGS_operator, FLAGS_user, FLAGS_limit);
+    status = nebula::meta::SessionTool::sessionsCount(
+        FLAGS_path.c_str(), FLAGS_operator, FLAGS_user, FLAGS_limit);
   } else {
     status = nebula::meta::SessionTool::deleteSession(FLAGS_path.c_str(), FLAGS_user);
   }
