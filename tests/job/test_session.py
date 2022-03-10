8e0251fe28d46d4221ea17aa53462d602a3ad8e8
冯亮(26011345) <fengliang003@ke.com>
2022-03-10 17:41:12 +0800

    delete spaces at last row of file

--- tests/job/test_session.py
+++ tests/job/test_session.py
@@ -269,4 +269,3 @@ class TestSession(NebulaTestSuite):
         resp = self.execute('UPDATE CONFIGS graph:max_sessions_per_ip_per_user = 300')
         self.check_resp_succeeded(resp)
         time.sleep(3)
-        
