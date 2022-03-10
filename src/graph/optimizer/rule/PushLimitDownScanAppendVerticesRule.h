4a062cbdff5361ec8ca99eb96fb805cdea1801ee
kyle.cao <kyle.cao@vesoft.com>
2022-03-10 16:58:16 +0800

    add comments for PushLimitDownScanEdgesAppendVerticesRule

--- src/graph/optimizer/rule/PushLimitDownScanAppendVerticesRule.h
+++ src/graph/optimizer/rule/PushLimitDownScanAppendVerticesRule.h
@@ -12,7 +12,7 @@ namespace nebula {
 namespace opt {
 
 /*
-  Push down the limit to storage layer
+  Embedding limit to [[ScanVertices]]
   Required conditions:
    1. Match the pattern
    2. All filters of [[AppendVertices]] must be nullptr
