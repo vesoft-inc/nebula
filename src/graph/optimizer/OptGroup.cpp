54500b42bf962ffbee45c6201c05f8cd911094e4
kyle.cao <kyle.cao@vesoft.com>
2022-03-14 15:28:57 +0800

    add comments for memo operations

--- src/graph/optimizer/OptGroup.cpp
+++ src/graph/optimizer/OptGroup.cpp
@@ -111,6 +111,7 @@ Status OptGroup::explore(const OptRule *rule) {
 }
 
 Status OptGroup::exploreUntilMaxRound(const OptRule *rule) {
+  // The maximum number of iterations of a rule
   auto maxRound = kMaxExplorationRound;
   while (!isExplored(rule)) {
     if (0 >= maxRound--) {
