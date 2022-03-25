7f08c8ecc2d84354abd3fde43564c3cd4c43a874
Yichen Wang <18348405+Aiee@users.noreply.github.com>
2022-03-25 15:29:14 +0800

    Revert pr 3942 (#4075)
    Co-authored-by: Sophie <84560950+Sophie-Xie@users.noreply.github.com>

--- src/clients/meta/MetaClient.h
+++ src/clients/meta/MetaClient.h
@@ -156,7 +156,6 @@ using FTIndexMap = std::unordered_map<std::string, cpp2::FTIndex>;
 
 using SessionMap = std::unordered_map<SessionID, cpp2::Session>;
 
-using clientAddrMap = folly::ConcurrentHashMap<HostAddr, int64_t>;
 class MetaChangedListener {
  public:
   virtual ~MetaChangedListener() = default;
@@ -671,10 +670,6 @@ class MetaClient {
     return options_.localHost_.toString();
   }
 
-  clientAddrMap& getClientAddrMap() {
-    return clientAddrMap_;
-  }
-
  protected:
   // Return true if load succeeded.
   bool loadData();
@@ -761,9 +756,6 @@ class MetaClient {
 
   Status verifyVersion();
 
-  // Removes expired keys in the clientAddrMap_
-  void clearClientAddrMap();
-
  private:
   std::shared_ptr<folly::IOThreadPoolExecutor> ioThreadPool_;
   std::shared_ptr<thrift::ThriftClientManager<cpp2::MetaServiceAsyncClient>> clientsMan_;
@@ -843,18 +835,6 @@ class MetaClient {
 
   NameIndexMap tagNameIndexMap_;
   NameIndexMap edgeNameIndexMap_;
-
-  // TODO(Aiee) This is a walkaround to address the problem that using a lower version(< v2.6.0)
-  // client to connect with higher version(>= v3.0.0) Nebula service will cause a crash.
-  //
-  // The key here is the host of the client that sends the request, and the value indicates the
-  // expiration of the key because we don't want to keep the key forever.
-  //
-  // The assumption here is that there is ONLY ONE VERSION of the client in the host.
-  //
-  // This map will be updated when verifyVersion() is called. Only the clients since v2.6.0 will
-  // call verifyVersion(), thus we could determine whether the client version is lower than v2.6.0
-  clientAddrMap clientAddrMap_;
 
   // Global service client
   ServiceClientsList serviceClientList_;
