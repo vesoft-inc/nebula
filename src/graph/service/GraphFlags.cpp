eb74062d13348de8a0cb3f6b81f37bc485ab5eaf
CPWstatic <13495049+CPWstatic@users.noreply.github.com>
2022-05-25 15:56:58 +0800

    Modify the default config.

--- src/graph/service/GraphFlags.cpp
+++ src/graph/service/GraphFlags.cpp
@@ -90,5 +90,5 @@ static bool ValidateSessIdleTimeout(const char* flagname, int32_t value) {
 DEFINE_validator(session_idle_timeout_secs, &ValidateSessIdleTimeout);
 DEFINE_validator(client_idle_timeout_secs, &ValidateSessIdleTimeout);
 
-DEFINE_int32(min_batch_size, 10000, "");
-DEFINE_int32(max_job_size, 20, "");
+DEFINE_int32(min_batch_size, 8192, "The min batch size for handling dataset in multi job mode.");
+DEFINE_int32(max_job_size, 1, "The max job size in multi job mode.");
