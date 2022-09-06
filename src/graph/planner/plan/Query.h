fc5735a133a21d1c1093c4735e0b3b5e9609856a
jimingquan <mingquan.ji@vesoft.com>
2022-09-06 16:22:18 +0800

    extract attribute from properties function (#4604)
    * extract attribute from properties function
    
    * fix error
    
    * fix subscript error
    
    * add test case
    
    * process scanEdges
    
    * fix test error

--- src/graph/planner/plan/Query.h
+++ src/graph/planner/plan/Query.h
@@ -719,6 +719,8 @@ class ScanEdges final : public Explore {
   PlanNode* clone() const override;
   std::unique_ptr<PlanNodeDescription> explain() const override;
 
+  void accept(PlanNodeVisitor* visitor) override;
+
  private:
   friend ObjectPool;
   ScanEdges(QueryContext* qctx,
