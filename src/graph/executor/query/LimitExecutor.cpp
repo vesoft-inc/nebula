085381ba456f94e828c070065de8d911dae62f34
kyle.cao <kyle.cao@vesoft.com>
2022-09-07 16:14:15 +0800

    Fix concurrent exception related to multi-match statement (#4605)
    * fix filter executor
    
    * Fix concurrency exception of multi-match statements
    
    fix iterator
    
    fix
    
    small delete
    
    small delete
    
    skip iterator type handle for concurrency
    
    small delete
    
    fix scan edges
    
    small delete
    
    small delete
    
    fix
    
    small delete
    
    small change
    
    small change
    
    fix ut
    
    small fix
    
    Co-authored-by: Sophie <84560950+Sophie-Xie@users.noreply.github.com>

--- src/graph/executor/query/LimitExecutor.cpp
+++ src/graph/executor/query/LimitExecutor.cpp
@@ -13,16 +13,37 @@ folly::Future<Status> LimitExecutor::execute() {
   SCOPED_TIMER(&execTime_);
 
   auto* limit = asNode<Limit>(node());
-  Result result = ectx_->getResult(limit->inputVar());
+  auto inputVar = limit->inputVar();
+  // Use the userCount of the operator's inputVar at runtime to determine whether concurrent
+  // read-write conflicts exist, and if so, copy the data
+  bool canMoveData = movable(inputVar);
+  Result result = ectx_->getResult(inputVar);
   auto* iter = result.iterRef();
+  // Always reuse getNeighbors's dataset to avoid some go statement execution plan related issues
+  if (iter->isGetNeighborsIter()) {
+    canMoveData = true;
+  }
   ResultBuilder builder;
-  builder.value(result.valuePtr());
-  auto offset = limit->offset();
+  auto offset = static_cast<std::size_t>(limit->offset());
   QueryExpressionContext qec(ectx_);
-  auto count = limit->count(qec);
-  iter->select(offset, count);
-  builder.iter(std::move(result).iter());
-  return finish(builder.build());
+  auto count = static_cast<std::size_t>(limit->count(qec));
+  if (LIKELY(canMoveData)) {
+    builder.value(result.valuePtr());
+    iter->select(offset, count);
+    builder.iter(std::move(result).iter());
+    return finish(builder.build());
+  } else {
+    DataSet ds;
+    ds.colNames = result.getColNames();
+    for (std::size_t i = 0; iter->valid(); ++i, iter->next()) {
+      if (i >= offset && i <= (offset + count - 1)) {
+        Row row;
+        row = *iter->row();
+        ds.rows.emplace_back(std::move(row));
+      }
+    }
+    return finish(builder.value(Value(std::move(ds))).iter(Iterator::Kind::kProp).build());
+  }
 }
 
 }  // namespace graph
