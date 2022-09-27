f98ce5656838f4ecaf488d9df9ea219adfa01a06
pengweisong <90180021+pengweisong@users.noreply.github.com>
2022-09-27 11:46:50 +0800

    change time point calculate in ttl

--- src/storage/CommonUtils.cpp
+++ src/storage/CommonUtils.cpp
@@ -5,8 +5,6 @@
 
 #include "storage/CommonUtils.h"
 
-#include "common/time/WallClock.h"
-
 namespace nebula {
 namespace storage {
 
@@ -27,7 +25,7 @@ bool CommonUtils::checkDataExpiredForTTL(const meta::SchemaProviderIf* schema,
       ftype != nebula::cpp2::PropertyType::INT64) {
     return false;
   }
-  auto now = time::WallClock::fastNowInSec();
+  auto now = ::time(NULL);
 
   // if the value is not INT type (sush as NULL), it will never expire.
   // TODO (sky) : DateTime
