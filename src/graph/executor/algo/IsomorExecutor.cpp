c66c8fad57ca109c149598a68d6a019d76357c72
peter-rich <yangzhanfu111@gmail.com>
2022-09-30 13:25:04 +0800

    new api for edge and vertex

--- src/graph/executor/algo/IsomorExecutor.cpp
+++ src/graph/executor/algo/IsomorExecutor.cpp
@@ -20,52 +20,53 @@ folly::Future<Status> IsomorExecutor::execute() {
    auto iterQV = ectx_->getResult(isomor->getqScanVOut()).iter();
    auto iterDE = ectx_->getResult(isomor->getdScanEOut()).iter();
    auto iterQE = ectx_->getResult(isomor->getqScanEOut()).iter();
+
    unsigned int v_count = iterDV->size();
    unsigned int l_count = iterDV->size();
    unsigned int e_count = iterDE->size();
 
-   unsigned int *offset = new unsigned int[v_count + 1];
+   unsigned int *offset = new unsigned int[v_count + 2];
    unsigned int *neighbors = new unsigned int[e_count * 2];
-   // neighborhood offset
-   unsigned int *offset_neighbors = new unsigned int[v_count + 1];
-
    unsigned int *labels = new unsigned int[l_count];
-
    // load data vertices id and tags
-
    while (iterDV->valid()) {
-     unsigned int v_id = (unsigned int)iterDV->getVertices();
-     unsigned int l_id = (unsigned int)iterDV->getTags();
-     labels[v_id] = (unsigned int)v.getInt(1);
+     const auto v = iterDV->getColumn(nebula::kVid);  // check if v is a vertex
+     auto v_id = v.getInt();
+     const auto v2 = iterDV->getColumn(1);  // get label
+     auto l_id = v2.getInt();
+     // unsigned int v_id = (unsigned int)v.getInt(0);
+     labels[v_id] = l_id;  // Tag Id
      iterDV->next();
    }
+
    // load edges degree
    while (iterDE->valid()) {
-     auto &e = iterDE->getEdge();
-     unsigned int src = (unsigned int)e.src;
-     offset[src + 1]++;
+     auto s = iterDE->getEdgeProp("*", kSrc);
+     unsigned int src = s.getInt();
+     offset[src]++;
      iterDE->next();
-   }
-   for (unsigned int i = 0; i < v_count; i++) {
-     offset[i + 1] += offset[i];
-     offset_neighbors[i] = 0;
    }
 
    // load data edges
    offset[0] = 0;
-   auto iterDE = ectx_->getResult(isomor->getdScanEOut()).iter();
+   iterDE = ectx_->getResult(isomor->getdScanEOut()).iter();
    while (iterDE->valid()) {
-     auto &e = iterDE->getEdge();
-     unsigned int src = (unsigned int)e.src;
-     unsigned int dst = (unsigned int)e.dst;
-     neighbors[offset[src] + (offset_neighbors[src]++)] = dst;
+     unsigned int src = iterDE->getEdgeProp("*", kSrc).getInt();
+     unsigned int dst = iterDE->getEdgeProp("*", kDst).getInt();
+
+     neighbors[offset[src + 1]] = dst;
+     offset[src + 1]++;
      iterDE->next();
    }
-   // load query vertices and tags
+
+   delete offset;
+   delete neighbors;
+   delete labels;
+
    ResultBuilder builder;
+
    // Set result in the ds and set the new column name for the (isomor matching 's) result.
    return finish(ResultBuilder().value(Value(std::move(ds))).build());
  }
-
 }  // namespace graph
 }  // namespace nebula
