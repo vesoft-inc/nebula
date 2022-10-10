92c9e15538bf5a43ca9700365882c7150d1f6239
zhaojunnan <zhaojunnan@kanzhun.com>
2022-10-10 17:53:53 +0800

    引擎层filter下推判断、剪枝
    # Conflicts:
    #	src/graph/executor/query/GetNeighborsExecutor.cpp
    #	src/graph/planner/ngql/GoPlanner.cpp

--- src/graph/executor/query/GetNeighborsExecutor.cpp
+++ src/graph/executor/query/GetNeighborsExecutor.cpp
@@ -18,12 +18,29 @@ StatusOr<std::vector<Value>> GetNeighborsExecutor::buildRequestVids() {
   auto inputVar = gn_->inputVar();
   auto iter = ectx_->getResult(inputVar).iter();
   return buildRequestListByVidType(iter.get(), gn_->src(), gn_->dedup());
+  DataSet reqDs = buildRequestDataSetByVidType(iter.get(), gn_->src(), gn_->dedup());
+  if(gn_->isPush()){
+    const Value& val = ectx_->getResult(inputVar).value();
+    if(val.isList()){
+    } else if(val.isDataSet()){
+      DataSet& ds = const_cast<DataSet&>(val.getDataSet());
+      for(size_t i = 0; i<ds.colSize();i++){
+        reqDs.colNames.emplace_back(ds.colNames[i]);
+      }
+      for(size_t i = 0; i<ds.rowSize();i++){
+        for(size_t j = 0; j<ds.colSize();j++) {
+          reqDs.rows[i].emplace_back(ds.rows[i][j]);
+        }
+      }
+    }
+  }
+  return reqDs;
 }
 
 folly::Future<Status> GetNeighborsExecutor::execute() {
-  auto res = buildRequestVids();
-  NG_RETURN_IF_ERROR(res);
-  auto vids = std::move(res).value();
+  auto reqDs = buildRequestVids();
+  NG_RETURN_IF_ERROR(reqDs);
+  auto vids = std::move(reqDs).value();
   if (vids.empty()) {
     List emptyResult;
     return finish(ResultBuilder()
@@ -41,8 +58,8 @@ folly::Future<Status> GetNeighborsExecutor::execute() {
                                           qctx()->plan()->isProfileEnabled());
   return storageClient
       ->getNeighbors(param,
-                     {nebula::kVid},
-                     std::move(vids),
+                     std::move(vids.colNames),
+                     std::move(vids.rows),
                      gn_->edgeTypes(),
                      gn_->edgeDirection(),
                      gn_->statProps(),
