b0f15e04766a254a69621508ffe2c8bd4d02fda6
zhaojunnan@kanzhun.com <zhaojunnan@kanzhun.com>
2022-11-09 18:32:10 +0800

    Fix plan Test(#4805)

--- src/graph/validator/test/QueryValidatorTest.cpp
+++ src/graph/validator/test/QueryValidatorTest.cpp
@@ -57,7 +57,6 @@ TEST_F(QueryValidatorTest, GoZeroStep) {
         "GO 0 STEPS FROM \"1\" OVER like YIELD like._dst as id"
         "| GO FROM $-.id OVER serve YIELD edge as e";
     std::vector<PlanNode::Kind> expected = {PK::kProject,
-                                            PK::kInnerJoin,
                                             PK::kProject,
                                             PK::kGetNeighbors,
                                             PK::kDedup,
@@ -140,11 +139,11 @@ TEST_F(QueryValidatorTest, GoWithPipe) {
         "GO 1 STEPS FROM \"1\" OVER like YIELD like._dst AS "
         "id | GO 2 STEPS FROM $-.id OVER like YIELD edge as e";
     std::vector<PlanNode::Kind> expected = {
-        PK::kProject,      PK::kInnerJoin,    PK::kInnerJoin, PK::kProject, PK::kGetNeighbors,
-        PK::kLoop,         PK::kDedup,        PK::kDedup,     PK::kProject, PK::kProject,
-        PK::kDedup,        PK::kInnerJoin,    PK::kProject,   PK::kDedup,   PK::kProject,
-        PK::kProject,      PK::kGetNeighbors, PK::kDedup,     PK::kStart,   PK::kProject,
-        PK::kGetNeighbors, PK::kStart};
+        PK::kProject,      PK::kInnerJoin, PK::kProject, PK::kGetNeighbors, PK::kLoop,
+        PK::kDedup,        PK::kDedup,     PK::kProject, PK::kProject,      PK::kDedup,
+        PK::kInnerJoin,    PK::kProject,   PK::kDedup,   PK::kProject,      PK::kProject,
+        PK::kGetNeighbors, PK::kDedup,     PK::kStart,   PK::kProject,      PK::kGetNeighbors,
+        PK::kStart};
     EXPECT_TRUE(checkResult(query, expected));
   }
   {
@@ -152,7 +151,6 @@ TEST_F(QueryValidatorTest, GoWithPipe) {
         "GO 2 STEPS FROM \"1\" OVER like YIELD like._dst AS id"
         "| GO 1 STEPS FROM $-.id OVER like YIELD src(edge) as src";
     std::vector<PlanNode::Kind> expected = {PK::kProject,
-                                            PK::kInnerJoin,
                                             PK::kProject,
                                             PK::kGetNeighbors,
                                             PK::kDedup,
@@ -169,7 +167,6 @@ TEST_F(QueryValidatorTest, GoWithPipe) {
   {
     std::string query = "YIELD \"1\" AS id | GO FROM $-.id OVER like YIELD id($^) as id";
     std::vector<PlanNode::Kind> expected = {PK::kProject,
-                                            PK::kInnerJoin,
                                             PK::kProject,
                                             PK::kGetNeighbors,
                                             PK::kDedup,
@@ -181,7 +178,6 @@ TEST_F(QueryValidatorTest, GoWithPipe) {
   {
     std::string query = "YIELD \"1\" AS id | GO FROM $-.id OVER like YIELD id($$) as id";
     std::vector<PlanNode::Kind> expected = {PK::kProject,
-                                            PK::kInnerJoin,
                                             PK::kProject,
                                             PK::kGetNeighbors,
                                             PK::kDedup,
@@ -412,7 +408,6 @@ TEST_F(QueryValidatorTest, GoWithVariable) {
         "YIELD $var.name, $^.person.name, $$.person.name";
     std::vector<PlanNode::Kind> expected = {
         PK::kProject,
-        PK::kInnerJoin,
         PK::kLeftJoin,
         PK::kProject,
         PK::kGetVertices,
@@ -654,7 +649,6 @@ TEST_F(QueryValidatorTest, GoOneStep) {
         "| GO FROM $-.id OVER like YIELD dst(edge) as dst";
     std::vector<PlanNode::Kind> expected = {
         PK::kProject,
-        PK::kInnerJoin,
         PK::kProject,
         PK::kGetNeighbors,
         PK::kDedup,
@@ -718,13 +712,11 @@ TEST_F(QueryValidatorTest, OutputToAPipe) {
         "OVER serve YIELD dst(edge) as dst )";
     std::vector<PlanNode::Kind> expected = {
         PK::kProject,
-        PK::kInnerJoin,
         PK::kProject,
         PK::kGetNeighbors,
         PK::kDedup,
         PK::kProject,
         PK::kProject,
-        PK::kInnerJoin,
         PK::kProject,
         PK::kGetNeighbors,
         PK::kDedup,
