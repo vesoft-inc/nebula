ad34bcdc79652df4ba1e842dcc4638b48bd6da21
Cheng Xuntao <7731943+xtcyclist@users.noreply.github.com>
2022-12-05 14:41:55 +0800

    Restored UNKOWN_PROP but commented as deprecated in common.thrift (#4985)
    restore UNKOWN_PROP but commented as deprecated in common.thrift

--- src/interface/common.thrift
+++ src/interface/common.thrift
@@ -95,6 +95,7 @@ enum NullType {
     BAD_DATA = 2,
     BAD_TYPE = 3,
     ERR_OVERFLOW = 4,
+    UNKNOWN_PROP = 5,  // deprecated but not to be deleted
     DIV_BY_ZERO = 6,
     OUT_OF_RANGE = 7,
 } (cpp.enum_strict, cpp.type = "nebula::NullType")
