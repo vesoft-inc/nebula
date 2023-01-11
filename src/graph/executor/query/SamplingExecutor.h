de461aef9ca420ce0c04fe76416d86ddeb4bef00
codesigner <codesigner.huang@vesoft.com>
2023-01-11 15:32:20 +0800

    format change using clang-format

--- src/graph/executor/query/SamplingExecutor.h
+++ src/graph/executor/query/SamplingExecutor.h
@@ -18,11 +18,9 @@ class SamplingExecutor final : public Executor {
 
  private:
   template <typename U>
-  void executeBinarySample(Iterator *iter, size_t index, size_t count,
-                           DataSet &list);
+  void executeBinarySample(Iterator *iter, size_t index, size_t count, DataSet &list);
   template <typename U>
-  void executeAliasSample(Iterator *iter, size_t index, size_t count,
-                          DataSet &list);
+  void executeAliasSample(Iterator *iter, size_t index, size_t count, DataSet &list);
 };
 
 }  // namespace graph
