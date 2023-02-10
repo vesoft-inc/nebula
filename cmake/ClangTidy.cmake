39361664860fca9794ef73de31168de04a23a4af
yixinglu <2520865+yixinglu@users.noreply.github.com>
2023-02-10 10:52:52 +0800

    Support mold linker

--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -1,3 +1,8 @@
+# Copyright (c) 2020 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License.
+#
+
 if (ENABLE_CLANG_TIDY)
     if (${CMAKE_VERSION} VERSION_LESS "3.6.0")
         message(FATAL_ERROR "clang-tidy requires CMake version at least 3.6.")
