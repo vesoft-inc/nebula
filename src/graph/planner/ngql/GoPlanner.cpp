d1e21189b9fd57798718ab410bdee509298a26b4
Sophie <84560950+Sophie-Xie@users.noreply.github.com>
2023-05-15 19:13:50 +0800

    Cherry pick 3.5 (0513-0515) (#5552)
    * change batch size (#5550)
    
    * change batch size
    
    * add flag to config
    
    * Fix simple case #5551
    
    * address conflict
    
    ---------
    
    Co-authored-by: jimingquan <mingquan.ji@vesoft.com>

--- src/graph/planner/ngql/GoPlanner.cpp
+++ src/graph/planner/ngql/GoPlanner.cpp
@@ -295,10 +295,18 @@ StatusOr<SubPlan> GoPlanner::transform(AstContext* astCtx) {
       preRootNode_ = node;
     }
 
-    auto argNode = Argument::make(qctx, from.runtimeVidName);
-    argNode->setColNames({from.runtimeVidName});
-    goCtx_->vidsVar = argNode->outputVar();
-    startNode_ = argNode;
+    if (goCtx_->isSimple) {
+      //  go from 'xxx' over edge yield distinct edge._dst as id |
+      //    go from $-.id over edge yield distinct edge._dst
+      // above scenario, the second statement does not need the argument operator
+      startNode_ = preRootNode_;
+      goCtx_->vidsVar = varName;
+    } else {
+      auto argNode = Argument::make(qctx, from.runtimeVidName);
+      argNode->setColNames({from.runtimeVidName});
+      goCtx_->vidsVar = argNode->outputVar();
+      startNode_ = argNode;
+    }
   }
   auto& steps = goCtx_->steps;
   if (!steps.isMToN() && steps.steps() == 0) {
