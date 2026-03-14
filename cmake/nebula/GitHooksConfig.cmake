ec8968d4b6d94ca5f2db384ca4278aac35835917
Stevens <265947737+RichardWillamStevens@users.noreply.github.com>
2026-03-14 17:24:43 +0800

    Add Git hooks configuration for pre-commit hook creation

---
+++ cmake/nebula/GitHooksConfig.cmake
@@ -0,0 +1,19 @@
+if(ENABLE_CREATE_GIT_HOOKS AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git/")
+    # Create the pre-commit hook every time we run cmake
+    message(STATUS "Create the pre-commit hook")
+    set(PRE_COMMIT_HOOK ${CMAKE_CURRENT_SOURCE_DIR}/.git/hooks/pre-commit)
+    execute_process(
+        COMMAND
+        "rm" "-f" ${PRE_COMMIT_HOOK}
+    )
+    execute_process(
+        COMMAND
+        "ln" "-s" ${CMAKE_CURRENT_SOURCE_DIR}/.linters/cpp/hooks/pre-commit.sh ${PRE_COMMIT_HOOK}
+        RESULT_VARIABLE retcode
+    )
+    if(${retcode} EQUAL 0)
+        MESSAGE(STATUS "Creating pre-commit hook done")
+    else()
+        MESSAGE(FATAL_ERROR "Creating pre-commit hook failed: ${retcode}")
+    endif()
+endif()
